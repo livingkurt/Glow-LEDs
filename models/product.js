57d7e2b02055117f47d97b0d659d2e34d74c19cc
Kurt LaVacque <lavacquek@icloud.com>
2020-05-15 11:55:53 -0500

    Removed the Required True from the new images

--- models/product.js
+++ models/product.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose')
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   image_1: { type: String, required: true },
-  image_2: { type: String, required: true },
-  image_3: { type: String, required: true },
-  image_4: { type: String, required: true },
+  image_2: { type: String },
+  image_3: { type: String },
+  image_4: { type: String },
   brand: { type: String, required: true },
   price: { type: Number, default: 0, required: true },
   category: { type: String, required: true },
