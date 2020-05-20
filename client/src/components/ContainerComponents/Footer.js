9c74065f8585f1b062f8066a9daabf5a574a8352
Kurt LaVacque <lavacquek@icloud.com>
2020-05-20 14:29:50 -0500

    Simplified the Footer Styles

---
+++ client/src/components/ContainerComponents/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = (props) => {
+
+  const footer_styles = {
+    gridArea: "footer",
+    backgroundColor: "#333333",
+    color: "#ffffff",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center"
+  }
+
+  return (
+    <footer style={{ ...footer_styles, ...props.styles }}>
+      © 2020 Glow LEDs. All Rights Reserved
+    </footer>
+  );
+}
+
+export default Footer;
