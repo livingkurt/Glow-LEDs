db01ea1185609122f28a2eb268a3d588c02b5c3f
Kurt LaVacque <lavacquek@icloud.com>
2020-05-20 14:51:31 -0500

    Made the Label  a styled Component

--- client/src/components/UtilityComponents/Label.js
+++ client/src/components/UtilityComponents/Label.js
@@ -1,12 +1,15 @@
 // React
 import React from "react";
-// Styles
-import './label.css'
 
 
 function Label(props) {
+
+  const label_styles = {
+    fontSize: "16px"
+  }
+
   return (
-    <label style={props.styles} className="label">
+    <label style={{ ...props.styles, ...label_styles }} className="label">
       {props.children}
     </label>
   );
--- client/src/components/UtilityComponents/index.js
+++ client/src/components/UtilityComponents/index.js
@@ -1,7 +1,7 @@
 export { default as ButtonSymbol } from './ButtonSymbol/ButtonSymbol'
 export { default as ButtonWord } from './ButtonWord/ButtonWord'
 export { default as Checkbox } from './Checkbox/Checkbox'
-export { default as Label } from './Label/Label'
+export { default as Label } from './Label'
 export { default as Title } from './Title'
 export { default as EditorInput } from './EditorInput/EditorInput'
 export { default as Form } from './Form/Form'
