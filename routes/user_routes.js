c66e4cc8975d7420b331994c3bc32b095795bd83
Kurt LaVacque <lavacquek@icloud.com>
2020-05-28 15:58:34 -0500

    Create a new create admin path

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -7,6 +7,7 @@ const User = require('../models/user')
 const { getToken, isAuth } = require('../util')
 const bcrypt = require("bcryptjs");
 // const jwt = require("jsonwebtoken");
+require("dotenv")
 
 const router = express.Router();
 
@@ -43,16 +44,6 @@ router.post('/login', async (req, res) => {
   // Check password
   bcrypt.compare(password, login_user.password).then(isMatch => {
     if (isMatch) {
-      // User matched
-      // Create JWT Payload
-      // const payload = {
-      //   id: login_user.id,
-      //   name: login_user.name,
-      //   email: login_user.email,
-      //   isAdmin: login_user.isAdmin,
-      //   token: getToken(login_user)
-      // };
-      // Sign token
       res.send({
         _id: login_user.id,
         name: login_user.name,
@@ -61,49 +52,12 @@ router.post('/login', async (req, res) => {
         token: getToken(login_user)
       });
     }
-    //   jwt.sign(
-    //     payload,
-    //     "secret",
-    //     {
-    //       expiresIn: 31556926 // 1 year in seconds
-    //     },
-    //     (err, token) => {
-    //       // res.json({
-    //       //   success: true,
-    //       //   token: "Bearer " + token
-    //       // });
-    //       res.send({
-    //         _id: login_user.id,
-    //         name: login_user.name,
-    //         email: login_user.email,
-    //         isAdmin: login_user.isAdmin,
-    //         token: getToken(login_user)
-    //       });
-    //     }
-    //   );
     else {
       return res
         .status(400)
         .json({ passwordincorrect: "Password incorrect" });
     }
   });
-
-  // if (login_user) {
-  //   // if (login_user.confirmed) {
-  //   res.send({
-  //     _id: login_user.id,
-  //     name: login_user.name,
-  //     email: login_user.email,
-  //     isAdmin: login_user.isAdmin,
-  //     token: getToken(login_user)
-  //   });
-  //   // }
-  //   // else {
-  //   // res.status(401).send({ msg: 'Please confirm your email to login' });
-  //   // }
-  // } else {
-  //   res.status(401).send({ msg: 'Invalid Email or Password.' });
-  // }
 
 });
 
@@ -136,23 +90,51 @@ router.post('/register', async (req, res) => {
     });
   }
 
-
 })
 
 router.get("/createadmin", async (req, res) => {
-  try {
-    const user = new User({
-      name: 'Kurt',
-      email: 'lavacquek@icloud.com',
-      password: '1234',
-      confirmed: true,
-      isAdmin: true
-    });
-    const newUser = await user.save();
-    res.send(newUser);
-  } catch (error) {
-    res.send({ msg: error.message });
+  const admin = new User({
+    name: 'Kurt',
+    email: 'lavacquek@icloud.com',
+    password: "admin",
+    confirmed: true,
+    isAdmin: true
+  });
+  const user = await User.findOne({ email: req.body.email });
+  if (user) {
+    return res.status(400).json({ email: "Email already exists" });
   }
+  else {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(admin.password, salt, (err, hash) => {
+        if (err) throw err;
+        admin.password = hash;
+        admin
+          .save()
+          .then(admin => res.json({
+            _id: admin.id,
+            name: admin.name,
+            email: admin.email,
+            isAdmin: admin.isAdmin,
+            token: getToken(admin)
+          }))
+          .catch(err => console.log(err));
+      });
+    });
+  }
+  // try {
+  //   const user = new User({
+  //     name: 'Kurt',
+  //     email: 'lavacquek@icloud.com',
+  //     password: '1234',
+  //     confirmed: true,
+  //     isAdmin: true
+  //   });
+  //   const newUser = await user.save();
+  //   res.send(newUser);
+  // } catch (error) {
+  //   res.send({ msg: error.message });
+  // }
 });
 
 router.get("/:id", async (req, res) => {
