a04a22d87e639a1ea713530a6dd19cfc2797722c
Kurt LaVacque <lavacquek@icloud.com>
2020-06-25 14:41:43 -0500

    Updated all of the .env things in the reconfigs in heroku

--- config.js
+++ config.js
@@ -1,20 +1,12 @@
-// import dotenv from 'dotenv';
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-// export default {
-//   PORT: process.env.PORT || 5000,
-//   MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/amazona',
-//   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
-//   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
-// };
-
 module.exports = {
   PORT: process.env.PORT || 5000,
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/glow_leds_db',
+  MONGODB_URI: process.env.MONGODB_URI,
+  JWT_SECRET: process.env.JWT_SECRET,
+  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID
   // MONGODB_URL: process.env.MONGODB_URI || 'mongodb://livingkurt:07QXtrAS7vN7gp4G@ds217349.mlab.com:17349/heroku_vljhv1ts',
-  JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
-  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
 };
 
