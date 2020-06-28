bbec47dc2fc4c7976d3a937c4c6a5a6ae3f15c95
Kurt LaVacque <lavacquek@icloud.com>
2020-06-28 18:27:04 -0500

    Updated the Color of the Reviews

--- client/src/pages/ProductPage.jsx
+++ client/src/pages/ProductPage.jsx
@@ -323,9 +323,10 @@ const ProductPage = (props) => {
 									key={review._id}
 									style={{
 										listStyleType: 'none',
-										background: 'gray',
+										background: '#616161',
 										padding: '5px',
-										borderRadius: '15px'
+										borderRadius: '15px',
+										boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
 									}}
 								>
 									<div>{review.name}</div>
