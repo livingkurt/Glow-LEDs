860c5f673b781b80dd2adb5809e38af3d6012dcc
Kurt LaVacque <lavacquek@icloud.com>
2020-07-05 18:37:20 -0500

    Updated teh Home page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -41,6 +41,18 @@ const HomePage = (props) => {
 		padding: '56.25% 0 0'
 	};
 
+	const div_styles = {
+		// backgroundColor: '#2f2f2f',
+		// border: '3px #2f2f2f solid',
+		// background:'linear-gradient(135deg, rgba(0,154,120,1) 15%, rgba(0,35,142,1) 46%, rgba(76,0,139,1) 62%, rgba(129,0,118,1) 95%)',
+		// background: "linear-gradient(135deg, rgba(255,0,0,1) 0%, rgba(255,173,0,1) 14%, rgba(247,255,0,1) 28%, rgba(16,255,0,1) 40%, rgba(0,255,199,1) 52%, rgba(0,63,255,1) 66%, rgba(139,0,255,1) 83%, rgba(255,0,234,1) 95%)",
+		// background: "linear-gradient(135deg, rgba(125,0,0,1) 0%, rgba(152,103,0,1) 14%, rgba(156,161,0,1) 28%, rgba(9,145,0,1) 40%, rgba(0,154,120,1) 52%, rgba(0,35,142,1) 66%, rgba(76,0,139,1) 83%, rgba(129,0,118,1) 95%)",
+		boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+		borderRadius: '20px',
+		padding: '25px',
+		marginBottom: '25px'
+	};
+
 	return (
 		<BlockContainer class="main_container">
 			<FlexContainer h_center>
@@ -48,28 +60,95 @@ const HomePage = (props) => {
 					Welcome to Glow LEDs
 				</Title>
 			</FlexContainer>
-			<FlexContainer h_center>
-				<Title class="h2_title" styles={{ fontSize: 30 }}>
-					Introducing Diffuser Caps
-				</Title>
-			</FlexContainer>
-			<p className="p_descriptions" style={{ textAlign: 'center' }}>
-				Custom 3D Printed Caps that go over the Diffuser and Glove to bring your light shows to another
-				dimmension
-			</p>
+			<div style={div_styles}>
+				<FlexContainer h_center>
+					<Title class="h2_title" styles={{ fontSize: '30px' }}>
+						Diffuser Caps
+					</Title>
+				</FlexContainer>
+				<FlexContainer h_center styles={video_wrapper}>
+					<video id="caps_vid" style={homepage_video} controls>
+						<source
+							src="videos/optimized_videos/Custom_Infinity_Mirror/Tri-Force-Product-Video.mp4"
+							type="video/mp4"
+						/>
+					</video>
+				</FlexContainer>
+				<p className="p_descriptions" style={{ textAlign: 'center' }}>
+					It's been too long since a truly new element has been introduced to gloving. This new technology
+					puts the designs on the outside of your glove for ultimate definition. 100% facemelt guarantee.
+				</p>
+				<FlexContainer h_center>
+					<Link to="/category/Caps">
+						<ButtonWord styles={{ fontSize: '2rem' }}>
+							<Title class="h2_title" styles={{ fontSize: 20 }}>
+								Shop Diffuser Caps
+							</Title>
+						</ButtonWord>
+					</Link>
+				</FlexContainer>
+			</div>
+			<div style={div_styles}>
+				<FlexContainer h_center>
+					<Title class="h2_title" styles={{ fontSize: '30px' }}>
+						Infinity Mirrors
+					</Title>
+				</FlexContainer>
+				<FlexContainer h_center styles={video_wrapper}>
+					<video id="caps_vid" style={homepage_video} controls>
+						<source
+							src="videos/optimized_videos/Custom_Infinity_Mirror/Tri-Force-Product-Video.mp4"
+							type="video/mp4"
+						/>
+					</video>
+				</FlexContainer>
+				<p className="p_descriptions" style={{ textAlign: 'center' }}>
+					Wanting to invoke a sense of wonder and amazement in your guests (and yourself)? Infinity mirrors
+					are the perfect addition to any chill space. Look into another dimension as vibrant LEDs go on for
+					miles of rainbow bliss. Order a custom infinity mirror to add that personal touch that will only be
+					found in your space.
+				</p>
+				<FlexContainer h_center>
+					<Link to="/category/Caps">
+						<ButtonWord styles={{ fontSize: '2rem' }}>
+							<Title class="h2_title" styles={{ fontSize: 20 }}>
+								Shop Infinity Mirrors
+							</Title>
+						</ButtonWord>
+					</Link>
+				</FlexContainer>
+			</div>
+			<div style={div_styles}>
+				<FlexContainer h_center>
+					<Title class="h2_title" styles={{ fontSize: '30px' }}>
+						String Lights
+					</Title>
+				</FlexContainer>
+				<FlexContainer h_center styles={video_wrapper}>
+					<video id="caps_vid" style={homepage_video} controls>
+						<source
+							src="videos/optimized_videos/Custom_Infinity_Mirror/Tri-Force-Product-Video.mp4"
+							type="video/mp4"
+						/>
+					</video>
+				</FlexContainer>
+				<p className="p_descriptions" style={{ textAlign: 'center' }}>
+					Make your space glow! Our string lights come with 14 preprogrammed patterns that will turn your
+					space into a festival. Strobes, fades, flashes, they have it all. fill your space with a swimming
+					pool of light in every color of the rainbow.
+				</p>
 
-			<Title class="h2_title" styles={{ fontSize: 20, justifyContent: 'center', textAlign: 'center' }}>
-				Watch the Video Below to See Them in Action
-			</Title>
-			<FlexContainer h_center styles={video_wrapper}>
-				<video id="caps_vid" style={homepage_video} controls>
-					<source
-						src="videos/optimized_videos/Custom_Infinity_Mirror/Tri-Force-Product-Video.mp4"
-						type="video/mp4"
-					/>
-				</video>
-			</FlexContainer>
-			<FlexContainer h_center>
+				<FlexContainer h_center>
+					<Link to="/category/Caps">
+						<ButtonWord styles={{ fontSize: '2rem' }}>
+							<Title class="h2_title" styles={{ fontSize: 20 }}>
+								Shop String Lights
+							</Title>
+						</ButtonWord>
+					</Link>
+				</FlexContainer>
+			</div>
+			{/* <FlexContainer h_center>
 				<Link to="/category/Caps">
 					<ButtonWord styles={{ fontSize: '2rem', margin: 20 }}>
 						<Title class="h2_title" styles={{ fontSize: 30 }}>
@@ -115,7 +194,7 @@ const HomePage = (props) => {
 						/>
 					</FlexContainer>
 				</Link>
-			</FlexContainer>
+			</FlexContainer> */}
 		</BlockContainer>
 	);
 };
