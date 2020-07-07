e7259434b44ccb3c9343a6fbc5e675ba342e7034
Kurt LaVacque <lavacquek@icloud.com>
2020-07-06 20:06:57 -0500

    Fixed the isAdmin Error on the Admin Route

--- client/src/components/AdminRoute.jsx
+++ client/src/components/AdminRoute.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 // import { isLogin } from '../utils';
@@ -6,15 +6,20 @@ import { useSelector } from 'react-redux';
 const AdminRoute = ({ component: Component, ...rest }) => {
 	const userLogin = useSelector((state) => state.userLogin);
 	const { userInfo } = userLogin;
-	console.log({ AdminRoute: userInfo.isAdmin });
+	// console.log({ AdminRoute: userInfo.isAdmin });
+
+	const [ admin, setAdmin ] = useState(false);
+	useEffect(() => {
+		if (userInfo) {
+			setAdmin(true);
+		}
+		return () => {};
+	}, []);
 
 	return (
 		// Show the component only when the user is logged in
 		// Otherwise, redirect the user to /signin page
-		<Route
-			{...rest}
-			render={(props) => (userInfo.isAdmin ? <Component {...props} /> : <Redirect to="/allproducts" />)}
-		/>
+		<Route {...rest} render={(props) => (admin ? <Component {...props} /> : <Redirect to="/allproducts" />)} />
 	);
 };
 
