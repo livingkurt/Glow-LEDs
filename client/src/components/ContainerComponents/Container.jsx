6895a1c2e94770c26daefb085d61aa6bf6b7bfd3
Kurt LaVacque <lavacquek@icloud.com>
2020-07-09 11:24:38 -0500

    Installed Node Sass

--- client/src/components/ContainerComponents/Container.jsx
+++ client/src/components/ContainerComponents/Container.jsx
@@ -2,16 +2,8 @@
 import React from 'react';
 
 const Container = (props) => {
-	const container_styles = {
-		// display: "grid",
-		// gridTemplateAreas: "\"header\" \"main\" \"footer\"",
-		// gridTemplateColumns: "1fr",
-		// gridTemplateRows: "16rem 1fr 5rem",
-		// height: "100%"
-	};
-
 	return (
-		<div style={{ ...container_styles, ...props.styles }} className="fade_in">
+		<div style={props.styles} className="fade_in">
 			{props.children}
 		</div>
 	);
