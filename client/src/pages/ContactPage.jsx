29758a73bdc042609cea928af55647d194b5cfc8
Kurt LaVacque <lavacquek@icloud.com>
2020-07-10 22:00:13 -0500

    Saved

--- client/src/pages/ContactPage.jsx
+++ client/src/pages/ContactPage.jsx
@@ -63,8 +63,8 @@ const ContactPage = () => {
 			<FlexContainer h_center>
 				{loading && (
 					<FlexContainer h_center column>
-						<img src="loading.gif" className="loading_gif" alt="loading" />
 						<img src="loading_overlay.png" className="loading_png" alt="loading" />
+						<img src="loading.gif" className="loading_gif" alt="loading" />
 						<h3 style={{ textAlign: 'center' }}>If pages doesn't show in 5 seconds, refresh the page.</h3>
 					</FlexContainer>
 				)}
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -66,8 +66,8 @@ const RegisterPage = (props) => {
 						<FlexContainer h_center>
 							{loading && (
 								<FlexContainer h_center column>
-									<img src="loading.gif" className="loading_gif" alt="loading" />
 									<img src="loading_overlay.png" className="loading_png" alt="loading" />
+									<img src="loading.gif" className="loading_gif" alt="loading" />
 									<h3 style={{ textAlign: 'center' }}>
 										If pages doesn't show in 5 seconds, refresh the page.
 									</h3>
