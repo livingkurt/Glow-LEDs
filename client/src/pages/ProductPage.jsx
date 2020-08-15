dfc3b62275fa67eea77829b7ef110f8042c9aee9
Kurt LaVacque <lavacquek@icloud.com>
2020-08-15 10:02:37 -0500

    Added the meta tags in where the other data is being waited for

--- client/src/pages/ProductPage.jsx
+++ client/src/pages/ProductPage.jsx
@@ -40,59 +40,6 @@ const ProductPage = (props) => {
 
 	return (
 		<FlexContainer column>
-			<MetaTags>
-				<title>{loading ? 'Product' : product.name} | Glow LEDs</title>
-				<meta
-					name="description"
-					content="Glow LEDs offers a full selection of hand made LED products and accessories that are made to light up your world."
-				/>
-				<meta property="og:title" content="Products | Glow LEDs" />
-				<meta
-					property="og:description"
-					content="Glow LEDs offers a full selection of hand made LED products and accessories that are made to light up your world."
-				/>
-				{/* <meta
-					property="og:image"
-					content={
-						'https://www.glow-leds.com' + loading ? (
-							'/images/optimized_images/logo_images/glow_leds_link_logo_optimized.png'
-						) : (
-							product.display_image
-						)
-					}
-				/>
-				/> */}
-				<meta property="og:image" content={'https://www.glow-leds.com' + product.display_image} />
-				{/* <meta
-					property="og:image:secure_url"
-					content={
-						'https://www.glow-leds.com' + loading ? (
-							'/images/optimized_images/logo_images/glow_leds_link_logo_optimized.png'
-						) : (
-							product.display_image
-						)
-					}
-				/> */}
-				<meta property="og:image:secure_url" content={'https://www.glow-leds.com' + product.display_image} />
-				<meta property="og:url" content="https://www.glow-leds.com" />
-				<meta name="twitter:card" content="summary" />
-				<meta name="twitter:title" content="Products | Glow LEDs" />
-				<meta
-					name="twitter:description"
-					content="Glow LEDs offers a full selection of hand made LED products and accessories that are made to light up your world."
-				/>
-				<meta name="twitter:image" content={'https://www.glow-leds.com' + product.display_image} />
-				{/* <meta
-					name="twitter:image"
-					content={
-						'https://www.glow-leds.com' + loading ? (
-							'/images/optimized_images/logo_images/glow_leds_link_logo_optimized.png'
-						) : (
-							product.display_image
-						)
-					}
-				/> */}
-			</MetaTags>
 			<div className="back-to-result">
 				<FlexContainer h_between>
 					<FlexContainer styles={{ marginBottom: 10 }}>
@@ -126,6 +73,62 @@ const ProductPage = (props) => {
 			<Loading loading={loading} error={error}>
 				{product && (
 					<FlexContainer column>
+						<MetaTags>
+							<title>{loading ? 'Product' : product.name} | Glow LEDs</title>
+							<meta
+								name="description"
+								content="Glow LEDs offers a full selection of hand made LED products and accessories that are made to light up your world."
+							/>
+							<meta property="og:title" content="Products | Glow LEDs" />
+							<meta
+								property="og:description"
+								content="Glow LEDs offers a full selection of hand made LED products and accessories that are made to light up your world."
+							/>
+							{/* <meta
+					property="og:image"
+					content={
+						'https://www.glow-leds.com' + loading ? (
+							'/images/optimized_images/logo_images/glow_leds_link_logo_optimized.png'
+						) : (
+							product.display_image
+						)
+					}
+				/>
+				/> */}
+							<meta property="og:image" content={'https://www.glow-leds.com' + product.display_image} />
+							{/* <meta
+					property="og:image:secure_url"
+					content={
+						'https://www.glow-leds.com' + loading ? (
+							'/images/optimized_images/logo_images/glow_leds_link_logo_optimized.png'
+						) : (
+							product.display_image
+						)
+					}
+				/> */}
+							<meta
+								property="og:image:secure_url"
+								content={'https://www.glow-leds.com' + product.display_image}
+							/>
+							<meta property="og:url" content="https://www.glow-leds.com" />
+							<meta name="twitter:card" content="summary" />
+							<meta name="twitter:title" content="Products | Glow LEDs" />
+							<meta
+								name="twitter:description"
+								content="Glow LEDs offers a full selection of hand made LED products and accessories that are made to light up your world."
+							/>
+							<meta name="twitter:image" content={'https://www.glow-leds.com' + product.display_image} />
+							{/* <meta
+					name="twitter:image"
+					content={
+						'https://www.glow-leds.com' + loading ? (
+							'/images/optimized_images/logo_images/glow_leds_link_logo_optimized.png'
+						) : (
+							product.display_image
+						)
+					}
+				/> */}
+						</MetaTags>
 						<div className="details">
 							<FlexContainer column>
 								<h1 class="product_title_top" style={{ display: 'none' }}>
