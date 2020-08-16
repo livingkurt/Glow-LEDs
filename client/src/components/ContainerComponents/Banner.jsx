c9607daf86562a7231b342eba8db04ea00e9a474
Kurt LaVacque <lavacquek@icloud.com>
2020-08-16 17:33:28 -0500

    Made the submit content mobile friendly

--- client/src/components/ContainerComponents/Banner.jsx
+++ client/src/components/ContainerComponents/Banner.jsx
@@ -10,20 +10,36 @@ const Banner = (props) => {
 			<div
 				style={{
 					maxWidth: '1125px',
-					width: '1125px',
+					// width: '1125px',
 					margin: 'auto',
 					display: 'flex',
 					justifyContent: 'space-between'
 				}}
 			>
-				<div>
+				<div
+					style={{ marginLeft: '10px', display: 'none', textAlign: 'center', width: '100%' }}
+					className="small_screen"
+				>
+					<Link to="/pages/contact/submit_content_to_be_featured">
+						<button className="banner-button">Submit content to be featured on our pages</button>
+					</Link>
+				</div>
+				<div
+					style={{ marginLeft: '10px', display: 'none', textAlign: 'center', width: '100%' }}
+					className="smaller_screen"
+				>
+					<Link to="/pages/contact/submit_content_to_be_featured">
+						<button className="banner-button">Submit content to be featured</button>
+					</Link>
+				</div>
+				<div style={{ marginLeft: '10px' }} className="big_screen">
 					<label>Submit content to be featured on our pages </label>
 					<i style={{ margin: '0 10px ' }} class="fas fa-arrow-right" />
 					<Link to="/pages/contact/submit_content_to_be_featured">
 						<button className="banner-button">Send us a message!</button>
 					</Link>
 				</div>
-				<FlexContainer styles={{ marginTop: '3px' }}>
+				<FlexContainer styles={{ marginTop: '3px' }} class="big_screen">
 					<div style={{ marginLeft: '10px' }}>
 						<a href="https://www.facebook.com/Glow-LEDscom-100365571740684" target="_blank">
 							<i class="fab fa-facebook zoom" />
