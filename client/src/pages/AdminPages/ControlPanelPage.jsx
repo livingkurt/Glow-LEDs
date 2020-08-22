6039768306a9269c67865ab1e6dc7983177d7abb
Kurt LaVacque <lavacquek@icloud.com>
2020-08-22 17:46:44 -0500

    Added a amount of each document into the control panel

--- client/src/pages/AdminPages/ControlPanelPage.jsx
+++ client/src/pages/AdminPages/ControlPanelPage.jsx
@@ -7,6 +7,7 @@ import { Loading } from '../../components/UtilityComponents';
 import MetaTags from 'react-meta-tags';
 import { listOrders } from '../../actions/orderActions';
 import { listExpenses } from '../../actions/expenseActions';
+import { listUsers } from '../../actions/userActions';
 
 const colors = {
 	hidden: '#333333'
@@ -20,9 +21,17 @@ const ControlPanelPage = (props) => {
 	const orderList = useSelector((state) => state.orderList);
 	const { loading, orders, error } = orderList;
 
+	const productList = useSelector((state) => state.productList);
+	const { loading: loading_products, products, error: error_products } = productList;
+
+	const userList = useSelector((state) => state.userList);
+	const { loading: loading_users, users, error: error_users } = userList;
+
 	useEffect(() => {
 		dispatch(listOrders());
 		dispatch(listExpenses());
+		dispatch(listProducts());
+		dispatch(listUsers());
 	}, []);
 
 	return (
@@ -80,6 +89,66 @@ const ControlPanelPage = (props) => {
 									${(orders.reduce((a, expense) => a + expense.totalPrice, 0) -
 										expenses.reduce((a, order) => a + order.amount, 0)).toFixed(2)}
 								</th>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			)}
+			{expenses &&
+			orders &&
+			products &&
+			users && (
+				<div className="order-list responsive_table">
+					<table className="table" style={{ width: '50%' }}>
+						<thead>
+							<tr>
+								<th>Category</th>
+								<th>Count</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr
+								style={{
+									backgroundColor: '#626262',
+									fontSize: '1.4rem',
+									height: '50px'
+								}}
+								className=""
+							>
+								<th style={{ padding: '15px' }}>Total Products</th>
+								<th style={{ padding: '15px' }}>{products.length}</th>
+							</tr>
+
+							<tr
+								style={{
+									backgroundColor: '#626262',
+									fontSize: '1.4rem',
+									height: '50px'
+								}}
+							>
+								<th style={{ padding: '15px' }}>Total Orders</th>
+								<th style={{ padding: '15px' }}>{orders.length}</th>
+							</tr>
+
+							<tr
+								style={{
+									backgroundColor: '#626262',
+									fontSize: '1.4rem',
+									height: '50px'
+								}}
+							>
+								<th style={{ padding: '15px' }}>Total Users</th>
+								<th style={{ padding: '15px' }}>{users.length}</th>
+							</tr>
+							<tr
+								style={{
+									backgroundColor: '#626262',
+									fontSize: '1.4rem',
+									height: '50px'
+								}}
+							>
+								<th style={{ padding: '15px' }}>Total Expenses</th>
+								<th style={{ padding: '15px' }}>{expenses.length}</th>
 							</tr>
 						</tbody>
 					</table>
