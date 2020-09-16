d3bee6bc7203ba5ec19d8d637e0b96708f9400f9
Kurt LaVacque <lavacquek@icloud.com>
2020-09-15 19:10:56 -0500

    Added a bunch of pictures for the new products

--- client/src/sitemap/sitemap-generator.js
+++ client/src/sitemap/sitemap-generator.js
@@ -4,9 +4,46 @@ require('babel-register')({
 
 const router = require('./sitemap-routes').default;
 const Sitemap = require('react-router-sitemap').default;
+const fetch = require('node-fetch');
+const API = 'http://localhost:5000';
 
-function generateSitemap() {
-	return new Sitemap(router).build('https://www.glow-leds.com').save('./public/sitemap.xml');
+async function generateSitemap() {
+	let products_res = await fetch(API + '/api/products/');
+	let products = await products_res.json();
+	let categories_res = await fetch(API + '/api/sitemap/categories');
+	let categories = await categories_res.json();
+
+	let categoryMap = categories.map((cat) => {
+		return { category: cat.path_name };
+	});
+
+	let productMap = categories.map((cat) => {
+		return { category: cat.path_name };
+	});
+
+	productMap = productMap.map((obj) => {
+		obj.product = [];
+		return obj;
+	});
+
+	products.forEach((product) => {
+		product.categories.forEach((cat) => {
+			productMap = productMap.map((obj) => {
+				if (obj.category === cat.path_name) {
+					obj.product.push(product.path_name);
+				}
+				return obj;
+			});
+		});
+	});
+
+	productMap = productMap.filter((obj) => obj.product.length !== 0);
+
+	const paramsConfig = {
+		'/shop/:category/:product': productMap,
+		'/shop/:category': categoryMap
+	};
+	return new Sitemap(router).applyParams(paramsConfig).build('http://my-site.ru').save('public/sitemap.xml');
 }
 
 generateSitemap();
--- client/src/sitemap/sitemap-routes.js
+++ client/src/sitemap/sitemap-routes.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Switch, Route } from 'react-router';
 
 export default (
-	<Route>
+	// Switch is added in v4 react-router
+	<Switch>
 		<Route path="/account/login" />
 		<Route path="/account/verified/:id" />
 		<Route path="/account/checkemail" />
+		<Route path="/account/emailsent" />
 		<Route path="/account/changepassword" />
 		<Route path="/account/register" />
 		<Route path="/account/passwordreset" />
 		<Route path="/account/resetpassword/:id" />
 		<Route path="/checkout/cart/:pathname?" />
-		<Route path="/collections/all/products" />
+		<Route exact path="/collections/all/products" />
 		<Route path="/collections/all/products/category/:category?" />
 		<Route path="/collections/all/products/:pathname" />
-		<Route path="/pages/contact/:reason?" />
-		<Route path="/pages/terms" />
-		<Route path="/pages/about" />
-		<Route path="/pages/faq" />
-		<Route path="/" />
-	</Route>
+		<Route exact path="/pages/contact/:reason?" />
+		<Route exact path="/pages/terms" />
+		<Route exact path="/pages/about" />
+		<Route exact path="/pages/faq" />
+		<Route exact path="/pages/sitemap" />
+		<Route exact path="/pages/featured" />
+		<Route exact path="/" />
+		<Route />
+	</Switch>
 );
