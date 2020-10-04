7484363b1b764fc96e440a0c5bbe7e0577ec4c6f
Kurt LaVacque <lavacquek@icloud.com>
2020-10-04 01:54:54 -0500

    Made it so the sitemap generates in the correct place

--- client/src/sitemap/sitemap-generator.js
+++ client/src/sitemap/sitemap-generator.js
@@ -1,53 +1,3 @@
-// require('babel-register')({
-// 	presets: [ 'es2015', 'react' ]
-// });
-
-// const router = require('./sitemap-routes').default;
-// const Sitemap = require('react-router-sitemap').default;
-// const fetch = require('node-fetch');
-// const API = 'http://localhost:5000';
-
-// async function generateSitemap() {
-// 	let products_res = await fetch(API + '/api/products/');
-// 	let products = await products_res.json();
-// 	let categories_res = await fetch(API + '/api/sitemap/categories');
-// 	let categories = await categories_res.json();
-
-// 	let categoryMap = categories.map((cat) => {
-// 		return { category: cat.path_name };
-// 	});
-
-// 	let productMap = categories.map((cat) => {
-// 		return { category: cat.path_name };
-// 	});
-
-// 	productMap = productMap.map((obj) => {
-// 		obj.product = [];
-// 		return obj;
-// 	});
-
-// 	products.forEach((product) => {
-// 		product.categories.forEach((cat) => {
-// 			productMap = productMap.map((obj) => {
-// 				if (obj.category === cat.path_name) {
-// 					obj.product.push(product.path_name);
-// 				}
-// 				return obj;
-// 			});
-// 		});
-// 	});
-
-// 	productMap = productMap.filter((obj) => obj.product.length !== 0);
-
-// 	const paramsConfig = {
-// 		'/shop/:category/:product': productMap,
-// 		'/shop/:category': categoryMap
-// 	};
-// return new Sitemap(router).applyParams(paramsConfig).build('http://my-site.ru').save('public/sitemap.xml');
-// }
-
-// generateSitemap();
-
 require('babel-register')({
 	presets: [ 'es2015', 'react' ]
 });
@@ -65,7 +15,7 @@ async function generateSitemap() {
 	let subcategories_res = await fetch(API + '/api/products/subcategories');
 	let subcategories = await subcategories_res.json();
 
-	let productMap = products.map((product) => {
+	let productMap = products.filter((product) => product.hidden === false).map((product) => {
 		return { pathname: product.pathname };
 	});
 	let categoryMap = categories.map((category) => {
@@ -90,37 +40,6 @@ async function generateSitemap() {
 	console.log({ categoryMap });
 	console.log({ subcategoryMap });
 
-	// 	let categoryMap = categories.map((category) => {
-	// 	return { category: category.pathname };
-	// });
-
-	// let subcategoryMap = subcategories.map((cat) => {
-	// 	return { subcategory: cat.pathname };
-	// });
-
-	// productMap = productMap.map((obj) => {
-	// 	obj.product = [];
-	// 	return obj;
-	// });
-
-	// products.forEach((product) => {
-	// 	product.categories.forEach((cat) => {
-	// 		productMap = productMap.map((obj) => {
-	// 			if (obj.category === cat.pathname) {
-	// 				obj.product.push(product.pathname);
-	// 			}
-	// 			return obj;
-	// 		});
-	// 	});
-	// });
-
-	// productMap = productMap.map((obj) => {
-	// 	obj.product = [];
-	// 	return obj;
-	// });
-
-	// productMap = productMap.filter((obj) => obj.product.length !== 0);
-
 	const paramsConfig = {
 		'/collections/all/products/category/accessories/subcategory/:subcategory': subcategoryMap,
 		'/collections/all/products/category/diffuser_caps/subcategory/:subcategory': subcategoryMap,
@@ -132,7 +51,11 @@ async function generateSitemap() {
 		'/collections/all/products/:pathname': productMap,
 		'/pages/contact/:reason': contact_reason
 	};
-	return new Sitemap(router).applyParams(paramsConfig).build('https://glow-leds.com').save('public/sitemap.xml');
+	return new Sitemap(router)
+		.applyParams(paramsConfig)
+		.build('https://glow-leds.com')
+		.save('../sitemap.xml')
+		.save('public/sitemap.xml');
 }
 
 generateSitemap();
