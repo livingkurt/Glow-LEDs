3e578ade074f0a0e261585eae631e693c0de4270
Kurt LaVacque <lavacquek@icloud.com>
2020-10-21 18:44:00 -0500

    Fixed teh typescript error

--- email_templates_2/App.ts
+++ email_templates_2/App.ts
@@ -1,4 +1,4 @@
-export default (props) => {
+export default (props: any) => {
 	return `
   <body style="padding:0;margin:0">
   <div>
@@ -17,6 +17,30 @@ export default (props) => {
       Log Error: ${props.title}</h4>
   </div>
   ${props.body}
+  <div style="background-color:#333333;padding:20px">
+        <div
+          style="margin-left:10px;display:flex;justify-content:space-between;max-width:250px;width:100%;margin:0 auto;color:white">
+          <div style="font-size:30px;color:white"><a rel="noopener noreferrer"
+              href="https://www.facebook.com/Glow-LEDscom-100365571740684" target="_blank"><i
+                class="fab fa-facebook zoom" style="color:white"></i></a></div>
+          <div style="font-size:30px;color:white"><a rel="noopener noreferrer"
+              href="https://www.instagram.com/glow_leds/" target="_blank"><i class="fab fa-instagram zoom"
+                style="color:white"></i></a></div>
+          <div style="font-size:30px;color:white"><a rel="noopener noreferrer"
+              href="https://www.youtube.com/channel/UCm_gDyTIy7d0oR9LeowPkYw" target="_blank"><i
+                class="fab fa-youtube zoom" style="color:white"></i></a></div>
+          <div style="font-size:30px;color:white"><a rel="noopener noreferrer" href="https://soundcloud.com/ntre/tracks"
+              target="_blank"><i class="fab fa-soundcloud" style="color:white"></i></a></div>
+        </div>
+        <div style="border-bottom:1px white solid;max-width:600px;width:100%;margin:15px auto"></div>
+        <p style="text-align:center;font-size:14px;color:white">Our mailing address is: <br />404 Kenniston Dr Apt D,
+          Austin, TX 78752 </p>
+        <p style="text-align:center;font-size:14px;color:white">Want to change how you receive these emails? <br /> You
+          can <a rel="noopener noreferrer"
+            href="https://www.glow-leds.com/account/login?redirect=/secure/account/editprofile" target="_blank"
+            style="text-decoration:underline;color:white">update your preferences</a> or <a rel="noopener noreferrer"
+            href="https://www.glow-leds.com/account/login?redirect=/secure/account/editprofile" target="_blank"
+            style="text-decoration:underline;color:white">unsubscribe </a>from this list.</p>
   </div>
   </div>
 </div>
