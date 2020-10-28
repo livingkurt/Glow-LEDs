6a37295aa6b525ff86303fe0ea8634b8557762d4
Kurt LaVacque <lavacquek@icloud.com>
2020-10-28 10:17:01 -0500

    Removed pics from 404 page

--- client/src/pages/PublicPages/Four04Page.jsx
+++ client/src/pages/PublicPages/Four04Page.jsx
@@ -38,36 +38,6 @@ const Four04Page = (props) => {
 			</MetaTags>
 			<h1 styles={{ margin: '20px auto' }}>404 Page Not Found</h1>
 			<label>Sorry About that</label>
-			<a href="https://imgbox.com/VFhZo1GK" target="_blank">
-				<img src="https://thumbs2.imgbox.com/c7/b2/VFhZo1GK_t.jpg" alt="image host" />
-			</a>{' '}
-			<a href="https://imgbox.com/x1CUK8E7" target="_blank">
-				<img src="https://thumbs2.imgbox.com/7a/6e/x1CUK8E7_t.jpg" alt="image host" />
-			</a>{' '}
-			<a href="https://imgbox.com/zj1QkTQ8" target="_blank">
-				<img src="https://thumbs2.imgbox.com/a3/96/zj1QkTQ8_t.jpg" alt="image host" />
-			</a>{' '}
-			<a href="https://imgbox.com/XxHQutVf" target="_blank">
-				<img src="https://thumbs2.imgbox.com/5d/64/XxHQutVf_t.jpg" alt="image host" />
-			</a>{' '}
-			<a href="https://imgbox.com/hJivf8jc" target="_blank">
-				<img src="https://thumbs2.imgbox.com/e5/c9/hJivf8jc_t.jpg" alt="image host" />
-			</a>{' '}
-			<a href="https://imgbox.com/nWARUCDn" target="_blank">
-				<img src="https://thumbs2.imgbox.com/dd/7a/nWARUCDn_t.jpg" alt="image host" />
-			</a>{' '}
-			<a href="https://imgbox.com/aXTp5Y68" target="_blank">
-				<img src="https://thumbs2.imgbox.com/84/1f/aXTp5Y68_t.jpg" alt="image host" />
-			</a>{' '}
-			<a href="https://imgbox.com/dcUQxok0" target="_blank">
-				<img src="https://thumbs2.imgbox.com/05/fe/dcUQxok0_t.jpg" alt="image host" />
-			</a>{' '}
-			<a href="https://imgbox.com/uwewk4AX" target="_blank">
-				<img src="https://thumbs2.imgbox.com/fd/f5/uwewk4AX_t.jpg" alt="image host" />
-			</a>{' '}
-			<a href="https://imgbox.com/JpJyUVn2" target="_blank">
-				<img src="https://thumbs2.imgbox.com/b4/c1/JpJyUVn2_t.jpg" alt="image host" />
-			</a>
 		</FlexContainer>
 	);
 };
