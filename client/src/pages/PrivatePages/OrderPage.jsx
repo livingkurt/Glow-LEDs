5fad19a89fb43979092dc3b3d9a20cf98eb42fd6
Kurt LaVacque <lavacquek@icloud.com>
2020-11-16 20:52:43 -0600

    Fixed the way the cart Items look on the place order page

--- client/src/pages/PrivatePages/OrderPage.jsx
+++ client/src/pages/PrivatePages/OrderPage.jsx
@@ -156,6 +156,55 @@ const OrderPage = (props) => {
 		return result;
 	};
 
+	const [ show_color, set_show_color ] = useState(false);
+
+	const handleWindowResize = (width) => {
+		if (width > 0 && width < 407) {
+			set_show_color(true);
+		} else {
+			set_show_color(false);
+		}
+	};
+
+	const getWidth = () => window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+	function useCurrentWidth() {
+		// save current window width in the state object
+		let [ width, setWidth ] = useState(getWidth());
+		// in this case useEffect will execute only once because
+		// it does not have any dependencies.
+		useEffect(() => {
+			// timeoutId for debounce mechanism
+			let timeoutId = null;
+			let numberId = null;
+			const resizeListener = () => {
+				// prevent execution of previous setTimeout
+				clearTimeout(timeoutId);
+				clearTimeout(numberId);
+				// change width from the state object after 150 milliseconds
+				timeoutId = setTimeout(() => setWidth(getWidth()), 150);
+				numberId = setTimeout(() => handleWindowResize(getWidth()), 150);
+			};
+			// handleWindowResize(width);
+
+			// set resize listener
+			window.addEventListener('resize', resizeListener);
+			// clean up function
+			return () => {
+				// remove resize listener
+				window.removeEventListener('resize', resizeListener);
+			};
+		}, []);
+		return width;
+	}
+
+	let width = useCurrentWidth();
+
+	useEffect(() => {
+		handleWindowResize(getWidth());
+		return () => {};
+	}, []);
+
 	return loading ? (
 		<div className="column jc-c">
 			<h2 style={{ textAlign: 'center' }}>Loading...</h2>
@@ -193,7 +242,7 @@ const OrderPage = (props) => {
 			</div>
 
 			<LoadingPayments loading={payment_loading} error={errorPay} />
-			<div className="placeorder">
+			<div className="placeorder br-20px" style={{ backgroundColor: show_color && determine_color(order) }}>
 				<div className="placeorder-info">
 					<div style={{ backgroundColor: determine_color(order) }}>
 						{order.isRefunded && (
