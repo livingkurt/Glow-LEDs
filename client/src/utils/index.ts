c77e9c2ceb46cd356334668f42e08dea2aa2bfc6
Kurt LaVacque <lavacquek@icloud.com>
2020-12-03 16:43:40 -0600

    Removed the google sheets reference

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -3,4 +3,3 @@ export { default as API_Glow_Control } from './API_Glow_Control';
 export { default as API_Revenue } from './API_Revenue';
 export { default as API_Products } from './API_Products';
 export { default as API_External } from './API_External';
-export { default as google_sheets } from './google_sheets';
