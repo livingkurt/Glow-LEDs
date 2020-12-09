212a8b51ba064d0f4369b7e7b382e4dcb9fda59d
Kurt LaVacque <lavacquek@icloud.com>
2020-12-09 17:14:14 -0600

    Finished the Footer

--- client/src/components/ContainerComponents/Footer.jsx
+++ client/src/components/ContainerComponents/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
 	return (
-		<footer className="ta-c w-100per mt-100px h-500px" style={{ backgroundColor: '#333333' }}>
+		<footer className="ta-c w-100per mt-5rem h-400px" style={{ backgroundColor: '#333333' }}>
 			<div className="footer-image">
 				<Link to="/">
 					<div className="">
@@ -15,7 +15,7 @@ const Footer = () => {
 						/>
 					</div>
 				</Link>
-				<div className="ml-8px wrap jc-c ">
+				<div className="mt-2rem wrap jc-c ">
 					<div className="ml-10px fs-30px jc-b w-100per max-w-500px">
 						<div className="ml-10px fs-40px">
 							<a
@@ -48,7 +48,7 @@ const Footer = () => {
 					</div>
 				</div>
 
-				<div className="jc-b ai-c w-100per p-10px">
+				<div className="jc-b ai-c w-100per p-10px footer_buttons">
 					<div className="jc-a w-100per">
 						<div>
 							<h2 className="ta-l">
@@ -111,6 +111,71 @@ const Footer = () => {
 								</li>
 							</ul>
 						</div>
+					</div>
+				</div>
+				<div className="jc-b ai-c w-100per p-10px footer_small none">
+					<div className="jc-a w-100per">
+						<div>
+							<h2 className="ta-l">
+								<Link to="/collections/all/products">Products</Link>
+							</h2>
+							<ul className="lst-none">
+								<li className="ta-l mv-2rem">
+									<Link to="/collections/all/products/category/glowskins">Glowskins</Link>
+								</li>
+								<li className="ta-l mv-2rem">
+									<Link to="/collections/all/products/category/frosted_diffusers">
+										Frosted Diffusers
+									</Link>
+								</li>
+								<li className="ta-l mv-2rem">
+									<Link to="/collections/all/products/category/diffuser_caps">Diffuser Caps</Link>
+								</li>
+								<li className="ta-l mv-2rem">
+									<Link to="/collections/all/products/category/mega_diffuser_caps">
+										Mega Diffusers Caps
+									</Link>
+								</li>
+								<li className="ta-l mv-2rem">
+									<Link to="/collections/all/products/category/accessories">Accessories</Link>
+								</li>
+							</ul>
+						</div>
+						<div>
+							<h2 className="ta-l">
+								<Link to="/pages/menu/community">Community</Link>
+							</h2>
+							<ul className="lst-none">
+								<li className="ta-l mv-2rem">
+									<Link to="/pages/announcments">Announcments</Link>
+								</li>
+								<li className="ta-l mv-2rem">
+									<Link to="/pages/menu/featured">Featured</Link>
+								</li>
+								<li className="ta-l mv-2rem">
+									<Link to="/pages/music">Music</Link>
+								</li>
+							</ul>
+						</div>
+						{/* <div>
+							<h2 className="ta-l">
+								<Link to="/collections/all/products">Services</Link>
+							</h2>
+							<ul className="lst-none">
+								<li className="ta-l mv-2rem">
+									<Link to="/secure/account/profile">MY ACCOUNT</Link>
+								</li>
+								<li className="ta-l mv-2rem">
+									<Link to="/account/login">LOGIN</Link>
+								</li>
+								<li className="ta-l mv-2rem">
+									<Link to="/pages/terms">Terms</Link>
+								</li>
+								<li className="ta-l mv-2rem">
+									<Link to="/pages/sitemap">Sitemap</Link>
+								</li>
+							</ul>
+						</div> */}
 					</div>
 				</div>
 			</div>
