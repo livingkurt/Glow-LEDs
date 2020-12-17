6005ab5d325e5dd7f24c000c81b06fd28af306a9
Kurt LaVacque <lavacquek@icloud.com>
2020-12-16 22:15:13 -0600

    Fixed the warning for the checkouthandler function

--- client/src/pages/PublicPages/CartPage.jsx
+++ client/src/pages/PublicPages/CartPage.jsx
@@ -32,16 +32,14 @@ const CartPage = (props) => {
 
 	const decide_warning = () => {
 		if (new Date() > new Date('2020-12-18') && new Date() < new Date('2021-01-02')) {
-			const confirm = window.confirm('Orders will not be shipped until after January 2nd 2021');
+			const confirm = window.confirm(
+				'Glow LEDs is Out of Office, Orders will not be shipped until after January 2nd 2021'
+			);
 			if (confirm) {
 				checkoutHandler();
 			}
 		} else {
-			if (cartItems.length === 0) {
-				set_no_items_in_cart('Cannot proceed to checkout without any items in cart');
-			} else {
-				checkoutHandler();
-			}
+			checkoutHandler();
 		}
 	};
 	const no_adapters_warning = () => {
