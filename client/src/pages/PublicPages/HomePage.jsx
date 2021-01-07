664b643f6a467443c943051c45044f46363801ec
Kurt LaVacque <lavacquek@icloud.com>
2021-01-07 14:17:33 -0600

    Added more color to the home page

--- client/src/pages/PublicPages/HomePage.jsx
+++ client/src/pages/PublicPages/HomePage.jsx
@@ -38,6 +38,7 @@ const HomePage = (props) => {
 			name: 'Glowskins',
 			category: 'glowskins',
 			video: '3Yk0QOMBlAo',
+			color: '#8e4a4a',
 			description:
 				'What makes Glowskins special? Glowskins are a Casing and Diffuser all in one! Place your entire chip inside and create a glow throughout the whole casing! This differs from our Frosted Diffusers which create a glow only around the bulb. There are 3 unique sizes, each designed for Coffin, Nano or Coin chip microlights. Glowskins are made with semi-flexible TPU plastic so your fingers will always feel comfortable! They do not inhibit access to your microlight button for mode switching. Our light and streamline design makes your fingers feel weightless. Smooth finish for easy removal from whites.'
 		},
@@ -45,6 +46,7 @@ const HomePage = (props) => {
 			name: 'Diffuser Caps',
 			category: 'diffuser_caps',
 			video: '0b1cn_3EczE',
+			color: '#b1832f',
 			description:
 				'Take your light shows to a new dimension with Diffuser Caps! This new gloving tech puts patterns and designs on the outside of your glove to add a mesmerizing and unique effect to your lightshows. These Diffuser Adapters are the secret to the technology. Simply place the Diffuser Adapters (sold separately) on your microlight inside of the glove and then twist on the cap to the Diffuser Adapter from the outside of the glove! Diffuser caps are about the size of a classic dome diffuser. 15mm in Diameter. People will be speechless at your tracers and effects! 100% facemelt guarantee. Lights not included. Patent pending. The Diffuser Caps are compatible with the Mini Diffuser Caps purchased before 12/3/20. View the graphic below for visual representation of what we'
 		},
@@ -52,6 +54,7 @@ const HomePage = (props) => {
 			name: 'Mega Diffuser Caps',
 			category: 'mega_diffuser_caps',
 			video: 'CaC-86DAql4',
+			color: '#427942',
 			description:
 				'Take your light shows to a new dimension with Mega Diffuser Caps! This new gloving tech puts patterns and designs on the outside of your glove to add a mesmerizing and unique effect to your lightshows. Now in Mega Size! These Mega Diffuser Caps are Just like our regular sized Diffuser Caps, but 20% bigger! 20mm in Diameter (Same size as our Mega Dome Diffusers) The mega size allows for a more intricate design that may be too complex for the regular sized diffuser caps. Simply place the Diffuser Adapters (sold separately) on your microlight like normal diffusers and the caps screw on on the outside of the glove! People will be speechless at your tracers and trails! 100% facemelt guarantee. Lights not included. Patent pending. The Mega Diffuser Caps are compatible with the Original Diffuser Caps purchased before 12/3/20. View the graphic below for visual representation of what we mean.'
 		},
@@ -59,6 +62,7 @@ const HomePage = (props) => {
 			name: 'Frosted Diffusers',
 			category: 'frosted_diffusers',
 			video: 'uY2xjrGrZd0',
+			color: '#3f3f80',
 			description:
 				'Tired of diffusers that dont actually diffuse? these frosted diffusers will give your lightshow an added smoothness and flow. these diffusers will distribute the light into an even glow without a bright center point.'
 		},
@@ -66,6 +70,7 @@ const HomePage = (props) => {
 			name: 'Glow Strings',
 			category: 'glow_strings',
 			video: 'TCArM88Ll1s',
+			color: '#6d416d',
 			description:
 				'Make your space glow! Our Glow Strings come with many preprogrammed patterns that will turn your home into a festival. Strobes, fades, flashes, they have it all. fill your universe with a swimming pool of light in every color of the rainbow. Available in 12 ft (50 LED), 23 ft (100 LED), 34 ft (150 LED), and 46 ft (200 LED) options so there’s a size for every need.'
 		}
@@ -167,7 +172,7 @@ const HomePage = (props) => {
 				{/* <h2 className="ta-c phrase_font">From a Glover that just wants the world to stay lit 🔥 </h2> */}
 				{/* <h2 className="ta-c phrase_font">Lighting up your world one LED at a time </h2> */}
 			</div>
-			<p className="p_descriptions paragraph_font ta-c">
+			<p className="p_descriptions paragraph_font ta-c home_page_description">
 				Here at Glow-LEDs.com we strive to bring as much light in to as many lives as possible. All items are
 				handmade at my home in Austin, TX and all ideas came from my own brain. Our items were dreamt up with
 				the intention of turning your home into a glowing rainbow dreamland with infinite hours of
@@ -179,7 +184,7 @@ const HomePage = (props) => {
 				{homepage_videos.map((card) => {
 					return (
 						<Link to={`/collections/all/products/category/${card.category}`}>
-							<div className="home_page_divs max-h-66rem">
+							<div className="home_page_divs max-h-66rem" style={{ backgroundColor: card.color }}>
 								<div className="jc-c">
 									<h2 className="ta-c">{card.name}</h2>
 								</div>
@@ -217,11 +222,11 @@ const HomePage = (props) => {
 				{homepage_videos.map((card) => {
 					return (
 						<Link to={`/collections/all/products/category/${card.category}`}>
-							<div className="home_page_divs">
+							<div className="home_page_divs" style={{ backgroundColor: card.color }}>
 								<div className="jc-c">
 									<h2 className="ta-c">{card.name}</h2>
 								</div>
-								<div className="jc-c pos-rel">
+								<div className="jc-c pos-rel mb-2rem">
 									<div className="iframe-container">
 										<iframe
 											title={`${card.name} Promo Video`}
@@ -233,7 +238,9 @@ const HomePage = (props) => {
 										/>
 									</div>
 								</div>
-								<p className="p_descriptions paragraph_font">{card.description}</p>
+								<p className="p_descriptions paragraph_font home_page_description">
+									{card.description}
+								</p>
 								<div className="jc-c">
 									<Link
 										className="w-100per"
