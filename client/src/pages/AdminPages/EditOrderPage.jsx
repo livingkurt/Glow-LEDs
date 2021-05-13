091120e0eac3b9c3126401c1aa6ca0b09b8d3f7c
Kurt LaVacque <lavacquek@icloud.com>
2021-05-12 21:35:22 -0500

    Made it so You can update the product options as well in the edit order page

--- client/src/pages/AdminPages/EditOrderPage.jsx
+++ client/src/pages/AdminPages/EditOrderPage.jsx
@@ -39,6 +39,8 @@ const EditOrderPage = (props) => {
 	const [ tracking_number, set_tracking_number ] = useState('');
 	const [ loading_checkboxes, set_loading_checkboxes ] = useState(true);
 	const [ loading_tax_rate, set_loading_tax_rate ] = useState(false);
+	// const [ product_option, set_product_option ] = useState({});
+	// const [ product_option_images, set_product_option_images ] = useState([] ]);
 
 	const history = useHistory();
 
@@ -94,9 +96,13 @@ const EditOrderPage = (props) => {
 		if (order.refundedAt) {
 			set_refundedAt(format_date(order.refundedAt));
 		}
+
 		set_order_note(order.order_note);
 		set_promo_code(order.promo_code);
 		set_tracking_number(order.tracking_number);
+		// set_product_option_images(
+		// 	product.product_options && product.product_options.map((option, index) => option.images)
+		// );
 
 		// console.log(format_date(order.isManufactured));
 	};
@@ -125,6 +131,7 @@ const EditOrderPage = (props) => {
 		set_order_note('');
 		set_promo_code('');
 		set_tracking_number('');
+		// set_product_option({});
 	};
 
 	const stableDispatch = useCallback(dispatch, []);
@@ -191,6 +198,7 @@ const EditOrderPage = (props) => {
 				order_note,
 				promo_code,
 				tracking_number
+				// product_option
 			})
 		);
 		e.target.reset();
@@ -200,7 +208,7 @@ const EditOrderPage = (props) => {
 
 	const add_order_item = (e) => {
 		e.preventDefault();
-		set_orderItems((items) => [ ...items, {} ]);
+		set_orderItems((items) => [ ...items, { product_option: {} } ]);
 	};
 
 	setTimeout(() => {
@@ -210,6 +218,8 @@ const EditOrderPage = (props) => {
 	const update_order_item = async (e, index) => {
 		const order_item = JSON.parse(e.target.value);
 		console.log({ order_item });
+		console.log({ product_options: order_item.product_options.find((option) => option.default === true) });
+		const product_option = order_item.product_options.find((option) => option.default === true);
 		let new_order_items = [ ...orderItems ];
 		new_order_items[index] = {
 			...new_order_items[index],
@@ -226,12 +236,22 @@ const EditOrderPage = (props) => {
 			package_length: order_item.package_length,
 			package_width: order_item.package_width,
 			package_height: order_item.package_height,
+			product_option: order_item.product_options.find((option) => option.default === true),
+			// product_option: {
+			// 	name: product_option.name,
+			// 	size: product_option.size,
+			// 	price: product_option.price,
+			// 	sale_price: product_option.sale_price,
+			// 	color: product_option.color,
+			// 	count_in_stock: product_option.count_in_stock
+			// },
 			reviewed: order_item.reviewed,
 			product: { _id: order_item._id }
 
 			// secondary_product: orderItems[index].secondary_product
 		};
 		// console.log(order_item._id);
+		set_orderItems(new_order_items);
 		set_orderItems(new_order_items);
 		console.log({ orderItems: [ ...new_order_items ] });
 		const price_items = [ ...new_order_items ].reduce((a, c) => a + c.price * c.qty, 0);
@@ -269,6 +289,68 @@ const EditOrderPage = (props) => {
 		set_loading_tax_rate(false);
 	};
 
+	// const product_option_image_display = (images, option_index) => {
+	// 	// console.log({ images });
+	// 	return (
+	// 		<div>
+	// 			<div className="row wrap">
+	// 				{images &&
+	// 					images.map((picture, index) => {
+	// 						return (
+	// 							<div className="promo_code mv-1rem jc-b max-w-46rem w-100per">
+	// 								<div className="pos-rel">
+	// 									<img
+	// 										style={{
+	// 											width: '100%',
+	// 											package_height: 'auto',
+	// 											maxWidth: '100px',
+	// 											maxHeight: '100px',
+	// 											borderRadius: '15px'
+	// 										}}
+	// 										className="mv-10px ml-10px"
+	// 										src={picture}
+	// 									/>
+	// 									<div className="ml-10px">{picture}</div>
+
+	// 									<button
+	// 										className="btn icon pos-abs right-10px top-15px"
+	// 										onClick={(e) => remove_product_option_image(index, e, option_index)}
+	// 									>
+	// 										<i className="fas fa-times" />
+	// 									</button>
+	// 									<div className="pos-abs right-40px top-15px column">
+	// 										{/* {index > 0 && (
+	// 											<button className="btn icon" onClick={(e) => move_image_up(index, e)}>
+	// 												<i className=" fas fa-sort-up" />
+	// 											</button>
+	// 										)}
+
+	// 										{index < images.length - 1 && (
+	// 											<button className="btn icon" onClick={(e) => move_image_down(index, e)}>
+	// 												<i
+	// 													style={{ '-webkitTransform': 'rotate(-180deg)' }}
+	// 													className=" fas fa-sort-up"
+	// 												/>
+	// 											</button>
+	// 										)} */}
+	// 									</div>
+	// 								</div>
+	// 							</div>
+	// 						);
+	// 					})}
+	// 			</div>
+	// 			<div className="promo_code mv-1rem jc-b max-w-46rem w-100per fs-14px">
+	// 				<p>
+	// 					{images &&
+	// 						images.map((picture, index) => {
+	// 							return `${picture}\n`;
+	// 						})}
+	// 				</p>
+	// 			</div>
+	// 		</div>
+	// 	);
+	// };
+
 	const update_order_item_property = (value, field_name, index) => {
 		console.log({ value, field_name, index });
 		let new_order_items = [ ...orderItems ];
@@ -279,6 +361,95 @@ const EditOrderPage = (props) => {
 		set_orderItems(new_order_items);
 		console.log({ orderItems });
 	};
+
+	// const add_option_image = (e, index) => {
+	// 	e.preventDefault();
+	// 	// console.log({ image });
+	// 	if (image.indexOf(' ') >= 0) {
+	// 		console.log('indexOf');
+	// 		image.split(' ').map((image) => {
+	// 			product_options_images[index].push(image);
+	// 			update_product_option_property(product_options_images[index], 'images', index);
+	// 		});
+	// 	} else if (images) {
+	// 		console.log('images.length > 0');
+	// 		product_options_images[index].push(image);
+	// 		update_product_option_property(product_options_images[index], 'images', index);
+	// 	} else {
+	// 		console.log('images.length === 0');
+	// 		set_product_options_images([ image ]);
+	// 		update_product_option_property([ image ], 'images', index);
+	// 	}
+
+	// 	set_image('');
+	// };
+
+	const update_product_option_property = (value, field_name, index) => {
+		console.log({ value, field_name, index });
+		let new_order_items = [ ...orderItems ];
+		new_order_items[index] = {
+			...new_order_items[index],
+			product_option: { ...new_order_items[index].product_option, [field_name]: value }
+		};
+		set_orderItems(new_order_items);
+		console.log({ orderItems });
+		// let new_product_options = [ ...product_options ];
+		// new_product_options[index] = {
+		// 	...new_product_options[index],
+		// 	[field_name]: value
+		// };
+		// set_product_option(new_product_options);
+		// console.log({ product_options });
+	};
+
+	// const update_product_option = (e, index) => {
+
+	// };
+
+	const update_product_option = async (e, index) => {
+		const product_option = JSON.parse(e.target.value);
+		console.log({ product_option });
+		// console.log({ product_options: product_option.product_options.find((option) => option.default === true) });
+		let new_product_option = [ ...orderItems ];
+		new_product_option[index].product_option = {
+			...new_product_option[index].product_option,
+			name: product_option.name,
+			size: product_option.size,
+			price: product_option.price,
+			sale_price: product_option.sale_price,
+			color: product_option.color,
+			count_in_stock: product_option.count_in_stock
+
+			// secondary_product: orderItems[index].secondary_product
+		};
+		// new_product_option = [ ...orderItems ];
+		new_product_option[index] = {
+			...new_product_option[index],
+			color: product_option.color
+		};
+		// console.log(order_item._id);
+		set_orderItems(new_product_option);
+	};
+
+	// const remove_product_option_image = (image_index, e, option_index) => {
+	// 	e.preventDefault();
+	// 	// set_product_options_images((images) =>
+	// 	// 	images.filter((image, index) => {
+	// 	// 		return image_index !== index;
+	// 	// 	})
+	// 	// );
+	// 	// const images = product_options.find((option, index) => index === option_index);
+	// 	const images = product_options
+	// 		.map((option, index) => option.images)
+	// 		.find((images, index) => index === option_index)
+	// 		.filter((image, index) => image_index !== index);
+	// 	console.log({ images });
+
+	// 	// .filter((image, index) => {
+	// 	// 	return image_index !== index;
+	// 	// });
+	// 	update_product_option_property(images, 'images', option_index);
+	// };
 
 	return (
 		<div className="main_container p-20px">
@@ -789,14 +960,16 @@ const EditOrderPage = (props) => {
 																		<option key={1} defaultValue="">
 																			---Choose Product---
 																		</option>
-																		{products.map((product, index) => (
-																			<option
-																				key={index}
-																				value={JSON.stringify(product)}
-																			>
-																				{product.name}
-																			</option>
-																		))}
+																		{products
+																			.filter((product) => !product.hidden)
+																			.map((product, index) => (
+																				<option
+																					key={index}
+																					value={JSON.stringify(product)}
+																				>
+																					{product.name}
+																				</option>
+																			))}
 																	</select>
 																	<span className="custom-arrow" />
 																</div>
@@ -1105,6 +1278,243 @@ const EditOrderPage = (props) => {
 																</div>
 															</div>
 														</li>
+														{item.product_option && (
+															<div key={index} className="">
+																<div className="jc-b">
+																	<h2>Product Option {index + 1}</h2>
+																	<button className="btn primary w-4rem h-4rem p-14px mr-1rem mb-1rem">
+																		<i className="fas fa-times mr-5px" />
+																	</button>
+																</div>
+																<li>
+																	{/* <label
+																aria-label="sortOrder"
+																htmlFor="sortOrder"
+																className="select-label mb-15px"
+															>
+																Product Option
+															</label> */}
+																	<div className="ai-c h-25px mb-15px">
+																		<div className="custom-select">
+																			<select
+																				className="qty_select_dropdown"
+																				onChange={(e) =>
+																					update_product_option(e, index)}
+																			>
+																				<option key={1} defaultValue="">
+																					---Choose Option---
+																				</option>
+																				{item.product &&
+																					item.product.product_options &&
+																					item.product.product_options.map(
+																						(option, index) => (
+																							<option
+																								key={index}
+																								value={JSON.stringify(
+																									option
+																								)}
+																							>
+																								{option.name}
+																							</option>
+																						)
+																					)}
+																			</select>
+																			<span className="custom-arrow" />
+																		</div>
+																	</div>
+																</li>
+																<li>
+																	<label htmlFor="name">Name</label>
+																	<input
+																		type="text"
+																		name="name"
+																		defaultValue={item.product_option.name}
+																		value={item.product_option.name}
+																		id="name"
+																		onChange={(e) =>
+																			update_product_option_property(
+																				e.target.value,
+																				e.target.name,
+																				index
+																			)}
+																	/>
+																</li>
+																<li>
+																	<label htmlFor="size">Size</label>
+																	<input
+																		type="text"
+																		name="size"
+																		defaultValue={item.product_option.size}
+																		value={item.product_option.size}
+																		id="size"
+																		onChange={(e) =>
+																			update_product_option_property(
+																				e.target.value,
+																				e.target.name,
+																				index
+																			)}
+																	/>
+																</li>
+																<li>
+																	<label htmlFor="price">Price</label>
+																	<input
+																		type="text"
+																		name="price"
+																		defaultValue={item.product_option.price}
+																		value={item.product_option.price}
+																		id="price"
+																		onChange={(e) =>
+																			update_product_option_property(
+																				e.target.value,
+																				e.target.name,
+																				index
+																			)}
+																	/>
+																</li>
+																<li>
+																	<label htmlFor="sale_price">Sale Price</label>
+																	<input
+																		type="text"
+																		name="sale_price"
+																		defaultValue={item.product_option.sale_price}
+																		value={item.product_option.sale_price}
+																		id="sale_price"
+																		onChange={(e) =>
+																			update_product_option_property(
+																				e.target.value,
+																				e.target.name,
+																				index
+																			)}
+																	/>
+																</li>
+																<li>
+																	<label htmlFor="color">Color</label>
+																	<input
+																		type="text"
+																		name="color"
+																		defaultValue={item.product_option.color}
+																		value={item.product_option.color}
+																		id="color"
+																		onChange={(e) =>
+																			update_product_option_property(
+																				e.target.value,
+																				e.target.name,
+																				index
+																			)}
+																	/>
+																</li>
+																<li>
+																	<label htmlFor="count_in_stock">
+																		Count In Stock
+																	</label>
+																	<input
+																		type="text"
+																		name="count_in_stock"
+																		defaultValue={
+																			item.product_option.count_in_stock
+																		}
+																		value={item.product_option.count_in_stock}
+																		id="count_in_stock"
+																		onChange={(e) =>
+																			update_product_option_property(
+																				e.target.value,
+																				e.target.name,
+																				index
+																			)}
+																	/>
+																</li>
+
+																{/* {loading_checkboxes ? (
+																	<li>Loading...</li>
+																) : (
+																	<li>
+																		<label htmlFor="dropdown">Dropdown</label>
+																		<input
+																			type="checkbox"
+																			name="dropdown"
+																			defaultChecked={
+																				item.product_option.dropdown
+																			}
+																			id="dropdown"
+																			onChange={(e) =>
+																				update_product_option_property(
+																					e.target.checked,
+																					e.target.name,
+																					index
+																				)}
+																		/>
+																	</li>
+																)} */}
+																{/* {loading_checkboxes ? (
+																	<li>Loading...</li>
+																) : (
+																	<li>
+																		<label htmlFor="default">Default Option</label>
+																		<input
+																			type="checkbox"
+																			name="default"
+																			defaultChecked={item.product_option.default}
+																			id="default"
+																			onChange={(e) =>
+																				update_product_option_property(
+																					e.target.checked,
+																					e.target.name,
+																					index
+																				)}
+																		/>
+																	</li>
+																)} */}
+																{/* <li>
+															<label htmlFor="image">Option Image</label>
+															<input
+																type="text"
+																name={option.name}
+																// value={option.image}
+																id="image"
+																// onChange={(e) => set_image(e.target.value)}
+																// onChange={(e) => add_chip(e)}
+																onChange={(e) =>
+																	update_product_option_property(
+																		e.target.value,
+																		e.target.name,
+																		index
+																	)}
+															/>
+															<button
+																className="btn primary"
+																name={option.name}
+																// onClick={(e) => add_product_option_image(e)}
+																onClick={(e) =>
+																	add_product_option_image(
+																		image,
+																		e.target.name,
+																		index
+																	)}
+															>
+																Add Image
+															</button>
+														</li> */}
+																{/* <li>
+																<label htmlFor="image">Option Image</label>
+																<input
+																	type="text"
+																	name="image"
+																	value={image}
+																	id="image"
+																	// onChange={(e) =>
+																	// 	update_product_option_image(e, index)}
+																	onChange={(e) => set_image(e.target.value)}
+																/>
+																<button
+																	className="btn primary"
+																	onClick={(e) => add_option_image(e, index)}
+																>
+																	Add Image
+																</button>
+															</li>
+															{product_option_image_display(option.images, index)} */}
+															</div>
+														)}
 													</div>
 												);
 											})}
