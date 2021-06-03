11f0ebcf4536cfb8784875fadbd343965c4d7c5b
Kurt LaVacque <lavacquek@icloud.com>
2021-06-02 21:57:17 -0500

    Removed the Maintanence Button

--- client/src/index.js
+++ client/src/index.js
@@ -1,38 +1,16 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import store from './store';
-// import './scss/animation.scss';
-import './scss/index.scss';
 import './scss/style.scss';
-import './scss/media_queries.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { MaintenancePage } from './pages';
+import reportWebVitals from './reportWebVitals';
 
-const maintenance = false;
 
 ReactDOM.render(
 	<Provider store={store}>
-		<React.StrictMode>{maintenance ? <MaintenancePage /> : <App />}</React.StrictMode>
+		<React.StrictMode><App/></React.StrictMode>
 	</Provider>,
 	document.getElementById('root')
 );
@@ -41,3 +19,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+reportWebVitals();
