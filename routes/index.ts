15297d58834bb700eae425dbec5151262ed238f5
Kurt LaVacque <lavacquek@icloud.com>
2021-07-05 18:11:27 -0500

    Removed the other use from the index in routes

--- routes/index.ts
+++ routes/index.ts
@@ -35,9 +35,9 @@ const router = express.Router();
 // API Routes
 router.use('/api', apiRoutes);
 
-// If no API routes are hit, send the React app
-router.use((req, res) => {
-	res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// // If no API routes are hit, send the React app
+// router.use((req, res) => {
+// 	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 export default router;
