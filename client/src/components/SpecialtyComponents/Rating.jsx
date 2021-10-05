c01568048dc64030c78953c8492e0b694d5fa531
Kurt LaVacque <lavacquek@icloud.com>
2021-10-05 17:49:12 -0500

    Fixed the spacing for the star rating to be lined up better

--- client/src/components/SpecialtyComponents/Rating.jsx
+++ client/src/components/SpecialtyComponents/Rating.jsx
@@ -9,23 +9,23 @@ const Rating = (props) => {
 		<div className="rating">
 			<span>
 				<i className={rating >= 1 ? 'fa fa-star' : rating >= 0.5 ? 'fa fa-star-half' : 'fa fa-star-o'} />
-				<i className="fa fa-star-o pos-rel left-n18px" />
+				<i className="fa fa-star-o pos-rel left-n20px" />
 			</span>
 			<span className="ml-n15px">
 				<i className={rating >= 2 ? 'fa fa-star' : rating >= 1.5 ? 'fa fa-star-half' : 'fa fa-star-o'} />
-				<i className="fa fa-star-o pos-rel left-n18px" />
+				<i className="fa fa-star-o pos-rel left-n20px" />
 			</span>
 			<span className="ml-n15px">
 				<i className={rating >= 3 ? 'fa fa-star' : rating >= 2.5 ? 'fa fa-star-half' : 'fa fa-star-o'} />
-				<i className="fa fa-star-o pos-rel left-n18px" />
+				<i className="fa fa-star-o pos-rel left-n20px" />
 			</span>
 			<span className="ml-n15px">
 				<i className={rating >= 4 ? 'fa fa-star' : rating >= 3.5 ? 'fa fa-star-half' : 'fa fa-star-o'} />
-				<i className="fa fa-star-o pos-rel left-n18px" />
+				<i className="fa fa-star-o pos-rel left-n20px" />
 			</span>
 			<span className="ml-n15px">
 				<i className={rating >= 5 ? 'fa fa-star' : rating >= 4.5 ? 'fa fa-star-half' : 'fa fa-star-o'} />
-				<i className="fa fa-star-o pos-rel left-n18px" />
+				<i className="fa fa-star-o pos-rel left-n20px" />
 			</span>
 			<span className="rating">{numReviews && numReviews + ' reviews'}</span>
 		</div>
