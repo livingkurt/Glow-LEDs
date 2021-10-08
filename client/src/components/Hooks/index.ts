2e0058a41389d3a5db4d71be07ec73d8dfef3e85
Kurt LaVacque <lavacquek@icloud.com>
2021-10-07 21:46:42 -0500

    Removed the import

--- client/src/components/Hooks/index.ts
+++ client/src/components/Hooks/index.ts
@@ -1,3 +1,2 @@
 export { default as useHover } from './useHover';
 export { default as userWindowDimensions } from './windowDimensions';
-export { default as useAddressPredictions } from './useAddressPredictions';
