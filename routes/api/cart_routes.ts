f632f4b3dad86a92b7e1b560f6bfba63eb61e057
Kurt LaVacque <lavacquek@icloud.com>
2021-10-14 16:33:31 -0500

    Removed some of the need for headers on some of the routes

--- routes/api/cart_routes.ts
+++ routes/api/cart_routes.ts
@@ -6,10 +6,7 @@ const { isAuth, isAdmin } = require('../../util');
 const router = express.Router();
 
 router.route('/').get(isAuth, isAdmin, cart_controller.findAll_carts_c).post(cart_controller.create_carts_c);
-router
-	.route('/user/:id')
-	.get(isAuth, isAdmin, cart_controller.findByUser_carts_c)
-	.put(cart_controller.remove_cartitem_carts_c);
+router.route('/user/:id').get(cart_controller.findByUser_carts_c).put(cart_controller.remove_cartitem_carts_c);
 
 router
 	.route('/:id')
