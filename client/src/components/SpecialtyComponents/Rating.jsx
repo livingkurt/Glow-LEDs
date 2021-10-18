ffe413eb42cd3b23aeea7a3a127c1d3411636251
Kurt LaVacque <lavacquek@icloud.com>
2021-10-18 15:09:21 -0500

    Updated the Spacing of the review stars

--- client/src/components/SpecialtyComponents/Rating.jsx
+++ client/src/components/SpecialtyComponents/Rating.jsx
@@ -11,19 +11,19 @@ const Rating = (props) => {
 				<i className={rating >= 1 ? 'fa fa-star' : rating >= 0.5 ? 'fa fa-star-half' : 'fa fa-star-o'} />
 				<i className="fa fa-star-o pos-rel left-n20px" />
 			</span>
-			<span className="ml-n15px">
+			<span className="ml-n23px">
 				<i className={rating >= 2 ? 'fa fa-star' : rating >= 1.5 ? 'fa fa-star-half' : 'fa fa-star-o'} />
 				<i className="fa fa-star-o pos-rel left-n20px" />
 			</span>
-			<span className="ml-n15px">
+			<span className="ml-n23px">
 				<i className={rating >= 3 ? 'fa fa-star' : rating >= 2.5 ? 'fa fa-star-half' : 'fa fa-star-o'} />
 				<i className="fa fa-star-o pos-rel left-n20px" />
 			</span>
-			<span className="ml-n15px">
+			<span className="ml-n23px">
 				<i className={rating >= 4 ? 'fa fa-star' : rating >= 3.5 ? 'fa fa-star-half' : 'fa fa-star-o'} />
 				<i className="fa fa-star-o pos-rel left-n20px" />
 			</span>
-			<span className="ml-n15px">
+			<span className="ml-n23px">
 				<i className={rating >= 5 ? 'fa fa-star' : rating >= 4.5 ? 'fa fa-star-half' : 'fa fa-star-o'} />
 				<i className="fa fa-star-o pos-rel left-n20px" />
 			</span>
