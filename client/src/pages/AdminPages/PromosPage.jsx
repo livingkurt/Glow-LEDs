bc26da783dc6689b60f569e44b9e2695ecbc3ded
Kurt LaVacque <lavacquek@icloud.com>
2021-12-08 23:20:10 -0600

    Added Colors to the Promo codes so I can tell when its a single use code

--- client/src/pages/AdminPages/PromosPage.jsx
+++ client/src/pages/AdminPages/PromosPage.jsx
@@ -56,7 +56,9 @@ const PromosPage = (props) => {
 		// { name: 'Free Shipping', color: '#3e4c6d' },
 		{ name: 'Admin Only', color: '#525252' },
 		{ name: 'Affiliate Only', color: '#7d5555' },
-		{ name: 'No Restrictions', color: '#3e4c6d' }
+		{ name: 'No Restrictions', color: '#3e4c6d' },
+		{ name: 'Single Use', color: '#5f557d' },
+		{ name: 'Used', color: '#303030' }
 		// { name: 'Specific User', color: '#3d7f79' }
 		// { name: 'Active', color: '#3f6561' }
 	];
@@ -73,15 +75,51 @@ const PromosPage = (props) => {
 		// if (promo.free_shipping) {
 		// 	result = colors[0].color;
 		// }
+
 		if (promo.admin_only) {
 			result = colors[0].color;
 		}
 		if (promo.affiliate_only) {
 			result = colors[1].color;
 		}
+
 		if (!promo.affiliate_only && !promo.admin_only) {
 			result = colors[2].color;
 		}
+
+		if (promo.single_use) {
+			result = colors[3].color;
+		}
+		if (promo.used_once) {
+			result = colors[4].color;
+		}
+		//  else {
+		// 	result = colors[2].color;
+		// }
+		// if (promo.single_use && promo.used) {
+		// 	result = colors[4].color;
+		// } else {
+		// 	result = colors[2].color;
+		// }
+
+		// if (promo.used) {
+		// 	result = colors[4].color;
+		// } else if (promo.admin_only) {
+		// 	result = colors[0].color;
+		// } else if (promo.affiliate_only) {
+		// 	result = colors[1].color;
+		// } else if (!promo.affiliate_only && !promo.admin_only) {
+		// 	if (promo.single_use) {
+		// 		if (promo.single_use && promo.used) {
+		// 			result = colors[4].color;
+		// 		} else {
+		// 			result = colors[3].color;
+		// 		}
+		// 	}
+		// } else {
+		// 	result = colors[2].color;
+		// }
+
 		// if (promo.user) {
 		// 	result = colors[3].color;
 		// }
