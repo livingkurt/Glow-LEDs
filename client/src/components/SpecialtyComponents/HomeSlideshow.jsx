2501b553ba2d0d8900e15a385935245bde89cfaa
Kurt LaVacque <lavacquek@icloud.com>
2021-12-16 11:10:02 -0600

    Removed the Carosel from the homepage

--- client/src/components/SpecialtyComponents/HomeSlideshow.jsx
+++ client/src/components/SpecialtyComponents/HomeSlideshow.jsx
@@ -37,7 +37,7 @@ const HomeSlideshow = ({ slideshow }) => {
 		<Carousel
 			swipeable={mobile_check() ? true : false}
 			draggable={mobile_check() ? true : false}
-			// showDots={true}
+			showDots={false}
 			// partialVisible={true}
 			// centerMode={true}
 			responsive={responsive}
