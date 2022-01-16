a257c16f64fb5e58235bd95eb8195578b75168e7
Kurt LaVacque <lavacquek@icloud.com>
2022-01-16 16:59:51 -0600

    Changed the cors

--- server.ts
+++ server.ts
@@ -52,7 +52,12 @@ async function start() {
 		next();
 	});
 	// app.use(allowCrossDomain);
-	app.use(cors());
+	const corsOptions = {
+		origin: 'http://localhost:3000',
+		credentials: true, //access-control-allow-credentials:true
+		optionSuccessStatus: 200
+	};
+	app.use(cors(corsOptions));
 	app.use(express.json({ limit: '50mb' }));
 	app.use(express.urlencoded({ limit: '50mb' }));
 	// app.use(express.urlencoded({ extended: true }));
