f88e6c70de51a971aa166cbf26dce7720fed3d6e
Kurt LaVacque <klavacque@covalentnetworks.com>
2022-08-16 23:33:00 +0100

    Fixed the tooltip button

--- client/src/components/GlowLEDsComponents/GLButton/GLButton.jsx
+++ client/src/components/GlowLEDsComponents/GLButton/GLButton.jsx
@@ -2,29 +2,21 @@ import React from "react";
 import ReactTooltip from "react-tooltip";
 
 const GLButton = ({ children, icon, onClick, onKeyUp, className, disabled, variant, fullWidth, tooltip, ...otherProps }) => {
-  const button = (
-    <button
-      type="button"
-      className={`${variant ? "btn" : ""} ${variant} ${className} ${fullWidth ? "w-100per" : ""}`}
-      onClick={variant !== "disabled" && onClick}
-      onKeyUp={onKeyUp}
-      {...otherProps}
-    >
-      {children}
-    </button>
+  return (
+    <>
+      <button
+        type="button"
+        data-tip={tooltip}
+        className={`${variant ? "btn" : ""} ${variant} ${className} ${fullWidth ? "w-100per" : ""}`}
+        onClick={variant !== "disabled" && onClick}
+        onKeyUp={onKeyUp}
+        {...otherProps}
+      >
+        {children}
+      </button>
+      {tooltip && <ReactTooltip className="br-10px" />}
+    </>
   );
-  if (tooltip) {
-    return (
-      <>
-        <div className="column ai-c pos-rel" data-tip={tooltip}>
-          {button}
-        </div>
-        {tooltip && <ReactTooltip className="br-10px" />}
-      </>
-    );
-  } else {
-    return button;
-  }
 };
 
 export default GLButton;
