4d33fdb52e2648d65ad301109207944672dabbc0
Kurt LaVacque <klavacque@covalentnetworks.com>
2023-02-07 21:51:42 -0600

    Pushing up the correect stuff

--- background/monthly_worker.js
+++ background/monthly_worker.js
@@ -4,13 +4,13 @@ const monthly_worker = () => {
   // Get the current date
   const today = new Date();
   // Check if today is Froday (the 5th day of the week)
-  // if (today.getDate() === 1) {
-  //   // Run the code that you only want to run once a week
-  //   payout_affiliates();
-  //   payout_teams();
-  //   refresh_sponsor_codes();
-  payout_tips();
-  // }
+  if (today.getDate() === 1) {
+    // Run the code that you only want to run once a week
+    payout_affiliates();
+    payout_teams();
+    refresh_sponsor_codes();
+    payout_tips();
+  }
 };
 
 monthly_worker();
