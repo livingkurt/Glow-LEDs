52efcffafe7d9c9a45269bdc80825e89dbc9abb7
Kurt LaVacque <lavacquek@icloud.com>
2023-02-20 05:07:19 -0600

    Got the Google Sheets up and running

---
+++ background/daily_worker.ts
@@ -0,0 +1,9 @@
+import { facebook_catalog_upload } from "./daily_workers/facebook_catalog_upload";
+import { google_catalog_upload } from "./daily_workers/google_catalog_upload";
+
+const daily_worker = () => {
+  facebook_catalog_upload();
+  google_catalog_upload();
+};
+
+daily_worker();
