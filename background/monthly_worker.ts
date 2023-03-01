cc41c681c19577f3d17a1a2c3ce7a6100311a473
Kurt LaVacque <lavacquek@icloud.com>
2023-02-28 19:24:17 -0600

    Fixed the autopay

--- background/monthly_worker.ts
+++ background/monthly_worker.ts
@@ -7,9 +7,9 @@ const monthly_worker = () => {
   // Get the current date
   const today = new Date();
   // Check if today is Froday (the 5th day of the week)
-  payout_affiliates();
   if (today.getDate() === 1) {
     // Run the code that you only want to run once a week
+    payout_affiliates();
     payout_teams();
     refresh_sponsor_codes();
     payout_tips();
