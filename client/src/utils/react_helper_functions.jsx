2b13640300891dfcac985888a3d6988c11c24ab5
Kurt LaVacque <lavacquek@icloud.com>
2023-05-03 09:17:10 -0500

    Removed old Auth file

--- client/src/utils/react_helper_functions.jsx
+++ client/src/utils/react_helper_functions.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import jwt_decode from "jwt-decode";
-import setAuthToken from "../utils/setAuthToken";
 import { API_Users } from ".";
 import store from "../store";
 import { GLButton } from "../shared/GlowLEDsComponents";
