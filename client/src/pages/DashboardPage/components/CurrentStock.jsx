d9de62459509948d5c74dc846c309d88e90087e7
Kurt LaVacque <lavacquek@icloud.com>
2023-06-22 21:38:01 -0500

    Removed the console logs

--- client/src/pages/DashboardPage/components/CurrentStock.jsx
+++ client/src/pages/DashboardPage/components/CurrentStock.jsx
@@ -54,20 +54,18 @@ const CurrentStock = ({ currentStock }) => {
         rows={!currentStock.isLoading && currentStock?.data?.filter(row => row.category === "batteries")}
         onEdit={async value => {
           dispatch(API.saveProduct(value));
-          if (value.option_products.length === 0) {
+          if (value.option_products.length > 0) {
             // Fetch option products first
             const optionProductsActions = await Promise.all(value.option_products.map(id => dispatch(API.detailsProduct(id))));
 
             // Extract payload from each action
             const optionProducts = optionProductsActions.map(action => action.payload);
-            console.log({ optionProducts });
 
             const updatedProducts = optionProducts.map(product => {
               const batchStock = Number(value.count_in_stock) / Number(product.size);
               console.log({ batchStock: Math.floor(batchStock) });
               return { ...product, count_in_stock: Math.floor(batchStock) };
             });
-            console.log({ updatedProducts });
 
             await Promise.all(updatedProducts.map(product => dispatch(API.saveProduct(product))));
           }
