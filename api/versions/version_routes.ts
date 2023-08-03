7425e89307d8ad0b3013c88a39bfeb5e964c1e2f
Kurt LaVacque <lavacquek@icloud.com>
2023-08-03 10:34:17 -0500

    Fixing database

--- api/versions/version_routes.ts
+++ api/versions/version_routes.ts
@@ -2,9 +2,32 @@ import express from "express";
 import Version from "./version";
 const router = express.Router();
 
-router.get("/version", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
+    console.log({ version: "version" });
     const versionDoc = await Version.findOne();
+    console.log({ versionDoc });
+    res.json({ version: versionDoc.version });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+router.post("/", async (req, res) => {
+  try {
+    console.log({ version: "version" });
+    const versionDoc: any = await Version.create({ version: 1 });
+    console.log({ versionDoc });
+    res.json({ version: versionDoc.version });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+router.put("/", async (req, res) => {
+  try {
+    console.log({ version: "version" });
+    const versions = await Version.find();
+    const versionDoc = await Version.updateOne({ _id: versions[0]._id }, { version: versions[0].version + 1 });
+    console.log({ versionDoc });
     res.json({ version: versionDoc.version });
   } catch (err) {
     res.status(500).json({ message: err.message });
