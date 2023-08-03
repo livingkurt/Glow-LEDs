b48af88c672c5cfa3cd9daec01f17a71ab4c0b58
Kurt LaVacque <lavacquek@icloud.com>
2023-08-03 08:32:10 -0500

    Added a way to view sponsor monthly checkins on the EditAffiliateModal

--- client/src/pages/AffiliatesPage/components/EditAffiliateModal.jsx
+++ client/src/pages/AffiliatesPage/components/EditAffiliateModal.jsx
@@ -1,14 +1,19 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import GLModal from "../../../shared/GlowLEDsComponents/GLActiionModal/GLActiionModal";
 import { set_edit_affiliate_modal, set_affiliate } from "../../../slices/affiliateSlice";
 import * as API from "../../../api";
 import { GLForm } from "../../../shared/GlowLEDsComponents/GLForm";
 import { useLocation } from "react-router-dom";
+import { affiliateFormFields } from "./affiliateFormFields";
+import { setTabIndex } from "../../DashboardPage/dashboardSlice";
+import { AppBar, Tab, Tabs, Typography } from "@mui/material";
+import GLTabPanel from "../../../shared/GlowLEDsComponents/GLTabPanel/GLTabPanel";
 
 const EditAffiliateModal = () => {
   const dispatch = useDispatch();
   const location = useLocation();
+  const [tabIndex, setTabIndex] = useState(0);
   const affiliatePage = useSelector(state => state.affiliates.affiliatePage);
   const { edit_affiliate_modal, affiliate, loading } = affiliatePage;
 
@@ -35,140 +40,12 @@ const EditAffiliateModal = () => {
     return () => (clean = false);
   }, [dispatch]);
 
-  const formFields = {
-    user: {
-      type: "autocomplete_single",
-      label: "Users",
-      options: users,
-      labelProp: "user",
-      getOptionLabel: option => `${option.first_name} ${option.last_name}`,
-      permissions: ["admin"]
-    },
-    artist_name: {
-      type: "text",
-      label: "Artist Name"
-    },
-    location: {
-      type: "text",
-      label: "Location"
-    },
-    style: {
-      type: "text",
-      label: "Style"
-    },
-    inspiration: {
-      type: "text",
-      label: "Inspiration"
-    },
-    start_year: {
-      type: "text",
-      label: "The year you started gloving"
-    },
-    bio: {
-      type: "text_multiline",
-      label: "Bio"
-    },
-    instagram_link: {
-      type: "text",
-      label: "Instagram Link"
-    },
-    tiktok_link: {
-      type: "text",
-      label: "TikTok Link"
-    },
-    youtube_link: {
-      type: "text",
-      label: "YouTube Link"
-    },
-    facebook_link: {
-      type: "text",
-      label: "Facebook Link"
-    },
-
-    chips: {
-      type: "autocomplete_multiple",
-      label: "Microlights you currently have",
-      options: chips,
-      labelProp: "name"
-    },
-    products: {
-      type: "autocomplete_multiple",
-      label: "Glow LEDs Gear you currently have",
-      options: products,
-      labelProp: "name"
-    },
-    public_code: {
-      type: "autocomplete_single",
-      label: "Public Code",
-      options: promos,
-      labelProp: "promo_code",
-      permissions: ["admin"]
-    },
-    private_code: {
-      type: "autocomplete_single",
-      label: "Private Code",
-      options: promos,
-      labelProp: "promo_code",
-      permissions: ["admin"]
-    },
-
-    percentage_off: {
-      type: "number",
-      label: "Percentage Off",
-      permissions: ["admin"]
-    },
-    picture: {
-      type: "text",
-      label: "Picture",
-      permissions: ["admin"]
-    },
-    video: {
-      type: "text",
-      label: "Video",
-      permissions: ["admin"]
-    },
-
-    link: {
-      type: "text",
-      label: "Link",
-      permissions: ["admin"]
-    },
-    pathname: {
-      type: "text",
-      label: "Pathname",
-      permissions: ["admin"]
-    },
-    answers: {
-      type: "array",
-      label: "Answers",
-      permissions: ["admin"]
-    },
-    promoter: {
-      type: "checkbox",
-      label: "Promoter",
-      permissions: ["admin"]
-    },
-    rave_mob: {
-      type: "checkbox",
-      label: "Rave Mob",
-      permissions: ["admin"]
-    },
-    team: {
-      type: "checkbox",
-      label: "Team",
-      permissions: ["admin"]
-    },
-    sponsor: {
-      type: "checkbox",
-      label: "Sponsor",
-      permissions: ["admin"]
-    },
-    active: {
-      type: "checkbox",
-      label: "Active",
-      permissions: ["admin"]
-    }
-  };
+  const formFields = affiliateFormFields({
+    products,
+    users,
+    chips,
+    promos,
+  });
 
   return (
     <div>
@@ -178,7 +55,7 @@ const EditAffiliateModal = () => {
           dispatch(
             API.saveAffiliate({
               affiliate: { ...affiliate, user: affiliate?.user?._id || current_user._id },
-              profile: location.pathname === "/secure/account/profile"
+              profile: location.pathname === "/secure/account/profile",
             })
           );
           dispatch(API.listUsers());
@@ -199,6 +76,44 @@ const EditAffiliateModal = () => {
           onChange={value => dispatch(set_affiliate(value))}
           loading={loading && loading_users && loading_products && loading_chips && loading_promos}
         />
+        <Typography component="h4" variant="h4" sx={{ mb: 2 }}>
+          {formFields.sponsorMonthlyCheckins.title}
+        </Typography>
+
+        <AppBar position="sticky" color="transparent">
+          <Tabs
+            value={tabIndex}
+            className="jc-b"
+            onChange={(e, newValue) => {
+              dispatch(setTabIndex(newValue));
+            }}
+          >
+            {affiliate?.sponsorMonthlyCheckins?.map((item, index) => {
+              return <Tab label={item.month} value={index} />;
+            })}
+          </Tabs>
+        </AppBar>
+        {affiliate?.sponsorMonthlyCheckins?.map((item, index) => {
+          return (
+            <GLTabPanel value={tabIndex} index={index}>
+              <GLForm
+                formData={formFields.sponsorMonthlyCheckins.fields}
+                state={item}
+                onChange={value => {
+                  const sponsorMonthlyCheckins = affiliate?.sponsorMonthlyCheckins?.map((item, i) => {
+                    if (i === index) {
+                      return { ...item, ...value };
+                    } else {
+                      return item;
+                    }
+                  });
+                  dispatch(set_affiliate({ sponsorMonthlyCheckins }));
+                }}
+                loading={loading && loading_users}
+              />
+            </GLTabPanel>
+          );
+        })}
       </GLModal>
     </div>
   );
