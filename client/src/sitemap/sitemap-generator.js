25387519aa36368ca23ac29080311458fc1c4c72
Kurt LaVacque <lavacquek@icloud.com>
2023-09-12 08:07:47 -0500

    Fixing the build error

--- client/src/sitemap/sitemap-generator.js
+++ client/src/sitemap/sitemap-generator.js
@@ -46,7 +46,7 @@ async function generateSitemap() {
     sitemapXML += "</urlset>";
 
     fs.writeFileSync(path.join(__dirname, "../sitemap.xml"), sitemapXML);
-  } catch (error: any) {
+  } catch (error) {
     console.error("An error occurred while generating the sitemap:", error);
   }
 }
