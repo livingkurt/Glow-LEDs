5a1288fecafe115b781e9bd53f3893a95ad38a35
Kurt LaVacque <lavacquek@icloud.com>
2023-09-16 18:04:40 -0500

    Updated the eslinti json files

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,47 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
+    jquery: true,
+    jest: true,
+    serviceworker: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "airbnb",
+    "airbnb/hooks",
+    "plugin:jest/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
     ecmaVersion: 12,
-    sourceType: "module"
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+      experimentalObjectRestSpread: true,
+    },
   },
-  plugins: ["react", "@typescript-eslint"],
-  rules: {}
+  plugins: ["react", "@typescript-eslint", "@babel", "import", "jest", "jsx-a11y", "react-hooks", "prettier"],
+  rules: {
+    // Your merged rules here...
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    jest: {
+      version: "28",
+    },
+    "import/resolver": {
+      webpack: {
+        config: "config/webpack/custom.js",
+      },
+    },
+    "import/core-modules": ["react-redux"],
+  },
 };
