d9aef4ed6a10e8595f65aec10b33b88ad0c80213
Kurt LaVacque <lavacquek@icloud.com>
2023-09-17 09:42:09 -0500

    Making pre-commit be saved in git

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const environment = process.env.ENVIRONMENT;
+// const environment = process.env.ENVIRONMENT;
 // const environment = "staging";
 
 const decideEnvironment = ({ production, staging, development }) => {
---
+++ server/scripts/hooks/pre-commit.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Change this to the path of your file
+FILE="config.js"
+
+# Check if the environment variable in your file is set to process.env.ENVIRONMENT
+if ! grep -q 'const environment = process.env.ENVIRONMENT;' "$FILE"; then
+  echo "Can only commit when environment is set to process.env.ENVIRONMENT"
+  exit 1
+fi
+
+# Change this to the path of your file
+FILE="client/src/config.js"
+
+# Check if the environment variable in your file is set to process.env.ENVIRONMENT
+if ! grep -q 'const environment = process.env.REACT_APP_ENVIRONMENT;' "$FILE"; then
+  echo "Can only commit when environment is set to process.env.REACT_APP_ENVIRONMENT"
+  exit 1
+fi
+
+
+# Change this to the path of your file
+FILE="background/monthly_worker.ts"
+
+# Check if the date check in your file is commented out
+if grep -q '// if (today.getDate() === 1) {' "$FILE"; then
+  echo "Cannot commit because if (today.getDate() === 1) { in background/monthly_worker.ts is commented out"
+  exit 1
+fi
+
+# Change this to the path of your file
+FILE="background/weekly_worker.ts"
+
+# Check if the date check in your file is commented out
+if grep -q '// if (today.getDay() === 5) {' "$FILE"; then
+  echo "Cannot commit because if (today.getDay() === 1) { in background/weekly_worker.ts check is commented out"
+  exit 1
+fi
