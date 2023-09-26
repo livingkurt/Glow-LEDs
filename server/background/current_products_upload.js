a167379e87ea0b161753cf5754bb59f1dae7dc47
Kurt LaVacque <57471593+livingkurt@users.noreply.github.com>
2023-09-25 22:09:45 -0500

    GL-2786-Autopay-Fix #82
    GL-2786-Autopay-Fix #82

---
+++ server/background/current_products_upload.js
@@ -0,0 +1,79 @@
+import axios from "axios";
+import config from "../config";
+
+const Airtable = require("airtable");
+
+const current_products_upload = async () => {
+  try {
+    const { data } = await axios.get("https://www.glow-leds.com/api/products?limit=0&hidden=false&option=false");
+
+    const new_rows = data.products
+      .filter(product => !product.hidden)
+      .filter(product => product.category !== "options")
+      .map((product, i) => {
+        const id = product._id;
+        const title = product.name === "Supremes" ? "GL Gloves" : product.name;
+        const description = product.description;
+        const availability = "In Stock";
+        const condition = "New";
+        const price = `${product.price} USD`;
+        const link = `https://www.glow-leds.com/collections/all/products/${product.pathname}`;
+        const image_link = product.images_object[0].link;
+        const additional_image_link = product.images_object[1].link;
+        const brand = "Glow LEDs";
+        const inventory = product.count_in_stock;
+        const fb_product_category = "toys & games > electronic toys";
+        const google_product_category = "Toys & Games > Toys > Visual Toys";
+        const sale_price = `${product.sale_price && product.sale_price.toFixed(2)} USD`;
+        const sale_price_effective_date = `${product.sale_start_date && product.sale_start_date.slice(0, -1)}/${
+          product.sale_end_date && product.sale_end_date.slice(0, -1)
+        }`;
+        const product_type = product.category;
+        const color = product.color;
+        const size = product.size;
+
+        return {
+          title,
+          description,
+          availability,
+          condition,
+          price,
+          link,
+          image_link,
+          additional_image_link,
+          brand,
+          inventory,
+          fb_product_category,
+          google_product_category,
+          sale_price,
+          sale_price_effective_date,
+          product_type,
+          color,
+          size,
+          id,
+        };
+      });
+
+    const base = new Airtable({ apiKey: config.REACT_APP_AIRTABLE_ACCESS_TOKEN }).base("app9vDOYXFhhQr529");
+
+    base("Current Products").update(new_rows, function (err, records) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      records.forEach(function (record) {});
+    });
+
+    // await sheet.addRows(new_rows);
+    // await sheet.saveUpdatedCells();
+    // adding / removing sheets
+    // const newSheet = await doc.addSheet({ title: 'hot new sheet!' });
+    // await newSheet.delete();
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+  }
+};
+
+current_products_upload();
---
+++ server/background/monthly_workers/payout_affiliates.js
@@ -0,0 +1,80 @@
+import axios from "axios";
+
+import {
+  last_month_date_range,
+  determine_code_tier,
+  get_todays_date,
+  save_paycheck_to_expenses,
+} from "../worker_helpers";
+import { domain } from "../worker_helpers";
+
+export const payout_affiliates = async () => {
+  try {
+    const domainUrl = domain();
+
+    const { start_date, end_date } = last_month_date_range();
+    // Get promo code usage for the previous month
+    const { data } = await axios.get(`${domainUrl}/api/affiliates?active=true&rave_mob=false`);
+    const affiliates = data.data;
+    await Promise.all(
+      affiliates.map(async affiliate => {
+        const { data: promo_code_usage } = await axios.get(
+          `${domain()}/api/orders/code_usage/${
+            affiliate?.public_code?.promo_code
+          }?start_date=${start_date}&end_date=${end_date}&sponsor=${affiliate.sponsor}`
+        );
+
+        if (affiliate?.user?.stripe_connect_id && promo_code_usage.earnings >= 1) {
+          console.log({
+            amount: promo_code_usage.earnings,
+            stripe_connect_id: affiliate.user.stripe_connect_id,
+            description: `Monthly Payout for ${affiliate.user.first_name} ${affiliate.user.last_name}`,
+          });
+          await axios.post(`${domainUrl}/api/payments/payout_transfer`, {
+            amount: promo_code_usage.earnings,
+            stripe_connect_id: affiliate.user.stripe_connect_id,
+            description: `Monthly Payout for ${affiliate.user.first_name} ${affiliate.user.last_name}`,
+          });
+          const data = {
+            Expense: `${affiliate.artist_name} Affiliate Earnings`,
+            Date: get_todays_date(),
+            Amount: promo_code_usage.earnings,
+            "Place of Purchase": "Stripe",
+            Card: "Stripe",
+            Category: ["Employee Paycheck"],
+          };
+          save_paycheck_to_expenses(data);
+        }
+        console.log({
+          affiliate: affiliate?._id,
+          user: affiliate?.user?._id,
+          amount: promo_code_usage.earnings,
+          revenue: promo_code_usage.revenue,
+          promo_code: affiliate?.public_code?._id,
+          uses: promo_code_usage.number_of_uses,
+          stripe_connect_id: affiliate?.user?.stripe_connect_id || null,
+          paid: affiliate?.user?.stripe_connect_id ? true : false,
+          paid_at: new Date(),
+        });
+        await axios.post(`${domainUrl}/api/paychecks`, {
+          affiliate: affiliate?._id,
+          user: affiliate?.user?._id,
+          amount: promo_code_usage.earnings,
+          revenue: promo_code_usage.revenue,
+          promo_code: affiliate?.public_code?._id,
+          uses: promo_code_usage.number_of_uses,
+          stripe_connect_id: affiliate?.user?.stripe_connect_id || null,
+          paid: affiliate?.user?.stripe_connect_id ? true : false,
+          paid_at: new Date(),
+        });
+        const percentage_off = determine_code_tier(affiliate, promo_code_usage.number_of_uses);
+        await axios.put(`${domainUrl}/api/promos/${affiliate?.private_code?._id}`, {
+          ...affiliate.private_code,
+          percentage_off,
+        });
+      })
+    );
+  } catch (error) {
+    console.log("error", error);
+  }
+};
