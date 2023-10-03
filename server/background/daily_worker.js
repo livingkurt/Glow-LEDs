42c007e9acfb7cd11f4676b42bee6946c517564e
Kurt LaVacque <57471593+livingkurt@users.noreply.github.com>
2023-10-03 10:20:46 -0500

    GL-2794-Fix-Facebook-Shop-Upload-Add-CSV-To-Static-Folder (#92)
    GL-2794-Fix-Facebook-Shop-Upload-Add-CSV-To-Static-Folder (#92)

--- server/background/daily_worker.js
+++ server/background/daily_worker.js
@@ -1,9 +1,7 @@
 import { facebook_catalog_upload } from "./daily_workers/facebook_catalog_upload";
-import { google_catalog_upload } from "./daily_workers/google_catalog_upload";
 
 const daily_worker = () => {
   facebook_catalog_upload();
-  // google_catalog_upload();
 };
 
 daily_worker();
