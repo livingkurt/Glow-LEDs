bdf4cabc8ea30d92d5faa7538bc82e3c5d79c50c
Kurt LaVacque <lavacquek@icloud.com>
2023-10-03 22:18:12 -0500

    Added logic to generate the product options for each product to be uploaded to facebook

--- server/background/daily_workers/facebook_catalog_upload.js
+++ server/background/daily_workers/facebook_catalog_upload.js
@@ -5,48 +5,69 @@ import { domain } from "../worker_helpers";
 
 const determineImage = (product, imageNum) => {
   if (product.images_object && product.images_object[imageNum]) {
+    if (typeof product.images_object[imageNum].link === "function") {
+      return product.images[imageNum];
+    }
     return product.images_object[imageNum].link;
   } else {
-    return product.images[imageNum];
+    return "";
   }
 };
 
 export const facebook_catalog_upload = async () => {
   try {
-    // Fetch existing products from MongoDB
     const domainUrl = domain();
     const { data } = await axios.get(`${domainUrl}/api/products/facebook_catelog`);
 
-    const new_rows = data
-      .filter(product => !product.hidden)
-      .filter(product => product.category !== "options")
-      .filter(product => product.option === false)
-      .sort((a, b) => (a.order > b.order ? 1 : -1))
-      .map((product, i) => ({
-        id: product._id,
-        title: product.name,
-        description: product.description,
-        availability: "In Stock",
-        condition: "New",
-        price: `${product.price} USD`,
-        link: `https://www.glow-leds.com/collections/all/products/${product.pathname}`,
-        image_link: determineImage(product, 0),
-        additional_image_link: determineImage(product, 1),
-        brand: "Glow LEDs",
-        inventory: product.quantity,
-        fb_product_category: "toys & games > electronic toys",
-        google_product_category: "Toys & Games > Toys > Visual Toys",
-        sale_price: `${product.sale_price && product.sale_price.toFixed(2)} USD`,
-        sale_price_effective_date: `${product.sale_start_date && product.sale_start_date.slice(0, -1)}/${
-          product.sale_end_date && product.sale_end_date.slice(0, -1)
-        }`,
-        product_type: product.category,
-        color: product.color,
-        size: product.size,
-      }));
+    let csvRows = [];
+    let addedProductOptionIDs = {};
 
-    // Convert the normalized data to CSV
-    const csv = Papa.unparse(new_rows);
+    for (let product of data) {
+      const mainProductNormalized = normalizeProductForCSV(product);
+      // console.log({ mainProductNormalized });
+
+      // Validate main product before pushing
+      if (isValidRow(mainProductNormalized)) {
+        csvRows.push(mainProductNormalized);
+      }
+
+      // Handle variants.
+      let variantFields = [
+        "products",
+        "color_products",
+        "secondary_color_products",
+        "secondary_products",
+        "option_products",
+      ];
+
+      for (let field of variantFields) {
+        for (let variantProduct of product[field]) {
+          // Skip if this product option has already been added
+          if (addedProductOptionIDs[variantProduct._id]) continue;
+
+          let variantRow = normalizeProductForCSV(variantProduct);
+          variantRow.item_group_id = product._id; // Set the parent's ID as the item_group_id.
+          // console.log({ variantRow: variantRow.image_link, mainProductNormalized: mainProductNormalized.image_link });
+
+          // If the variant doesn't have its own image, inherit from the primary product
+          if (!variantRow.image_link) {
+            variantRow.image_link = mainProductNormalized.image_link;
+          }
+          if (!variantRow.additional_image_link) {
+            variantRow.additional_image_link = mainProductNormalized.additional_image_link;
+          }
+
+          // Validate variant before pushing
+          if (isValidRow(variantRow)) {
+            csvRows.push(variantRow);
+            addedProductOptionIDs[variantProduct._id] = true; // Mark as added
+          }
+        }
+      }
+    }
+
+    // Convert the CSV rows to a CSV string
+    const csv = Papa.unparse(csvRows);
 
     // Save the CSV to a file
     const csvFileName = "./client/public/facebook_product_catalog.csv";
@@ -58,3 +79,83 @@ export const facebook_catalog_upload = async () => {
     console.error(err);
   }
 };
+
+// Check if a row is valid (id and title must be defined)
+const isValidRow = row => {
+  return row.id && row.title;
+};
+
+// Function to normalize product data for CSV
+const normalizeProductForCSV = product => {
+  return {
+    id: product._id,
+    title: product.name,
+    description: product.description,
+    availability: "In Stock",
+    condition: "New",
+    price: `${product.price} USD`,
+    link: `https://www.glow-leds.com/collections/all/products/${product.pathname}`,
+    image_link: determineImage(product, 0),
+    additional_image_link: determineImage(product, 1),
+    brand: "Glow LEDs",
+    inventory: product.quantity,
+    fb_product_category: "toys & games > electronic toys",
+    google_product_category: "Toys & Games > Toys > Visual Toys",
+    sale_price: `${product.sale_price && product.sale_price.toFixed(2)} USD`,
+    sale_price_effective_date: `${product.sale_start_date && product.sale_start_date.slice(0, -1)}/${
+      product.sale_end_date && product.sale_end_date.slice(0, -1)
+    }`,
+    product_type: product.category,
+    color: product.color,
+    size: product.size,
+    item_group_id: null, // Add item_group_id with default null
+  };
+};
+
+// export const facebook_catalog_upload = async () => {
+//   try {
+//     // Fetch existing products from MongoDB
+//     const domainUrl = domain();
+//     const { data } = await axios.get(`${domainUrl}/api/products/facebook_catelog`);
+
+//     const new_rows = data
+//       .filter(product => !product.hidden)
+//       .filter(product => product.category !== "options")
+//       .filter(product => product.option === false)
+//       .sort((a, b) => (a.order > b.order ? 1 : -1))
+//       .map((product, i) => ({
+// id: product._id,
+// title: product.name,
+// description: product.description,
+// availability: "In Stock",
+// condition: "New",
+// price: `${product.price} USD`,
+// link: `https://www.glow-leds.com/collections/all/products/${product.pathname}`,
+// image_link: determineImage(product, 0),
+// additional_image_link: determineImage(product, 1),
+// brand: "Glow LEDs",
+// inventory: product.quantity,
+// fb_product_category: "toys & games > electronic toys",
+// google_product_category: "Toys & Games > Toys > Visual Toys",
+// sale_price: `${product.sale_price && product.sale_price.toFixed(2)} USD`,
+// sale_price_effective_date: `${product.sale_start_date && product.sale_start_date.slice(0, -1)}/${
+//   product.sale_end_date && product.sale_end_date.slice(0, -1)
+// }`,
+// product_type: product.category,
+// color: product.color,
+// size: product.size,
+//       }));
+
+//     // Convert the normalized data to CSV
+//     const csv = Papa.unparse(new_rows);
+
+//     // Save the CSV to a file
+//     const csvFileName = "./client/public/facebook_product_catalog.csv";
+//     fs.writeFile(csvFileName, csv, err => {
+//       if (err) throw err;
+//       console.log("CSV file has been saved.");
+//     });
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
