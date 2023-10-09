0cbc06fe2a5acd13891e2af5b2f488086c930ca5
Kurt LaVacque <lavacquek@icloud.com>
2023-10-09 18:30:32 +0100

    Making a multi step modal for the Edit Affiliate Modal when it was a create

--- client/src/api/affiliateApi.js
+++ client/src/api/affiliateApi.js
@@ -160,3 +160,172 @@ export const monthlyCheckin = createAsyncThunk(
     }
   }
 );
+
+// /* eslint-disable consistent-return */
+// import { createAsyncThunk } from "@reduxjs/toolkit";
+// import axios from "axios";
+// import { errorMessage } from "../helpers/sharedHelpers";
+// import { create_query } from "../utils/helper_functions";
+// import { showError, showSuccess } from "../slices/snackbarSlice";
+// import store from "../store";
+
+// import * as API from "../api";
+
+// import { handleTokenRefresh } from "./axiosInstance";
+// import { showStripeAccountLink } from "src/slices/affiliateSlice";
+
+// export const getAffiliates = async ({ search, sorting, filters, page, pageSize }) => {
+//   try {
+//     return await axios.get(`/api/affiliates`, {
+//       params: {
+//         limit: pageSize,
+//         page: page,
+//         search: search,
+//         // sort: sorting,
+//         // filters: pickBy(filters, (val) => val.length > 0)
+//       },
+//     });
+//   } catch (error) {
+//     store.dispatch(showError({ message: errorMessage(error) }));
+//   }
+// };
+
+// export const listAffiliates = createAsyncThunk(
+//   "affiliates/listAffiliates",
+//   async (query, { dispatch, rejectWithValue }) => {
+//     try {
+//       const { data } = await axios.get(`/api/affiliates?${create_query(query)}`);
+//       return data;
+//     } catch (error) {
+//       dispatch(showError({ message: errorMessage(error) }));
+//       return rejectWithValue(error.response?.data);
+//     }
+//   }
+// );
+
+// export const saveAffiliate = createAsyncThunk(
+//   "affiliates/saveAffiliate",
+//   async ({ affiliate, profile }, { dispatch, rejectWithValue, getState }) => {
+//     try {
+//       const {
+//         users: {
+//           userPage: { current_user },
+//         },
+//       } = getState();
+
+//       const newAffiliate = !affiliate._id;
+
+//       if (newAffiliate) {
+//         const { data } = await axios.post("/api/affiliates", affiliate);
+//         if (profile) {
+//           await dispatch(API.saveUser({ user: { _id: current_user._id, affiliate: data._id }, profile }));
+
+//           dispatch(showSuccess({ message: `Affiliate Created` }));
+//         }
+//         console.log({ accountLink: data.accountLink });
+//         dispatch(showStripeAccountLink({ link: data.accountLink }));
+//         return data;
+//       } else {
+//         const { data } = await axios.put(`/api/affiliates/${affiliate._id}`, affiliate);
+//         dispatch(showSuccess({ message: `Affiliate Updated` }));
+//         return data;
+//       }
+//     } catch (error) {
+//       dispatch(showError({ message: errorMessage(error) }));
+//       return rejectWithValue(error.response?.data);
+//     }
+//   }
+// );
+
+// export const detailsAffiliate = createAsyncThunk(
+//   "affiliates/detailsAffiliate",
+//   async ({ pathname, id }, { dispatch, rejectWithValue }) => {
+//     try {
+//       if (id) {
+//         const { data } = await axios.get(`/api/affiliates/${id}`);
+//         return data;
+//       } else if (pathname) {
+//         const { data } = await axios.get(`/api/affiliates/${pathname}/pathname`);
+//         return data;
+//       }
+//     } catch (error) {
+//       dispatch(showError({ message: errorMessage(error) }));
+//       return rejectWithValue(error.response?.data);
+//     }
+//   }
+// );
+
+// export const deleteAffiliate = createAsyncThunk(
+//   "affiliates/deleteAffiliate",
+//   async (id, { dispatch, rejectWithValue }) => {
+//     try {
+//       const { data } = await axios.delete(`/api/affiliates/${id}`);
+//       return data;
+//     } catch (error) {
+//       dispatch(showError({ message: errorMessage(error) }));
+//       return rejectWithValue(error.response?.data);
+//     }
+//   }
+// );
+
+// export const generateSponsorCodes = createAsyncThunk(
+//   "affiliates/generateSponsorCodes",
+//   async (id, { dispatch, rejectWithValue }) => {
+//     try {
+//       const { data } = await axios.post(`/api/affiliates/${id}/generate_sponsor_codes`);
+
+//       dispatch(showSuccess({ message: `Codes Generated` }));
+//       return data;
+//     } catch (error) {
+//       dispatch(showError({ message: errorMessage(error) }));
+//       return rejectWithValue(error.response?.data);
+//     }
+//   }
+// );
+
+// export const create_rave_mob_affiliates = createAsyncThunk(
+//   "affiliates/create_rave_mob_affiliates",
+//   async (csv, { dispatch, rejectWithValue }) => {
+//     try {
+//       const { data } = await axios.put("/api/affiliates/create_rave_mob_affiliates", { csv });
+//       return data;
+//     } catch (error) {
+//       dispatch(showError({ message: errorMessage(error) }));
+//       return rejectWithValue(error.response?.data);
+//     }
+//   }
+// );
+
+// export const affiliateEarnings = createAsyncThunk(
+//   "affiliates/affiliateEarnings",
+//   async ({ promo_code, start_date, end_date, sponsor, type }, { dispatch, rejectWithValue }) => {
+//     try {
+//       const { data } = await axios.get(
+//         `/api/orders/code_usage/${promo_code}?start_date=${start_date}&end_date=${end_date}&sponsor=${sponsor}`
+//       );
+//       return { data, type };
+//     } catch (error) {
+//       dispatch(showError({ message: errorMessage(error) }));
+//       return rejectWithValue(error.response?.data);
+//     }
+//   }
+// );
+
+// export const monthlyCheckin = createAsyncThunk(
+//   "affiliate/monthlyCheckin",
+//   async ({ affiliateId, questionsConcerns, numberOfContent, month, year }, { dispatch, rejectWithValue }) => {
+//     try {
+//       const { data } = await axios.put(`/api/affiliates/${affiliateId}/monthly_checkin`, {
+//         questionsConcerns,
+//         numberOfContent,
+//         month,
+//         year,
+//       });
+//       await handleTokenRefresh(true);
+//       return data;
+//     } catch (error) {
+//       dispatch(showError({ message: errorMessage(error) }));
+//       return rejectWithValue(error.response?.data);
+//     }
+//   }
+// );
--- client/src/pages/AffiliatesPage/components/EditAffiliateModal.jsx
+++ client/src/pages/AffiliatesPage/components/EditAffiliateModal.jsx
@@ -1,17 +1,18 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import GLActionModal from "../../../shared/GlowLEDsComponents/GLActionModal/GLActionModal";
-import { set_edit_affiliate_modal, set_affiliate } from "../../../slices/affiliateSlice";
+import { set_edit_affiliate_modal, set_affiliate, setCreateAffiliateStep } from "../../../slices/affiliateSlice";
 import * as API from "../../../api";
 import { GLForm } from "../../../shared/GlowLEDsComponents/GLForm";
 import { useLocation } from "react-router-dom";
 import { affiliateFormFields } from "./affiliateFormFields";
+import GLStepperModal from "src/shared/GlowLEDsComponents/GLStepperModal/GLStepperModal";
 
 const EditAffiliateModal = () => {
   const dispatch = useDispatch();
   const location = useLocation();
   const affiliatePage = useSelector(state => state.affiliates.affiliatePage);
-  const { edit_affiliate_modal, affiliate, loading } = affiliatePage;
+  const { edit_affiliate_modal, affiliate, loading, createAffiliateStep } = affiliatePage;
 
   const userPage = useSelector(state => state.users.userPage);
   const { users, loading: loading_users, current_user } = userPage;
@@ -43,38 +44,86 @@ const EditAffiliateModal = () => {
     promos,
   });
 
+  const stepLabels = ["Create Affiliate Account", "Create Stripe Account", "Join our Discord"];
+
   return (
     <div>
-      <GLActionModal
-        isOpen={edit_affiliate_modal}
-        onConfirm={() => {
-          dispatch(
-            API.saveAffiliate({
-              affiliate: { ...affiliate, user: affiliate?.user?._id || current_user._id },
-              profile: location.pathname === "/secure/account/profile",
-            })
-          );
-          dispatch(API.listUsers());
-        }}
-        onCancel={() => {
-          dispatch(set_edit_affiliate_modal(false));
-        }}
-        title={"Edit Affiliate"}
-        confirmLabel={"Save"}
-        confirmColor="primary"
-        cancelLabel={"Cancel"}
-        cancelColor="secondary"
-        disableEscapeKeyDown
-      >
-        {/* <Grid container spacing={2}>
-          <Grid item xs={12}> */}
-        <GLForm
-          formData={formFields}
-          state={affiliate}
-          onChange={value => dispatch(set_affiliate(value))}
-          loading={loading && loading_users && loading_products && loading_chips && loading_promos}
-        />
-      </GLActionModal>
+      {affiliate._id ? (
+        <GLActionModal
+          isOpen={edit_affiliate_modal}
+          onConfirm={() => {
+            dispatch(
+              API.saveAffiliate({
+                affiliate: { ...affiliate, user: affiliate?.user?._id || current_user._id },
+                profile: location.pathname === "/secure/account/profile",
+              })
+            );
+            dispatch(API.listUsers());
+          }}
+          onCancel={() => {
+            dispatch(set_edit_affiliate_modal(false));
+          }}
+          title={"Edit Affiliate"}
+          confirmLabel={"Save"}
+          confirmColor="primary"
+          cancelLabel={"Cancel"}
+          cancelColor="secondary"
+          disableEscapeKeyDown
+        >
+          <GLForm
+            formData={formFields}
+            state={affiliate}
+            onChange={value => dispatch(set_affiliate(value))}
+            loading={loading && loading_users && loading_products && loading_chips && loading_promos}
+          />
+        </GLActionModal>
+      ) : (
+        <GLStepperModal
+          isOpen={edit_affiliate_modal}
+          step={createAffiliateStep}
+          stepLabels={stepLabels}
+          onConfirm={() => {
+            if (createAffiliateStep === 2) {
+              dispatch(set_edit_affiliate_modal(false));
+              dispatch(API.listUsers());
+            } else if (createAffiliateStep === 1) {
+              dispatch(setCreateAffiliateStep(2));
+            } else if (createAffiliateStep === 0) {
+              dispatch(
+                API.saveAffiliate({
+                  affiliate: { ...affiliate, user: affiliate?.user?._id || current_user._id },
+                  profile: location.pathname === "/secure/account/profile",
+                })
+              );
+              dispatch(setCreateAffiliateStep(1));
+            }
+          }}
+          onCancel={() => {
+            if (createAffiliateStep === 2) {
+              dispatch(setCreateAffiliateStep(1));
+            } else if (createAffiliateStep === 1) {
+              dispatch(setCreateAffiliateStep(0));
+            } else if (createAffiliateStep === 0) {
+              dispatch(set_edit_affiliate_modal(false));
+            }
+          }}
+          title={"Edit Affiliate"}
+          confirmLabel={createAffiliateStep === 0 || createAffiliateStep === 1 ? "Next" : "Complete"}
+          cancelLabel={createAffiliateStep === 1 || createAffiliateStep === 2 ? "Back" : "Cancel"}
+          // cancelLabel={"Cancel"}
+          cancelColor="secondary"
+          disableEscapeKeyDown
+        >
+          {createAffiliateStep === 0 && (
+            <GLForm
+              formData={formFields}
+              state={affiliate}
+              onChange={value => dispatch(set_affiliate(value))}
+              loading={loading && loading_users && loading_products && loading_chips && loading_promos}
+            />
+          )}
+        </GLStepperModal>
+      )}
     </div>
   );
 };
