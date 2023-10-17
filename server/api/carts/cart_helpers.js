f47dcb4d5c217ba7dff874267856bdbfb7c7e98f
Kurt LaVacque <lavacquek@icloud.com>
2023-10-17 14:27:06 +0100

    Limited adding more than one sampler pack

--- server/api/carts/cart_helpers.js
+++ server/api/carts/cart_helpers.js
@@ -63,6 +63,14 @@ export const areCartItemsEqual = (item1, item2) => {
 export const updateCartItems = (cartItems, cart_item) => {
   let found = false;
 
+  // Check if the cart already contains 'Supreme Gloves V2 Sizing Sampler Pack'
+  const alreadyContainsSpecificProduct = cartItems.some(x => x.name.includes("Sizing Sampler Pack"));
+
+  // If adding 'Supreme Gloves V2 Sizing Sampler Pack' and it already exists in the cart, return cartItems as is
+  if (cart_item.name === "Supreme Gloves V2 Sizing Sampler Pack" && alreadyContainsSpecificProduct) {
+    return cartItems;
+  }
+
   const updatedItems = cartItems.map(x => {
     if (areCartItemsEqual(x, cart_item)) {
       found = true;
