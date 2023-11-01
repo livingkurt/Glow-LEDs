757da0a68471b6e860188535169753b760d703fb
Kurt LaVacque <lavacquek@icloud.com>
2023-11-01 11:16:59 +0100

    Cleaning up th styling on the header

--- client/src/layouts/Header/Header.jsx
+++ client/src/layouts/Header/Header.jsx
@@ -4,22 +4,19 @@ import LeftNavButtons from "./components/LeftNavButtons";
 import RightNavButtons from "./components/RightNavButtons";
 import SearchBar from "./components/SearchBar";
 import CenterNavButtons from "./components/CenterNavButtons/CenterNavButtons";
+import { Box } from "@mui/material";
 
 const Header = () => {
   return (
-    <div>
-      <div>
-        <Banner />
-        <header>
-          <div className="header">
-            <LeftNavButtons />
-            <CenterNavButtons />
-            <RightNavButtons />
-          </div>
-          <SearchBar />
-        </header>
-      </div>
-    </div>
+    <header>
+      <Banner />
+      <Box display="flex" alignItems="center" justifyContent="space-between" className="top-header">
+        <LeftNavButtons />
+        <CenterNavButtons />
+        <RightNavButtons />
+      </Box>
+      <SearchBar />
+    </header>
   );
 };
 
