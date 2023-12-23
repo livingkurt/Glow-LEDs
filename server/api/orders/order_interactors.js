47dbe165fc22e008776e2b162b17be2b7e2f306e
Kurt LaVacque <lavacquek@icloud.com>
2023-12-23 17:14:07 -0600

    HOTFIX: Fixed the getCodeUsage to be a mongoose calculation instead of javascript

--- server/api/orders/order_interactors.js
+++ server/api/orders/order_interactors.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import { isEmail } from "../../utils/util";
 import order_db from "./order_db";
+import Order from "./order";
 
 export const normalizeOrderFilters = input => {
   const output = {};
@@ -184,47 +185,147 @@ export const normalizeOrderSearch = query => {
 
 export const getCodeUsage = async ({ promo_code, start_date, end_date, sponsor, sponsorTeamCaptain }) => {
   try {
-    const sort = {};
-
-    const filter = {
+    const matchFilter = {
       deleted: false,
       isPaid: true,
-      createdAt: {
-        $gte: start_date,
-        $lte: end_date,
-      },
-      promo_code: new RegExp(promo_code, "i"),
+      createdAt: { $gte: new Date(start_date), $lte: new Date(end_date) },
+      promo_code: new RegExp(`^${promo_code}$`, "i"), // Adjusted to match the entire string
     };
 
-    const limit = "0";
-    const page = "1";
-
-    const orders = await order_db.findAll_orders_db(filter, sort, limit, page);
-
-    const number_of_uses = orders
-      .filter(order => order.promo_code)
-      .filter(order => order.promo_code.toLowerCase() === promo_code.toLowerCase()).length;
-    const revenue = orders
-      .filter(order => order.promo_code)
-      .filter(order => order.promo_code.toLowerCase() === promo_code.toLowerCase())
-      .reduce(
-        (a, order) =>
-          a +
-          order.itemsPrice -
-          (order.payment.refund ? order.payment.refund.reduce((a, c) => a + c.amount, 0) / 100 : 0),
-        0
-      );
-    let earnings = revenue * 0.1;
+    let earningsMultiplier = 0.1;
     if (sponsor === true || sponsor === "true") {
-      earnings = revenue * 0.15;
+      earningsMultiplier = 0.15;
     }
     if (sponsorTeamCaptain === true || sponsorTeamCaptain === "true") {
-      earnings = revenue * 0.2;
+      earningsMultiplier = 0.2;
     }
+
+    const aggregationPipeline = [
+      { $match: matchFilter },
+      {
+        $group: {
+          _id: null,
+          number_of_uses: { $sum: 1 },
+          revenue: { $sum: "$itemsPrice" },
+          totalRefund: { $sum: { $ifNull: [{ $sum: "$refunds.amount" }, 0] } }, // Assuming refunds are in the 'refunds' array
+        },
+      },
+      {
+        $project: {
+          number_of_uses: 1,
+          revenue: 1,
+          earnings: { $multiply: ["$revenue", earningsMultiplier] },
+          totalRefund: 1,
+        },
+      },
+      {
+        $addFields: {
+          revenue: { $subtract: ["$revenue", { $divide: ["$totalRefund", 100] }] },
+        },
+      },
+    ];
+
+    const results = await Order.aggregate(aggregationPipeline);
+    if (results.length === 0) {
+      return { number_of_uses: 0, revenue: 0, earnings: 0 };
+    }
+
+    const { number_of_uses, revenue, earnings } = results[0];
+
     return { number_of_uses, revenue, earnings };
   } catch (error) {
+    console.log({ error });
     if (error instanceof Error) {
       throw new Error(error.message);
     }
   }
 };
+
+// export const getCodeUsage = async ({ promo_code, start_date, end_date, sponsor, sponsorTeamCaptain }) => {
+//   console.log({ promo_code, start_date, end_date, sponsor, sponsorTeamCaptain });
+//   try {
+//     const sort = {};
+
+//     const filter = {
+//       deleted: false,
+//       isPaid: true,
+//       createdAt: {
+//         $gte: start_date,
+//         $lte: end_date,
+//       },
+//       promo_code: new RegExp(promo_code, "i"),
+//     };
+
+//     const limit = "0";
+//     const page = "1";
+
+//     const orders = await order_db.findAll_orders_db(filter, sort, limit, page);
+
+//     const number_of_uses = orders
+//       .filter(order => order.promo_code)
+//       .filter(order => order.promo_code.toLowerCase() === promo_code.toLowerCase()).length;
+//     const revenue = orders
+//       .filter(order => order.promo_code)
+//       .filter(order => order.promo_code.toLowerCase() === promo_code.toLowerCase())
+//       .reduce(
+//         (a, order) =>
+//           a +
+//           Number(order.itemsPrice) -
+//           (order.payment.refund ? order.payment.refund.reduce((a, c) => a + c.amount, 0) / 100 : 0),
+//         0
+//       );
+//     let earnings = revenue * 0.1;
+//     if (sponsor === true || sponsor === "true") {
+//       earnings = revenue * 0.15;
+//     }
+//     if (sponsorTeamCaptain === true || sponsorTeamCaptain === "true") {
+//       earnings = revenue * 0.2;
+//     }
+//     console.log({ number_of_uses, revenue, earnings });
+//     return { number_of_uses, revenue, earnings };
+//   } catch (error) {
+//     if (error instanceof Error) {
+//       throw new Error(error.message);
+//     }
+//   }
+// };
+
+// const aggregationPipeline1 = [
+//   { $match: matchFilter },
+//   { $sort: { itemsPrice: -1 } }, // Sort by itemsPrice in descending order
+//   { $limit: 1 }, // Limit to 1 document to get the highest itemsPrice document
+//   {
+//     $project: {
+//       _id: 1, // Include the _id field
+//       itemsPrice: 1, // Include the itemsPrice for confirmation
+//     },
+//   },
+// ];
+
+// const highestPriceResult = await Order.aggregate(aggregationPipeline1);
+// console.log(JSON.stringify(highestPriceResult, null, 2));
+// // const aggregationPipeline1 = [
+// //   { $match: matchFilter },
+// //   { $sort: { itemsPrice: -1 } }, // Sort by itemsPrice to see the highest values
+// //   { $limit: 10 }, // Limit to 10 documents for easier inspection
+// //   {
+// //     $group: {
+// //       _id: 1,
+// //       number_of_uses: { $sum: 1 },
+// //       totalRevenue: { $sum: "$itemsPrice" },
+// //       highestItemPrice: { $max: "$itemsPrice" }, // Check the highest itemsPrice
+// //       sampleOrders: { $push: "$$ROOT" }, // Push sample documents for inspection
+// //     },
+// //   },
+// //   {
+// //     $project: {
+// //       number_of_uses: 1,
+// //       totalRevenue: 1,
+// //       highestItemPrice: 1,
+// //       // sampleOrders: { $slice: ["$sampleOrders", 5] }, // Limit to 5 sample documents
+// //     },
+// //   },
+// // ];
+
+// const debugResults = await Order.aggregate(aggregationPipeline1);
+// console.log(JSON.stringify(debugResults, null, 2));
