6f97df8037b74602dcd2fdccaf7806848d591b3f
Kurt LaVacque <lavacquek@icloud.com>
2024-02-12 17:18:48 -0600

    Refactored and Restyled the OrderDropdown to use MUI V5 Button and Grid

--- client/src/pages/OrdersPage/components/MetaDataDisplay.jsx
+++ client/src/pages/OrdersPage/components/MetaDataDisplay.jsx
@@ -1,11 +1,11 @@
 import { determine_tracking_link } from "../ordersPageHelpers";
-import { GLButton } from "../../../shared/GlowLEDsComponents";
 import { useDispatch } from "react-redux";
 import { API_Emails } from "../../../utils";
 import { toCapitalize } from "../../../utils/helper_functions";
 import { set_loading_label } from "../../../slices/orderSlice";
 import config from "../../../config";
 import * as API from "../../../api";
+import { Button, Grid } from "@mui/material";
 
 const MetaDataDisplay = ({ row }) => {
   const dispatch = useDispatch();
@@ -32,7 +32,11 @@ const MetaDataDisplay = ({ row }) => {
   const send_order_email = async () => {
     dispatch(set_loading_label(true));
     await API_Emails.send_order_email(row, "Thank you for your Glow LEDs Order", row.shipping.email);
-    await API_Emails.send_order_email(row, "New Order Created by " + row.shipping.first_name, config.REACT_APP_INFO_EMAIL);
+    await API_Emails.send_order_email(
+      row,
+      "New Order Created by " + row.shipping.first_name,
+      config.REACT_APP_INFO_EMAIL
+    );
 
     dispatch(set_loading_label(false));
   };
@@ -40,18 +44,31 @@ const MetaDataDisplay = ({ row }) => {
   const send_refund_email = async () => {
     dispatch(set_loading_label(true));
     await API_Emails.send_refund_email(row, "Refund Successful", row.shipping.email, true);
-    await API_Emails.send_refund_email(row, "New Refunded for " + row.shipping.first_name, config.REACT_APP_INFO_EMAIL, true);
+    await API_Emails.send_refund_email(
+      row,
+      "New Refunded for " + row.shipping.first_name,
+      config.REACT_APP_INFO_EMAIL,
+      true
+    );
 
     dispatch(set_loading_label(false));
   };
   return (
-    <div>
-      <h3 className="fs-20px mv-5px">Meta Data</h3>
-      <div>ID #: {row._id}</div>
-      <div>Payment Method: {row?.payment?.paymentMethod}</div>
-      <div>Promo Code: {row.promo_code}</div>
+    <Grid container spacing={1}>
+      <Grid item xs={12}>
+        <h3 className="fs-20px mv-5px">Meta Data</h3>
+      </Grid>
+      <Grid item xs={12}>
+        <div>ID #: {row._id}</div>
+      </Grid>
+      <Grid item xs={12}>
+        <div>Payment Method: {row?.payment?.paymentMethod}</div>
+      </Grid>
+      <Grid item xs={12}>
+        <div>Promo Code: {row.promo_code}</div>
+      </Grid>
       {row.tracking_number && (
-        <div>
+        <Grid item xs={12}>
           <label className="phrase_font">Tracking Number: </label>
 
           <a
@@ -61,41 +78,49 @@ const MetaDataDisplay = ({ row }) => {
             className="mv-2rem"
             style={{
               textDecoration: "underline",
-              color: "white"
+              color: "white",
             }}
           >
             {row.tracking_number}
           </a>
-        </div>
+        </Grid>
       )}
       {row.return_tracking_number && (
-        <div>
+        <Grid item xs={12}>
           <label className="phrase_font">Return Tracking Number: </label>
 
           <a
-            href={row.return_tracking_url ? row.return_tracking_url : determine_tracking_link(row.return_tracking_number)}
+            href={
+              row.return_tracking_url ? row.return_tracking_url : determine_tracking_link(row.return_tracking_number)
+            }
             target="_blank"
             rel="noopener noreferrer"
             className="mv-2rem"
             style={{
               textDecoration: "underline",
-              color: "white"
+              color: "white",
             }}
           >
             {row.return_tracking_number}
           </a>
-        </div>
+        </Grid>
       )}
-      <GLButton variant="secondary" className="mv-5px w-100per" onClick={() => send_order_email()}>
-        Send Order Email
-      </GLButton>
-      <GLButton variant="secondary" className="mv-5px w-100per" onClick={() => send_order_status_email("updated")}>
-        Send Update Order Email
-      </GLButton>
-      <GLButton variant="secondary" className="mv-5px w-100per" onClick={() => send_refund_email()}>
-        Send Refund Email
-      </GLButton>
-    </div>
+      <Grid item xs={12}>
+        <Button color="secondary" variant="contained" fullWidth onClick={() => send_order_email()}>
+          Send Order Email
+        </Button>
+      </Grid>
+      <Grid item xs={12}>
+        <Button color="secondary" variant="contained" fullWidth onClick={() => send_order_status_email("updated")}>
+          Send Update Order Email
+        </Button>
+      </Grid>
+      <Grid item xs={12}>
+        <Button color="secondary" variant="contained" fullWidth onClick={() => send_refund_email()}>
+          Send Refund Email
+        </Button>
+      </Grid>
+    </Grid>
   );
 };
 
