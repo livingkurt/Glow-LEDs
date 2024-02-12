43662fae580d466a47760670fd4d7539e95a5c54
Kurt LaVacque <57471593+livingkurt@users.noreply.github.com>
2024-02-12 18:22:13 -0500

    GL-3655-Disable-Status-Buttons-Unless-Label-Exists (#189)
    GL-3655-Disable-Status-Buttons-Unless-Label-Exists (#189)

--- client/src/pages/OrdersPage/components/OrderActionButtons.jsx
+++ client/src/pages/OrdersPage/components/OrderActionButtons.jsx
@@ -1,4 +1,3 @@
-import { GLButton } from "../../../shared/GlowLEDsComponents";
 import { useDispatch, useSelector } from "react-redux";
 import * as API from "../../../api";
 import { API_Orders } from "../../../utils";
@@ -6,8 +5,8 @@ import { Loading } from "../../../shared/SharedComponents";
 import { printInvoice, printLabel } from "../ordersPageHelpers";
 import { openLinkLabelModal } from "../../../slices/shippingSlice";
 import { openShippingModal, set_order } from "../../../slices/orderSlice";
-import GenerateCSVLabel from "./GenerateCSVLabel";
 import { showSuccess } from "../../../slices/snackbarSlice";
+import { Button, Grid } from "@mui/material";
 
 const OrderActionButtons = ({ order }) => {
   const dispatch = useDispatch();
@@ -16,165 +15,192 @@ const OrderActionButtons = ({ order }) => {
   const { loading_label } = shippingSlice;
 
   return (
-    <div className="">
-      <h3 className="fs-20px mv-5px">Actions</h3>
+    <div>
       <Loading loading={loading_label} />
-      {order.shipping.shipping_label && (
-        <GLButton
-          variant="primary"
-          onClick={() => printLabel(order.shipping.shipping_label.postage_label.label_url)}
-          className="w-100per mv-5px"
-        >
-          Print Label
-        </GLButton>
-      )}
-      <GLButton
-        variant="primary"
-        onClick={async () => {
-          const { data: invoice } = await API_Orders.get_invoice(order._id);
-          printInvoice(invoice);
-        }}
-        className="w-100per mv-5px"
-      >
-        Print Invoice
-      </GLButton>
-      {order.shipping.shipment_id !== null && !order.shipping.shipping_label && (
-        <GLButton
-          variant="primary"
-          onClick={() => dispatch(API.buyLabel({ orderId: order._id }))}
-          className="w-100per mv-5px"
-        >
-          Buy Label
-        </GLButton>
-      )}
-      {/* {order.shipping.shipping_label && (
-        <GLButton
-          variant="secondary"
-          className="w-100per mv-5px"
-          onClick={() => dispatch(API.createLabel({ orderId: order._id, speed: "first" }))}
-        >
-          Replace Shipping Label
-        </GLButton>
-      )} */}
-      {order.shipping.shipping_label && (
-        <GLButton
-          variant="secondary"
-          onClick={() => {
-            const confirm = window.confirm("Are you sure you want REFUND the LABEL for this order?");
-            if (confirm) {
-              dispatch(API.refundLabel({ orderId: order._id, isReturnTracking: false }));
-            }
-          }}
-          className="w-100per mv-5px"
-        >
-          Refund Label
-        </GLButton>
-      )}
-
-      <GLButton
-        variant={order.shipping.shipment_id === null ? "primary" : "secondary"}
-        className="w-100per mv-5px"
-        onClick={() => dispatch(openShippingModal(order))}
-      >
-        Choose New Shipping Rate
-      </GLButton>
-
-      <GLButton
-        variant="secondary"
-        className="w-100per mv-5px"
-        onClick={() => {
-          const confirm = window.confirm(
-            "Are you sure you want CLEAR the LABEL for this order, if you have purchased the label you will need to manually refund it?"
-          );
-          if (confirm) {
-            dispatch(
-              API.saveOrder({
-                ...order,
-                shipping: {
-                  ...order.shipping,
-                  shipment_id: null,
-                  shipping_rate: null,
-                  shipment_tracker: null,
-                  shipping_label: null,
-                  return_shipment_id: null,
-                  return_shipping_rate: null,
-                  return_shipment_tracker: null,
-                  return_shipping_label: null,
-                },
-              })
-            );
-            dispatch(showSuccess({ message: `Label and Tracking Cleared for Order` }));
-          }
-        }}
-      >
-        Clear Shipping Label
-      </GLButton>
-      <GLButton
-        variant="secondary"
-        className="w-100per mv-5px"
-        onClick={() => {
-          dispatch(API.getShipments());
-          dispatch(set_order(order));
-          dispatch(openLinkLabelModal());
-        }}
-      >
-        Link Order to Label
-      </GLButton>
-      {!order.shipping.return_shipping_label && (
-        <GLButton
-          variant="secondary"
-          className="w-100per mv-5px"
-          onClick={() => dispatch(API.createReturnLabel({ orderId: order._id }))}
-        >
-          Buy Return Label
-        </GLButton>
-      )}
-      {!order.shipping.return_shipping_label && (
-        <GLButton
-          variant="secondary"
-          className="w-100per mv-5px"
-          onClick={() => dispatch(API.createReturnLabel({ orderId: order._id, returnToHeadquarters: true }))}
-        >
-          Buy Return Label To Headquarters
-        </GLButton>
-      )}
-      {order.shipping.return_shipping_label && (
-        <GLButton
-          variant="secondary"
-          className="w-100per mv-5px"
-          onClick={() => printLabel(order.shipping.return_shipping_label.postage_label.label_url)}
-        >
-          Print Return Label
-        </GLButton>
-      )}
-      {order.shipping.return_shipping_label && (
-        <GLButton
-          variant="secondary"
-          className="w-100per mv-5px"
-          onClick={() => {
-            const confirm = window.confirm("Are you sure you want REFUND the RETURN LABEL for this order?");
-            if (confirm) {
-              dispatch(API.refundLabel({ orderId: order._id, isReturnTracking: true }));
-            }
-          }}
-        >
-          Refund Return Label
-        </GLButton>
-      )}
-      {order.shipping.return_shipping_label && (
-        <a
-          href={order.shipping.return_shipping_label.postage_label.label_url}
-          style={{ width: "100%" }}
-          target="_blank"
-          rel="noreferrer"
-          download={order.shipping.return_shipping_label.postage_label.label_url}
-        >
-          <GLButton variant="secondary" className="mv-5px w-100per">
-            Download Return Label
-          </GLButton>
-        </a>
-      )}
-      <GenerateCSVLabel order={order} />
+      <Grid container spacing={1}>
+        <Grid item xs={12}>
+          <h3 className="fs-20px mv-5px">Actions</h3>
+        </Grid>
+        {order.shipping.shipping_label && (
+          <Grid item xs={12}>
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={() => printLabel(order.shipping.shipping_label.postage_label.label_url)}
+              className="w-100per mv-5px"
+            >
+              Print Label
+            </Button>
+          </Grid>
+        )}
+        <Grid item xs={12}>
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={async () => {
+              const { data: invoice } = await API_Orders.get_invoice(order._id);
+              printInvoice(invoice);
+            }}
+            className="w-100per mv-5px"
+          >
+            Print Invoice
+          </Button>
+        </Grid>
+        {order.shipping.shipment_id !== null && !order.shipping.shipping_label && (
+          <Grid item xs={12}>
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={() => dispatch(API.buyLabel({ orderId: order._id }))}
+              className="w-100per mv-5px"
+            >
+              Buy Label
+            </Button>
+          </Grid>
+        )}
+        {order.shipping.shipping_label && (
+          <Grid item xs={12}>
+            <Button
+              color="secondary"
+              variant="contained"
+              onClick={() => {
+                const confirm = window.confirm("Are you sure you want REFUND the LABEL for this order?");
+                if (confirm) {
+                  dispatch(API.refundLabel({ orderId: order._id, isReturnTracking: false }));
+                }
+              }}
+              className="w-100per mv-5px"
+            >
+              Refund Label
+            </Button>
+          </Grid>
+        )}
+        <Grid item xs={12}>
+          <Button
+            color={order.shipping.shipment_id === null ? "primary" : "secondary"}
+            variant="contained"
+            className="w-100per mv-5px"
+            onClick={() => dispatch(openShippingModal(order))}
+          >
+            Choose New Shipping Rate
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            color="secondary"
+            variant="contained"
+            className="w-100per mv-5px"
+            onClick={() => {
+              const confirm = window.confirm(
+                "Are you sure you want CLEAR the LABEL for this order, if you have purchased the label you will need to manually refund it?"
+              );
+              if (confirm) {
+                dispatch(
+                  API.saveOrder({
+                    ...order,
+                    shipping: {
+                      ...order.shipping,
+                      shipment_id: null,
+                      shipping_rate: null,
+                      shipment_tracker: null,
+                      shipping_label: null,
+                      return_shipment_id: null,
+                      return_shipping_rate: null,
+                      return_shipment_tracker: null,
+                      return_shipping_label: null,
+                    },
+                  })
+                );
+                dispatch(showSuccess({ message: `Label and Tracking Cleared for Order` }));
+              }
+            }}
+          >
+            Clear Shipping Label
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            color="secondary"
+            variant="contained"
+            className="w-100per mv-5px"
+            onClick={() => {
+              dispatch(API.getShipments());
+              dispatch(set_order(order));
+              dispatch(openLinkLabelModal());
+            }}
+          >
+            Link Order to Label
+          </Button>
+        </Grid>
+        {!order.shipping.return_shipping_label && (
+          <Grid item xs={12}>
+            <Button
+              color="secondary"
+              variant="contained"
+              className="w-100per mv-5px"
+              onClick={() => dispatch(API.createReturnLabel({ orderId: order._id }))}
+            >
+              Buy Return Label Production
+            </Button>
+          </Grid>
+        )}
+        {!order.shipping.return_shipping_label && (
+          <Grid item xs={12}>
+            <Button
+              color="secondary"
+              variant="contained"
+              className="w-100per mv-5px"
+              onClick={() => dispatch(API.createReturnLabel({ orderId: order._id, returnToHeadquarters: true }))}
+            >
+              Buy Return Label To HQ
+            </Button>
+          </Grid>
+        )}
+        {order.shipping.return_shipping_label && (
+          <Grid item xs={12}>
+            <Button
+              color="secondary"
+              variant="contained"
+              className="w-100per mv-5px"
+              onClick={() => printLabel(order.shipping.return_shipping_label.postage_label.label_url)}
+            >
+              Print Return Label
+            </Button>
+          </Grid>
+        )}
+        {order.shipping.return_shipping_label && (
+          <Grid item xs={12}>
+            <Button
+              color="secondary"
+              variant="contained"
+              className="w-100per mv-5px"
+              onClick={() => {
+                const confirm = window.confirm("Are you sure you want REFUND the RETURN LABEL for this order?");
+                if (confirm) {
+                  dispatch(API.refundLabel({ orderId: order._id, isReturnTracking: true }));
+                }
+              }}
+            >
+              Refund Return Label
+            </Button>
+          </Grid>
+        )}
+        {order.shipping.return_shipping_label && (
+          <Grid item xs={12}>
+            <a
+              href={order.shipping.return_shipping_label.postage_label.label_url}
+              style={{ width: "100%" }}
+              target="_blank"
+              rel="noreferrer"
+              download={order.shipping.return_shipping_label.postage_label.label_url}
+            >
+              <Button color="secondary" variant="contained" className="mv-5px w-100per">
+                Download Return Label
+              </Button>
+            </a>
+          </Grid>
+        )}
+      </Grid>
     </div>
   );
 };
