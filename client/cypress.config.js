a7c16373c9491a80e0427eb422adf3f4432bdca9
Kurt LaVacque <lavacquek@icloud.com>
2024-02-16 20:13:28 -0600

    Update Cypress configuration and add e2e support file

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -1,21 +1,15 @@
-const isCI = process.env.CI === "true";
+const { defineConfig } = require("cypress");
 
-const config = {
+module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: "http://localhost:3000",
+    supportFile: "cypress/support/e2e.js", // Make sure this path is correct
   },
   projectId: "ubcka4",
   chromeWebSecurity: false,
-};
-
-config.e2e.baseUrl = "http://localhost:3000"; // Replace with your CI server URL
-// if (isCI) {
-// } else {
-//   config.e2e.baseUrl = "http://localhost:3000"; // Your local server URL
-// }
-
-module.exports = config;
+});
