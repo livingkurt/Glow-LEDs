3ba6b17feae16659da36c5c5e1cf7e372aee4c9f
Kurt LaVacque <57471593+livingkurt@users.noreply.github.com>
2024-06-08 17:23:25 -0700

    GL-3998-Content-Width-Style-Update (#230)
    GL-3998-Content-Width-Style-Update (#230)

--- client/src/layouts/Content.jsx
+++ client/src/layouts/Content.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Content = ({ style, children }) => {
   return (
-    <main style={style} className="content">
-      {children}
+    <main style={style} className="outer-content">
+      <div className="content">{children}</div>
     </main>
   );
 };
