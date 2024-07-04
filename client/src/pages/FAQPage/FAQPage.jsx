1a646cffa0963165f92b4efb29a1a089b6ce77b3
Kurt LaVacque <lavacquek@icloud.com>
2024-07-03 20:34:33 -0500

    Added Accordians to the FAQ Page

--- client/src/pages/FAQPage/FAQPage.jsx
+++ client/src/pages/FAQPage/FAQPage.jsx
@@ -1,7 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { Container, Typography, Box, Button, Divider } from "@mui/material";
+import {
+  Container,
+  Typography,
+  Box,
+  Button,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Autocomplete,
+  TextField,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Filter from "../../shared/GlowLEDsComponents/GLTable/Filter";
 import * as API from "../../api";
 import { update_products_url } from "../../utils/helper_functions";
@@ -27,8 +38,7 @@ const FAQPage = () => {
     return () => (clean = false);
   }, []);
 
-  const filterHandler = e => {
-    const chip_selected = JSON.parse(e.target.value);
+  const filterHandler = chip_selected => {
     update_products_url(navigate, "", "", chip_selected.name, "", "0", "/collections/all/products");
     dispatch(
       API.listProducts({
@@ -46,56 +56,54 @@ const FAQPage = () => {
         {faqPage?.title}
       </Typography>
       <Box>
-        <Box>
-          {faqPage?.sections.map((section, index) => (
-            <>
-              <Box key={index} sx={{ margin: "10px 0" }}>
-                <Typography gutterBottom variant="h4" align="center">
-                  {section.title}
-                </Typography>
-                {section.video && <HeroVideo video={section.video} />}
-                {section.subsections.map((subsection, subIndex) => (
-                  <Box key={subIndex}>
-                    <Typography gutterBottom variant="h5">
-                      {subsection.title}
-                    </Typography>
-                    <Typography gutterBottom variant="body3" paragraph>
-                      {subsection.description}
-                    </Typography>
-                    {subsection.video && <HeroVideo video={subsection.video} />}
-                  </Box>
-                ))}
-                {section.button_text && section.button_link && (
-                  <Link to={section.button_link}>
-                    <Box className="jc-c">
-                      <Button variant="contained" color="primary" style={{ margin: "auto" }}>
-                        {section.button_text}
-                      </Button>
-                    </Box>
-                  </Link>
-                )}
-              </Box>
-              <Divider sx={{ backgroundColor: "white", my: 3 }} />
-            </>
-          ))}
-        </Box>
-      </Box>
-      <Box sx={{ margin: "10px 0" }}>
-        <Typography gutterBottom variant="h4" align="center">
-          Chip Compatibility
-        </Typography>
-        <Typography gutterBottom variant="body3" paragraph>
-          Select your chip from the dropdown below to see what products are compatible!
-        </Typography>
-        <Box sx={{ marginLeft: "-5px" }}>
-          <Filter
-            title="Shop By Chip"
-            width="100per"
-            state={chip_name}
-            filterHandler={filterHandler}
-            filter_options={chips_list}
-          />
-        </Box>
+        {faqPage?.sections.map((section, index) => (
+          <Accordion key={index}>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography variant="h4">{section.title}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              {section.video && <HeroVideo video={section.video} />}
+              {section.subsections.map((subsection, subIndex) => (
+                <Box key={subIndex}>
+                  <Typography gutterBottom variant="h5">
+                    {subsection.title}
+                  </Typography>
+                  <Typography gutterBottom variant="body1" paragraph>
+                    {subsection.description}
+                  </Typography>
+                  {subsection.video && <HeroVideo video={subsection.video} />}
+                </Box>
+              ))}
+              {section.button_text && section.button_link && (
+                <Box textAlign="center" mt={2}>
+                  <Button variant="contained" color="primary" component={Link} to={section.button_link}>
+                    {section.button_text}
+                  </Button>
+                </Box>
+              )}
+            </AccordionDetails>
+          </Accordion>
+        ))}
+        <Accordion>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography variant="h4">Chip Compatibility</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography gutterBottom variant="body1" paragraph>
+              Select your chip from the dropdown below to see what products are compatible!
+            </Typography>
+            <Autocomplete
+              options={chips_list || []}
+              getOptionLabel={option => option.name}
+              value={chip_name}
+              onChange={(event, newValue) => {
+                filterHandler(newValue);
+              }}
+              renderInput={params => <TextField {...params} label={"Filter By Chip"} variant="outlined" />}
+              sx={{ width: 221 }}
+            />
+          </AccordionDetails>
+        </Accordion>
       </Box>
     </Container>
   );
