c2d9fee3a16c98a00c77775896c974ee91123554
Kurt LaVacque <lavacquek@icloud.com>
2024-07-16 20:32:39 -0500

    Updated the Cart to make it easier to see the options

--- client/src/layouts/Cart/Cart.jsx
+++ client/src/layouts/Cart/Cart.jsx
@@ -1,44 +1,147 @@
 import React, { useCallback } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { determineItemsTotal } from "../../utils/helper_functions";
-import { GLTooltip } from "../../shared/GlowLEDsComponents";
-import { CartItem, RecentlyViewed, TopCategories } from "./components";
-import { checkoutHandler, determine_wholesale_proceed } from "./cartHelpers";
-import { Drawer, Typography } from "@mui/material";
-import { setCartDrawer } from "../../slices/cartSlice";
+import {
+  Drawer,
+  Box,
+  Typography,
+  IconButton,
+  Grid,
+  Chip,
+  Button,
+  Divider,
+  List,
+  ListItem,
+  useTheme,
+} from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
-import GLIconButton from "../../shared/GlowLEDsComponents/GLIconButton/GLIconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { determineItemsTotal } from "../../utils/helper_functions";
+import { setCartDrawer } from "../../slices/cartSlice";
+import { checkoutHandler, determine_wholesale_proceed } from "./cartHelpers";
+import * as API from "../../api";
+import { sale_price_switch } from "../../utils/react_helper_functions";
+import { RecentlyViewed, TopCategories } from "./components";
 import { getCartQuantity } from "../../helpers/sharedHelpers";
-import GLButtonV2 from "../../shared/GlowLEDsComponents/GLButtonV2/GLButtonV2";
+import GLSelect from "../../shared/GlowLEDsComponents/GLSelect/GLSelect";
+import GLIconButton from "../../shared/GlowLEDsComponents/GLIconButton/GLIconButton";
 
 const Cart = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const userPage = useSelector(state => state.users.userPage);
-  const { current_user } = userPage;
-
-  const cartPage = useSelector(state => state.carts.cartPage);
-  const { my_cart, cartDrawer } = cartPage;
+  const { current_user } = useSelector(state => state.users.userPage);
+  const { my_cart, cartDrawer } = useSelector(state => state.carts.cartPage);
   const { cartItems } = my_cart;
+  const { contents } = useSelector(state => state.contents.contentPage);
 
-  const contentPage = useSelector(state => state.contents.contentPage);
-  const { contents } = contentPage;
-
-  const closeMenu = () => {
-    dispatch(setCartDrawer(false));
-  };
+  const closeMenu = useCallback(() => dispatch(setCartDrawer(false)), [dispatch]);
 
   const handleCheckout = useCallback(
     () => checkoutHandler(dispatch, navigate, current_user, closeMenu),
-    [dispatch, current_user]
+    [dispatch, navigate, current_user, closeMenu]
   );
 
   const wholesaleProceed = useCallback(
     () => determine_wholesale_proceed(current_user, cartItems),
     [current_user, cartItems]
   );
+
+  const CartItem = ({ item, index }) => {
+    const theme = useTheme();
+
+    return (
+      <ListItem
+        divider
+        sx={{
+          py: 2,
+          "&.MuiListItem-divider": {
+            borderColor: "white", // This sets the divider color to white
+          },
+        }}
+      >
+        <Grid container spacing={2} alignItems="center" flexWrap="nowrap">
+          <Grid item>
+            <Link to={`/collections/all/products/${item.pathname}`}>
+              <Box
+                component="img"
+                src={item?.display_image}
+                alt={item.name}
+                sx={{ width: 80, height: 80, borderRadius: 2 }}
+              />
+            </Link>
+          </Grid>
+          <Grid item xs container direction="column" spacing={1}>
+            <Grid item>
+              <Typography variant="subtitle1" component={Link} to={`/collections/all/products/${item.pathname}`}>
+                {item.name}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Box sx={{ display: "flex", flexWrap: "wrap", gap: 1 }}>
+                {item.selectedOptions?.map((option, optionIndex) => {
+                  const bgColor = option.colorCode || theme.palette.background.default;
+                  return (
+                    <Chip
+                      key={optionIndex}
+                      label={`${item.currentOptions[optionIndex].name}: ${option.name}`}
+                      size="small"
+                      sx={{
+                        backgroundColor: bgColor,
+                        color: theme.palette.getContrastText(bgColor),
+                        fontSize: "1rem",
+                        fontWeight: "500",
+                      }}
+                    />
+                  );
+                })}
+              </Box>
+            </Grid>
+            <Grid item>
+              <Typography variant="body2">
+                {sale_price_switch({
+                  product: item,
+                  cartItem: true,
+                  background: "light",
+                  isWholesaler: current_user?.isWholesaler,
+                })}
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <GLSelect
+              value={item.quantity}
+              onChange={e => {
+                const updatedCartItems = [...my_cart.cartItems];
+                const itemIndex = updatedCartItems.findIndex(cartItem => cartItem._id === item._id);
+                updatedCartItems[itemIndex] = {
+                  ...updatedCartItems[itemIndex],
+                  quantity: parseInt(e.target.value),
+                };
+                dispatch(API.updateQuantity({ ...my_cart, cartItems: updatedCartItems }));
+              }}
+              size="small"
+              options={[...Array(current_user?.isWholesaler ? 500 : item.max_quantity).keys()].map(value => ({
+                name: value + 1,
+              }))}
+              width="70px"
+              getOptionLabel={option => option.name}
+              valueKey="name"
+            />
+            <GLIconButton
+              onClick={() => dispatch(API.deleteCartItem({ item_index: index, type: "add_to_cart" }))}
+              size="small"
+              sx={{ mt: 2 }}
+              tooltip="Remove"
+            >
+              <DeleteIcon color="white" />
+            </GLIconButton>
+          </Grid>
+        </Grid>
+      </ListItem>
+    );
+  };
 
   return (
     <Drawer
@@ -48,108 +151,98 @@ const Cart = () => {
         if (event.type === "keydown" && (event.key === "Tab" || event.key === "Shift")) {
           return;
         }
-        dispatch(setCartDrawer(false));
+        closeMenu();
       }}
-      transitionDuration={{ enter: 700, exit: 700 }}
+      PaperProps={{
+        sx: { width: { xs: "100%", sm: 600 }, backgroundColor: "#333333", color: "white" },
+      }}
     >
-      <div style={{ backgroundColor: "#333333", height: "100%", color: "white" }}>
-        <div
-          className={`column jc-b `}
-          style={{ minHeight: "100vh", padding: 0, listStyleType: "none", marginRight: "10px", margin: 0 }}
-        >
-          <div>
-            <li
-              style={{
-                display: "flex",
-                paddingBottom: "1rem",
-                marginBottom: "1rem",
-                borderBottom: "0.1rem #c0c0c0 solid",
+      <Box sx={{ display: "flex", flexDirection: "column", height: "100%" }}>
+        <Box sx={{ p: 2, display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <ShoppingCartIcon sx={{ mr: 1 }} />
+            <Typography variant="h6">Cart ({getCartQuantity(cartItems)})</Typography>
+          </Box>
+          <IconButton onClick={closeMenu} color="inherit">
+            <CloseIcon />
+          </IconButton>
+        </Box>
+        <Divider color="white" />
+        <Box sx={{ flexGrow: 1, overflowY: "auto" }}>
+          {cartItems && cartItems.length === 0 ? (
+            <Box sx={{ p: 2, textAlign: "center" }}>
+              <Typography variant="body1">Cart is Empty</Typography>
+              <RecentlyViewed closeMenu={closeMenu} />
+              <TopCategories
+                category_items={contents.length > 0 && contents[0].home_page?.slideshow}
+                closeMenu={closeMenu}
+              />
+            </Box>
+          ) : (
+            <List>
+              {cartItems?.map((item, index) => (
+                <CartItem key={index} item={item} index={index} />
+              ))}
+            </List>
+          )}
+        </Box>
+        <Divider color="white" />
+        <Box sx={{ p: 2 }}>
+          <Box display={"flex"} justifyContent={"space-between"} sx={{ mb: 1 }}>
+            <Typography variant="subtitle1">Total</Typography>
+            <Typography variant="subtitle1">
+              ${determineItemsTotal(cartItems, current_user?.isWholesaler).toFixed(2)} USD
+            </Typography>
+          </Box>
+          <Typography variant="body2" sx={{ mb: 2 }}>
+            Taxes and shipping calculated at checkout
+          </Typography>
+          <Box display={"flex"} justifyContent={"space-between"} gap={2} sx={{ mb: 2 }}>
+            <Button
+              variant="contained"
+              color="secondary"
+              fullWidth
+              onClick={closeMenu}
+              component={Link}
+              to="/checkout/cart"
+              sx={{
+                fontSize: "1.6rem",
+                padding: 1.5,
               }}
             >
-              <div className="p-1rem w-100per">
-                <div className="jc-b">
-                  <div className="logo_text ai-c">
-                    <Link to="/" aria-label="Home Page">
-                      <div className="h-50px w-50px">
-                        <img
-                          className="zoom logo_s"
-                          src="/images/optimized_images/logo_images/glow_logo_optimized.png"
-                          alt="Glow LEDs Logo"
-                          title="Small Logo"
-                        />
-                      </div>
-                    </Link>
-                    <Link to="/" aria-label="Home Page">
-                      <Typography variant="h5">Shopping Cart</Typography>
-                    </Link>
-                  </div>
-
-                  <div>
-                    <GLIconButton tooltip="Close" onClick={closeMenu}>
-                      <CloseIcon color="white" />
-                    </GLIconButton>
-                  </div>
-                </div>
-              </div>
-            </li>
-            {cartItems && cartItems.length === 0 ? (
-              <div className="p-1rem ta-c w-100per max-w-500px">
-                <div className="ta-c w-100per">Cart is Empty</div>
-                <RecentlyViewed closeMenu={closeMenu} />
-                <TopCategories
-                  category_items={contents.length > 0 && contents[0].home_page?.slideshow}
-                  closeMenu={closeMenu}
-                />
-              </div>
-            ) : (
-              <div
-                style={{
-                  overflowY: "scroll",
+              View Cart
+            </Button>
+            {current_user?.isWholesaler ? (
+              <Button
+                variant="contained"
+                color="primary"
+                fullWidth
+                onClick={handleCheckout}
+                disabled={!wholesaleProceed()}
+                sx={{
+                  fontSize: "1.6rem",
+                  padding: 1.5,
                 }}
               >
-                {cartItems &&
-                  cartItems.map((item, index) => (
-                    <CartItem key={index} item={item} index={index} dispatch={dispatch} current_user={current_user} />
-                  ))}
-                <RecentlyViewed closeMenu={closeMenu} />
-              </div>
-            )}
-          </div>
-          <div
-            className="column w-100per add_to_cart ph-1rem br-20px"
-            style={{ bottom: cartItems.length === 0 ? "-10px" : "0px" }}
-          >
-            <label className="fs-17px title_font mv-1rem">
-              Subtotal ( {cartItems && getCartQuantity(cartItems)} items ) : ${" "}
-              {determineItemsTotal(cartItems, current_user?.isWholesaler).toFixed(2)}
-            </label>
-            <Link to="/checkout/cart" className="w-100per">
-              <GLButtonV2 color="secondary" className=" w-100per mb-2rem" onClick={closeMenu} aria-label="Delete">
-                View Cart
-              </GLButtonV2>
-            </Link>
-            {current_user?.isWholesaler ? (
-              <GLTooltip
-                tooltip={!wholesaleProceed() && "You must meet your minimum order requirement to continue"}
-                className="w-100per"
-              >
-                <GLButtonV2
-                  onClick={handleCheckout}
-                  disabled={!wholesaleProceed()}
-                  color={"primary"}
-                  className={`w-100per mb-1rem ${wholesaleProceed() ? "bob" : "disabled"}`}
-                >
-                  Proceed to Checkout
-                </GLButtonV2>
-              </GLTooltip>
-            ) : (
-              <GLButtonV2 onClick={handleCheckout} color="primary" className="w-100per bob">
                 Proceed to Checkout
-              </GLButtonV2>
+              </Button>
+            ) : (
+              <Button
+                variant="contained"
+                color="primary"
+                fullWidth
+                onClick={handleCheckout}
+                sx={{
+                  fontSize: "1.6rem",
+                  padding: 1.5,
+                }}
+              >
+                Proceed to Checkout
+              </Button>
             )}
-          </div>
-        </div>
-      </div>
+          </Box>
+        </Box>
+      </Box>
     </Drawer>
   );
 };
