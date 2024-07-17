5dc2cf4a813f1a76fe48036cdd813dd2c79562df
Kurt LaVacque <lavacquek@icloud.com>
2024-07-17 08:55:28 -0500

    Removed console log

--- client/src/pages/ProductPage/components/InTheBox.jsx
+++ client/src/pages/ProductPage/components/InTheBox.jsx
@@ -3,7 +3,7 @@ import { Box, Typography, Grid } from "@mui/material";
 
 const InTheBox = ({ in_the_box }) => {
   if (in_the_box?.hidden) return null;
-  console.log({ in_the_box });
+
   return (
     <Box sx={{ marginTop: 4, marginBottom: 4 }}>
       <Grid container spacing={2}>
