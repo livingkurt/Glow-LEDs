43ae6cf4c287d2da4478a7580d71444e55b02236
Kurt LaVacque <lavacquek@icloud.com>
2024-07-23 18:00:01 -0500

    Working through some color removal

--- client/src/shared/SharedComponents/index.js
+++ client/src/shared/SharedComponents/index.js
@@ -8,7 +8,6 @@ export { default as ToggleSwitch } from "./ToggleSwitch";
 export { default as ImageLoad } from "./ImageLoad";
 export { default as LazyImage } from "./LazyImage";
 export { default as ErrorView } from "./ErrorView";
-export { default as DropdownDisplay } from "./DropdownDisplay";
 export { default as DropdownDisplayV2 } from "./DropdownDisplayV2";
 export { default as ImageDisplay } from "./ImageDisplay";
 export { default as Notification } from "./Notification";
