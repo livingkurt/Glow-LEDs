e8520a8d928147952f79c175159c53362c2b7d4c
Kurt LaVacque <lavacquek@icloud.com>
2024-07-30 19:21:29 -0500

    Got the cards looking cool

--- client/src/pages/ProductsGridPage/ProductsGridPage.jsx
+++ client/src/pages/ProductsGridPage/ProductsGridPage.jsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, Link } from "react-router-dom";
 import { Container, Grid, Typography, Card, CardContent, CardMedia, Rating, Chip, Box } from "@mui/material";
 import { useProductsGridQuery } from "../../api/allRecordsApi";
 import { useSelector } from "react-redux";
@@ -35,9 +35,10 @@ const ProductGridPage = () => {
   }, [products, current_user?.isWholesaler]);
 
   const handleTagClick = tagName => {
-    const newTags = tags.includes(tagName.toLowerCase())
-      ? tags.filter(t => t.toLowerCase() !== tagName.toLowerCase())
-      : [...tags, tagName.toLowerCase()];
+    const formattedTagName = tagName.toLowerCase().replace(/\s+/g, "_");
+    const newTags = tags.includes(formattedTagName)
+      ? tags.filter(t => t !== formattedTagName)
+      : [...tags, formattedTagName];
 
     const newUrl = `${location.pathname}?${newTags.map(tag => `tags[]=${tag}`).join("&")}`;
     navigate(newUrl);
@@ -47,8 +48,8 @@ const ProductGridPage = () => {
   if (isError) return <Typography>Error loading products</Typography>;
 
   return (
-    <Container maxWidth="lg">
-      <Typography variant="h4" component="h1" gutterBottom align="center">
+    <Container maxWidth="xl">
+      <Typography variant="h4" component="h1" gutterBottom align="center" pt={2}>
         Our Products
       </Typography>
 
@@ -60,8 +61,8 @@ const ProductGridPage = () => {
             onClick={() => handleTagClick(tag)}
             sx={{
               m: 0.5,
-              backgroundColor: tags.includes(tag.toLowerCase()) ? "white" : "transparent",
-              color: tags.includes(tag.toLowerCase()) ? "black" : "white",
+              backgroundColor: tags.includes(tag.toLowerCase().replace(/\s+/g, "_")) ? "white" : "transparent",
+              color: tags.includes(tag.toLowerCase().replace(/\s+/g, "_")) ? "black" : "white",
               border: "1px solid white",
               fontSize: "1rem",
               fontWeight: "500",
@@ -74,18 +75,50 @@ const ProductGridPage = () => {
         {products &&
           products.map(product => (
             <Grid item key={product._id} xs={12} sm={6} md={4} lg={3}>
-              <Card>
-                <CardMedia component="img" height="200" image={product.images[0].link} alt={product.name} />
-                <CardContent>
-                  <Typography gutterBottom variant="h6" component="div">
-                    {product.name}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    ${product.price.toFixed(2)}
-                  </Typography>
-                  {product.rating ? <Rating value={product.rating} readOnly /> : null}
-                </CardContent>
-              </Card>
+              <Link to={`/collections/all/products/${product._id}`} style={{ textDecoration: "none" }}>
+                <Card
+                  sx={{
+                    bgcolor: "transparent",
+                    height: "100%",
+                    transition: "box-shadow 0.3s ease-in-out",
+                    "&:hover": {
+                      boxShadow: "0 12px 24px 0 rgba(255,255,255,0.5)",
+                    },
+                    borderRadius: "1rem",
+                  }}
+                  elevation={0}
+                >
+                  <Box sx={{ position: "relative", paddingTop: "100%", overflow: "hidden" }}>
+                    <CardMedia
+                      component="img"
+                      image={product.images[0].link}
+                      alt={product.name}
+                      sx={{
+                        position: "absolute",
+                        top: 0,
+                        left: 0,
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "cover",
+                        borderRadius: "1rem",
+                        transition: "border-radius 0.3s ease-in-out",
+                        "&:hover": {
+                          borderRadius: "1rem 1rem 0 0",
+                        },
+                      }}
+                    />
+                  </Box>
+                  <CardContent>
+                    <Typography variant="h6" color="white">
+                      {product.name}
+                    </Typography>
+                    <Typography variant="body1" color="white">
+                      ${product.price.toFixed(2)}
+                    </Typography>
+                    {product.rating ? <Rating value={product.rating} readOnly /> : null}
+                  </CardContent>
+                </Card>
+              </Link>
             </Grid>
           ))}
       </Grid>
