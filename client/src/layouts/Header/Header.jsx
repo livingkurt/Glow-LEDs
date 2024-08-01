49f468f230002f741efa80c0e5a696afcd5db2ce
Kurt LaVacque <lavacquek@icloud.com>
2024-07-31 22:52:42 -0500

    Making good progress

--- client/src/layouts/Header/Header.jsx
+++ client/src/layouts/Header/Header.jsx
@@ -1,15 +1,25 @@
 import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { Box, AppBar, Toolbar, Typography, useMediaQuery, useTheme, Container } from "@mui/material";
+import { listContents } from "../../api";
+import { setSideNavDrawer } from "../../slices/cartSlice";
+import { GLButton } from "../../shared/GlowLEDsComponents";
+import { navItems } from "./headerHelpers";
 import Banner from "./components/Banner";
-import LeftNavButtons from "./components/LeftNavButtons";
 import RightNavButtons from "./components/RightNavButtons";
 import SearchBar from "./components/SearchBar";
-import CenterNavButtons from "./components/CenterNavButtons/CenterNavButtons";
-import { listContents } from "../../api";
-import { useDispatch } from "react-redux";
 import Environment from "./components/Environment";
+import HeaderButton from "./components/CenterNavButtons/components/HeaderButton";
+import HeaderColumn from "./components/CenterNavButtons/components/HeaderColumn";
+import HeaderDrawer from "./components/CenterNavButtons/components/HeaderDrawer";
+import HeaderSubDrawer from "./components/CenterNavButtons/components/HeaderSubDrawer";
 
 const Header = () => {
   const dispatch = useDispatch();
+  const theme = useTheme();
+  const isTablet = useMediaQuery(theme.breakpoints.down("md"));
+
   useEffect(() => {
     let clean = true;
     if (clean) {
@@ -21,18 +31,104 @@ const Header = () => {
   }, [dispatch]);
 
   return (
-    <header>
-      <nav aria-label="Main navigation" className="pb-2rem">
-        <Environment />
-        <Banner />
-        <div className="header-main">
-          <LeftNavButtons />
-          <CenterNavButtons />
-          <RightNavButtons />
-        </div>
-        <SearchBar />
-      </nav>
-    </header>
+    <AppBar position="static" color="transparent" elevation={5}>
+      <Environment />
+      <Banner />
+      <Toolbar
+        sx={{
+          padding: theme.spacing(1, 2),
+        }}
+      >
+        <Container
+          maxWidth="xl"
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          {isTablet && (
+            <GLButton onClick={() => dispatch(setSideNavDrawer(true))} aria-label="Sidebar" sx={{ fontSize: "30px" }}>
+              <Box>
+                <Box>
+                  <Box
+                    component="span"
+                    sx={{
+                      display: "block",
+                      width: "25px",
+                      height: "3px",
+                      backgroundColor: "currentColor",
+                      marginBottom: "5px",
+                    }}
+                  />
+                  <Box
+                    component="span"
+                    sx={{
+                      display: "block",
+                      width: "25px",
+                      height: "3px",
+                      backgroundColor: "currentColor",
+                      marginBottom: "5px",
+                    }}
+                  />
+                  <Box
+                    component="span"
+                    sx={{ display: "block", width: "25px", height: "3px", backgroundColor: "currentColor" }}
+                  />
+                </Box>
+              </Box>
+            </GLButton>
+          )}
+          <Box display="flex" alignItems="center">
+            <Box display="flex" alignItems="center">
+              <Link
+                to="/"
+                aria-label="Home Page"
+                style={{ textDecoration: "none", display: "flex", alignItems: "center" }}
+              >
+                <Box
+                  component="img"
+                  sx={{ width: 50, marginRight: 1 }}
+                  src="/images/optimized_images/logo_images/glow_leds_text_logo.png"
+                  alt="Glow LEDs Logo"
+                  title="Big Logo"
+                />
+                {/* <Typography variant="glow_leds" sx={{ fontSize: "24px", color: "white" }}>
+                  Glow LEDs
+                </Typography> */}
+              </Link>
+            </Box>
+
+            {!isTablet && (
+              <Box display="flex" alignItems="center" justifyContent="center" flexGrow={1}>
+                {navItems.map(item => (
+                  <div key={item.name} className="header-center-dropdown-container">
+                    <HeaderButton to={item.path} ariaLabel={item.ariaLabel}>
+                      {item.name}
+                    </HeaderButton>
+                    {item.columns && (
+                      <div className="header-center-dropdown hover_fade_in">
+                        <div className="jc-c">
+                          <HeaderColumn columns={item.columns} />
+                          <HeaderDrawer columns={item.columns} />
+                          <HeaderSubDrawer columns={item.columns} />
+                          <HeaderColumn columns={item.otherColumns} />
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                ))}
+              </Box>
+            )}
+          </Box>
+
+          <Box display="flex" alignItems="center">
+            <SearchBar />
+            <RightNavButtons />
+          </Box>
+        </Container>
+      </Toolbar>
+    </AppBar>
   );
 };
 
