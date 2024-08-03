cc71541a06424b6e0ba17022aa58af490a46d226
Kurt LaVacque <57471593+livingkurt@users.noreply.github.com>
2024-08-03 10:49:40 -0500

    GL-4200-Disable-Options-Based-On-Count-In-Stock #257
    GL-4200-Disable-Options-Based-On-Count-In-Stock #257

--- client/src/shared/GlowLEDsComponents/GLToggleButtons/GLToggleButtons.jsx
+++ client/src/shared/GlowLEDsComponents/GLToggleButtons/GLToggleButtons.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ToggleButtonGroup, ToggleButton, Box, useTheme } from "@mui/material";
 
-const GLToggleButtons = ({ ariaLabel, value, onChange, options, additionalCostLabel, sx }) => {
+const GLToggleButtons = ({ ariaLabel, value, onChange, options, additionalCostLabel, sx, disabledOptions }) => {
   const theme = useTheme();
   return (
     <ToggleButtonGroup
@@ -18,10 +18,27 @@ const GLToggleButtons = ({ ariaLabel, value, onChange, options, additionalCostLa
           color: "white",
           width: "100%",
           padding: "8px 16px",
+          position: "relative", // For absolute positioning of pseudo-element
         },
         "& .Mui-selected": {
           backgroundColor: "white !important",
           color: `${theme.palette.primary.main} !important`,
+        },
+        "& .Mui-disabled": {
+          backgroundColor: theme.palette.grey[600],
+          color: `${theme.palette.grey[400]} !important`,
+          cursor: "not-allowed",
+          overflow: "hidden", // Ensure the pseudo-element doesn't overflow
+          "&::after": {
+            content: '""',
+            position: "absolute",
+            top: "50%",
+            left: "10%", // Start the line at 10% from the left
+            right: "10%", // End the line at 10% from the right
+            borderTop: `2px solid ${theme.palette.grey[400]}`,
+            transform: "translateY(-50%) rotate(-45deg)", // Center the line vertically
+            transformOrigin: "center", // Rotate around the center
+          },
         },
       }}
     >
@@ -30,6 +47,7 @@ const GLToggleButtons = ({ ariaLabel, value, onChange, options, additionalCostLa
           key={index}
           value={option.name}
           aria-label={option.name}
+          disabled={disabledOptions?.includes(option.name)}
           sx={{
             "&:hover": { backgroundColor: "#393e55" },
           }}
