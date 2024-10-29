5e2ed3f1917a87f7a1a103d03b67f72028fb4880
Kurt LaVacque <lavacquek@icloud.com>
2024-10-29 14:45:52 +0100

    Removed comment

--- client/src/pages/OrdersPage/components/EditOrderModal.jsx
+++ client/src/pages/OrdersPage/components/EditOrderModal.jsx
@@ -118,7 +118,6 @@ const EditOrderModal = () => {
           formData={formFields}
           state={order}
           onChange={(value, fieldName, index) => {
-            console.log({ value, fieldName, index });
             if (fieldName === "delete") {
               handleDelete(value, dispatch, order, isUpdatePricesActive);
             } else if (fieldName === "duplicate") {
