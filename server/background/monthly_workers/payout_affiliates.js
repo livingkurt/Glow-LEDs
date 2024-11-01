1f586410411315ab7ab37d13910ec0db5586eaca
Kurt LaVacque <57471593+livingkurt@users.noreply.github.com>
2024-11-01 21:17:41 +0100

    GL-4690-Fixed-Affiliate-Earnings #380
    GL-4690-Fixed-Affiliate-Earnings #380

--- server/background/monthly_workers/payout_affiliates.js
+++ server/background/monthly_workers/payout_affiliates.js
@@ -7,8 +7,9 @@ export const payout_affiliates = async () => {
     const domainUrl = domain();
 
     const { start_date, end_date } = last_month_date_range();
+
     const { data } = await axios.get(`${domainUrl}/api/affiliates?active=true&rave_mob=false`);
-    const affiliates = data.data;
+    const affiliates = data.filter(affiliate => affiliate?.active).filter(affiliate => affiliate?.user?.first_name);
 
     for (const affiliate of affiliates) {
       const { data: promo_code_usage } = await axios.get(
@@ -22,8 +23,8 @@ export const payout_affiliates = async () => {
       if (affiliate?.user?.stripe_connect_id && promo_code_usage.earnings >= 1) {
         console.log({
           amount: promo_code_usage.earnings,
-          stripe_connect_id: affiliate.user.stripe_connect_id,
-          description: `Monthly Payout for ${affiliate.user.first_name} ${affiliate.user.last_name}`,
+          stripe_connect_id: affiliate?.user?.stripe_connect_id,
+          description: `Monthly Payout for ${affiliate?.user?.first_name} ${affiliate?.user?.last_name}`,
         });
         await axios.post(`${domainUrl}/api/payments/payout_transfer`, {
           amount: promo_code_usage.earnings,
@@ -40,9 +41,10 @@ export const payout_affiliates = async () => {
         uses: promo_code_usage.number_of_uses,
         stripe_connect_id: affiliate?.user?.stripe_connect_id || null,
         paid: affiliate?.user?.stripe_connect_id ? true : false,
-        description: `Monthly Payout for ${affiliate?.user.first_name} ${affiliate?.user.last_name}`,
-        paid_at: new Date(),
-        email: affiliate.user.email,
+        description: `Monthly Payout for ${affiliate?.user?.first_name} ${affiliate?.user?.last_name}`,
+        // paid_at: new Date(),
+        paid_at: new Date("2024-11-01"),
+        email: affiliate?.user?.email,
       });
       await axios.post(`${domainUrl}/api/paychecks`, {
         affiliate: affiliate?._id,
@@ -53,9 +55,9 @@ export const payout_affiliates = async () => {
         uses: promo_code_usage.number_of_uses,
         stripe_connect_id: affiliate?.user?.stripe_connect_id || null,
         paid: affiliate?.user?.stripe_connect_id ? true : false,
-        description: `Monthly Payout for ${affiliate?.user.first_name} ${affiliate?.user.last_name}`,
-        first_name: affiliate.user?.first_name,
-        paid_at: new Date(),
+        description: `Monthly Payout for ${affiliate?.user?.first_name} ${affiliate?.user?.last_name}`,
+        // paid_at: new Date(),
+        paid_at: new Date("2024-11-01"),
         email: affiliate.user.email,
         subject: "Your Glow LEDs Affiliate Earnings",
       });
@@ -64,9 +66,10 @@ export const payout_affiliates = async () => {
         ...affiliate.private_code,
         percentage_off,
       });
+      console.log({ percentage_off });
 
       // Delay between each iteration
-      await new Promise(resolve => setTimeout(resolve, 10000)); // 10 seconds delay
+      await new Promise(resolve => setTimeout(resolve, 1000)); // 10 seconds delay
     }
   } catch (error) {
     console.log("error", error);
