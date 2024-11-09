22a360ec5bd4f2a82d6d5f907a63ca6ab7f95021
Kurt LaVacque <lavacquek@icloud.com>
2024-11-09 18:14:21 +0100

    Removed other formFields

--- client/src/pages/TagsPage/tagHelpers.js
+++ client/src/pages/TagsPage/tagHelpers.js
@@ -1,23 +1,5 @@
 import { tableColors } from "../../shared/GlowLEDsComponents/GLTableV2/glTableHelpers";
 
-export const tagFormFields = () => {
-  return {
-    name: {
-      type: "text",
-      label: "Name",
-      required: true,
-    },
-    pathname: {
-      type: "text",
-      label: "Pathname",
-    },
-    active: {
-      type: "checkbox",
-      label: "Active",
-    },
-  };
-};
-
 export const determineTagColors = tag => {
   return tag.active ? tableColors.active : tableColors.inactive;
 };
