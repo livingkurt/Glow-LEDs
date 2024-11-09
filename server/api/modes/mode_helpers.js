d55fa81ceea3767b32c8902ae856fb6c3cf6b915
Kurt LaVacque <lavacquek@icloud.com>
2024-11-09 21:26:49 +0100

    Removed Comments

--- server/api/modes/mode_helpers.js
+++ server/api/modes/mode_helpers.js
@@ -1,30 +1,3 @@
-// export const normalizeModeFilters = input => {
-//   const output = {};
-//   Object.keys(input).forEach(key => {
-//     switch (key) {
-//       case "category":
-//         for (const category of input.category) {
-//           output["category"] = category;
-//         }
-//         break;
-//       case "card":
-//         for (const card of input.card) {
-//           output["card"] = card;
-//         }
-//         break;
-//       case "place_of_purchase":
-//         for (const place_of_purchase of input.place_of_purchase) {
-//           output["place_of_purchase"] = place_of_purchase;
-//         }
-//         break;
-
-//       default:
-//         break;
-//     }
-//   });
-//   return output;
-// };
-
 export const normalizeModeSearch = query => {
   const search = query.search
     ? {
