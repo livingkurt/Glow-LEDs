aebe6c7559366897ea28e4a133002b681888e1c3
Kurt LaVacque <lavacquek@icloud.com>
2024-11-16 23:49:26 +0100

    Put the Modes on the Profile Page

--- client/src/pages/ProfilePage/ProfilePage.jsx
+++ client/src/pages/ProfilePage/ProfilePage.jsx
@@ -13,19 +13,18 @@ import ProfileAffiliateMetrics from "./components/ProfileAffiliateActions";
 import GLTableV2 from "../../shared/GlowLEDsComponents/GLTableV2/GLTableV2";
 import { determineColor } from "../PaychecksPage/paychecksHelpers";
 import { orderStatusColors, determineOrderColors } from "../OrdersPage/ordersPageHelpers";
-import { Container, Grid, Box, Tabs, Tab } from "@mui/material";
+import { Container, Grid, Box, Divider, Typography, Button } from "@mui/material";
 import SponsorMonthlyCheckinModal from "./components/SponsorMonthlyCheckinModal";
 import { useUserProfilePage } from "./useUserProfilePage";
 import useAffiliateProfilePage from "./useAffiliateProfilePage";
 import { GLDisplayTable } from "../../shared/GlowLEDsComponents/GLDisplayTable";
 import { months } from "../DashboardPage/dashboardHelpers";
-import ProfileModes from "./components/ProfileModes";
+import ModeCard from "../ModesGridPage/components/ModeCard";
 
 export const ProfilePage = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
-  const currentTab = searchParams.get("tab") || "profile";
 
   const userPage = useSelector(state => state.users.userPage);
   const { current_user, user } = userPage;
@@ -47,89 +46,6 @@ export const ProfilePage = () => {
     monthlyEarnings,
   } = useAffiliateProfilePage();
 
-  const handleTabChange = (event, newValue) => {
-    navigate(`/secure/account/profile?tab=${newValue}`);
-  };
-
-  const renderTabContent = () => {
-    switch (currentTab) {
-      case "modes":
-        return <ProfileModes />;
-      default:
-        return (
-          <>
-            <Grid container spacing={2}>
-              <Grid item xs={12} md={6}>
-                <ProfileDetails />
-                <ProfileActions />
-              </Grid>
-              <Grid item xs={12} md={6}>
-                <ProfileAffiliateMetrics
-                  sponsorCodes={sponsorCodes}
-                  currentMonthEarnings={currentMonthEarnings}
-                  yearlyEarnings={yearlyEarnings}
-                />
-              </Grid>
-
-              <Grid item xs={12}>
-                {user && user?.affiliate?._id && (
-                  <GLDisplayTable
-                    title="Monthly Revenue"
-                    rows={
-                      !monthlyEarnings.isLoading &&
-                      monthlyEarnings.data &&
-                      monthlyEarnings.data.length > 0 &&
-                      [...monthlyEarnings.data].sort((a, b) => a.month - b.month)
-                    }
-                    columnDefs={[
-                      { title: "Month", display: row => months[row.month - 1] },
-                      { title: "Number of Uses", display: row => row.number_of_uses },
-                      { title: "Earnings", display: row => `-$${row.earnings.toFixed(2)}` },
-                      { title: "Revenue", display: row => `$${row.revenue?.toFixed(2)}` },
-                    ]}
-                  />
-                )}
-              </Grid>
-              <Grid item xs={12}>
-                {user._id && (
-                  <GLTableV2
-                    remoteApi={ordersRemoteApi}
-                    tableName="Orders"
-                    colors={Object.values(orderStatusColors)}
-                    enableSearch={false}
-                    noURLParams
-                    determineColor={determineOrderColors}
-                    namespaceScope="orders"
-                    namespace="orderTable"
-                    columnDefs={orderColumnDefs}
-                    loading={loading_order}
-                    enableRowSelect={false}
-                  />
-                )}
-              </Grid>
-              <Grid item xs={12}>
-                {user && user?.affiliate?._id && (
-                  <GLTableV2
-                    remoteApi={paychecksRemoteApi}
-                    remoteVersionRequirement={remoteVersionRequirement}
-                    determineColor={determineColor}
-                    noURLParams
-                    tableName="Paychecks"
-                    enableSearch={false}
-                    namespaceScope="paychecks"
-                    namespace="paycheckTable"
-                    columnDefs={paycheckColumnDefs}
-                    loading={loading}
-                    enableRowSelect={false}
-                  />
-                )}
-              </Grid>
-            </Grid>
-          </>
-        );
-    }
-  };
-
   return (
     <Container maxWidth="lg" sx={{ py: 2 }}>
       <Helmet>
@@ -147,14 +63,128 @@ export const ProfilePage = () => {
       </div>
       <Loading loading={loading}></Loading>
 
-      <Box sx={{ borderBottom: 1, borderColor: "divider", mb: 3 }}>
-        <Tabs value={currentTab} onChange={handleTabChange} aria-label="profile tabs">
-          <Tab label="Profile" value="profile" />
-          <Tab label="Modes" value="modes" />
-        </Tabs>
-      </Box>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <ProfileDetails />
+          <ProfileActions />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <ProfileAffiliateMetrics
+            sponsorCodes={sponsorCodes}
+            currentMonthEarnings={currentMonthEarnings}
+            yearlyEarnings={yearlyEarnings}
+          />
+        </Grid>
 
-      {renderTabContent()}
+        <Grid item xs={12}>
+          {user && user?.affiliate?._id && (
+            <GLDisplayTable
+              title="Monthly Revenue"
+              rows={
+                !monthlyEarnings.isLoading &&
+                monthlyEarnings.data &&
+                monthlyEarnings.data.length > 0 &&
+                [...monthlyEarnings.data].sort((a, b) => a.month - b.month)
+              }
+              columnDefs={[
+                { title: "Month", display: row => months[row.month - 1] },
+                { title: "Number of Uses", display: row => row.number_of_uses },
+                { title: "Earnings", display: row => `-$${row.earnings.toFixed(2)}` },
+                { title: "Revenue", display: row => `$${row.revenue?.toFixed(2)}` },
+              ]}
+            />
+          )}
+        </Grid>
+        <Grid item xs={12}>
+          {user.modes && user.modes.length > 0 && (
+            <>
+              <Divider sx={{ my: 4, borderColor: "#fff" }} />
+              <Box>
+                <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", mb: 2 }}>
+                  <Typography variant="h4" align="left">
+                    {"Modes by "}
+                    {user.first_name} {user.last_name}
+                  </Typography>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={() => navigate("/modes", { state: { user: user._id } })}
+                  >
+                    {"View All Modes"}
+                  </Button>
+                </Box>
+                <Box
+                  sx={{
+                    pb: 6,
+                    px: 2,
+                    display: "flex",
+                    overflowX: "auto",
+                    minWidth: "100%",
+                    "&::-webkit-scrollbar": {
+                      height: "8px",
+                    },
+                    "&::-webkit-scrollbar-thumb": {
+                      backgroundColor: "rgba(0, 0, 0, 0.2)",
+                      borderRadius: "4px",
+                    },
+                  }}
+                >
+                  {user.modes.map(mode => (
+                    <Box
+                      key={mode._id}
+                      sx={{
+                        minWidth: "250px",
+                        maxWidth: "300px",
+                        width: "100%",
+                        marginRight: "20px",
+                        "&:last-child": {
+                          marginRight: 0,
+                        },
+                      }}
+                    >
+                      <ModeCard mode={mode} />
+                    </Box>
+                  ))}
+                </Box>
+              </Box>
+            </>
+          )}
+        </Grid>
+        <Grid item xs={12}>
+          {user._id && (
+            <GLTableV2
+              remoteApi={ordersRemoteApi}
+              tableName="Orders"
+              colors={Object.values(orderStatusColors)}
+              enableSearch={false}
+              noURLParams
+              determineColor={determineOrderColors}
+              namespaceScope="orders"
+              namespace="orderTable"
+              columnDefs={orderColumnDefs}
+              loading={loading_order}
+              enableRowSelect={false}
+            />
+          )}
+        </Grid>
+        <Grid item xs={12}>
+          {user && user?.affiliate?._id && (
+            <GLTableV2
+              remoteApi={paychecksRemoteApi}
+              remoteVersionRequirement={remoteVersionRequirement}
+              determineColor={determineColor}
+              noURLParams
+              tableName="Paychecks"
+              enableSearch={false}
+              namespaceScope="paychecks"
+              namespace="paycheckTable"
+              columnDefs={paycheckColumnDefs}
+              loading={loading}
+              enableRowSelect={false}
+            />
+          )}
+        </Grid>
+      </Grid>
       <SponsorMonthlyCheckinModal />
     </Container>
   );
