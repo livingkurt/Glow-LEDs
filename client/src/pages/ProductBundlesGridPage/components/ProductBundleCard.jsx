ff28a578698d025171c29ebdcb7fc677f2af4cc7
Kurt LaVacque <lavacquek@icloud.com>
2025-01-31 11:05:10 +0100

    Reorder import of random function in ProductBundleCard

--- client/src/pages/ProductBundlesGridPage/components/ProductBundleCard.jsx
+++ client/src/pages/ProductBundlesGridPage/components/ProductBundleCard.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import PropTypes from "prop-types";
 
 import { Link } from "react-router-dom";
+import random from "lodash/random";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import GLLazyImage from "../../../shared/GlowLEDsComponents/GLLazyImage/GLLazyImage";
@@ -13,7 +14,6 @@ import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import useTheme from "@mui/material/styles/useTheme";
-import random from "lodash/random";
 
 const ProductBundleCard = ({ bundle, affiliate, goHorizontal = true, index }) => {
   const theme = useTheme();
