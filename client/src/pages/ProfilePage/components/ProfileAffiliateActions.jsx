bbdbeabc9c5c848947a0d3a7f4825f28aeeabb6f
Kurt LaVacque <lavacquek@icloud.com>
2025-02-12 14:59:43 +0100

    Removed react import to fix linting

--- client/src/pages/ProfilePage/components/ProfileAffiliateActions.jsx
+++ client/src/pages/ProfilePage/components/ProfileAffiliateActions.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useSelector } from "react-redux";
 import { determine_terms_link } from "../profileHelpers";
 import { Loading } from "../../../shared/SharedComponents";
