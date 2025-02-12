6e075ecf1b781b746afcf498f923a3582c4ac22e
Kurt LaVacque <lavacquek@icloud.com>
2025-02-12 11:17:59 +0100

    Saving

--- client/src/pages/ProfilePage/components/ProfileAffiliateActions.jsx
+++ client/src/pages/ProfilePage/components/ProfileAffiliateActions.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { determine_terms_link } from "../profileHelpers";
-
 import { Loading } from "../../../shared/SharedComponents";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import { determineRevenueTier } from "../../../utils/helpers/universal_helpers";
+
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { Card, CardContent } from "@mui/material";
 
 const ProfileAffiliateMetrics = ({ sponsorCodes, currentMonthEarnings, yearlyEarnings }) => {
   const userPage = useSelector(state => state.users.userPage);
@@ -35,105 +35,137 @@ const ProfileAffiliateMetrics = ({ sponsorCodes, currentMonthEarnings, yearlyEar
       </CardContent>
     </Card>
   );
-
   const determineTitle = () => {
     if (user.affiliate.sponsorTeamCaptain) return "Sponsor Team Captain Metrics";
     if (user.affiliate.sponsor) return "Sponsor Metrics";
     if (user.affiliate.promoter) return "Promoter Metrics";
-    return "Affiliate Metrics"; // default title
+    return "Affiliate Metrics";
   };
 
   return (
-    <div className="group_item w-100per">
+    <Box sx={{ p: 3 }}>
       {user.is_affiliated && user?.affiliate && user?.affiliate?.public_code && (
-        <div className="mb-20px ">
-          <h2 className="group_images">{determineTitle()}</h2>
+        <Stack spacing={3}>
+          <Typography variant="h5" gutterBottom>
+            {determineTitle()}
+          </Typography>
+
           <Loading loading={yearlyEarnings.isLoading || currentMonthEarnings.isLoading}>
             {!currentMonthEarnings.isLoading && <EarningsCard title="Current Month" data={currentMonthEarnings.data} />}
             {!yearlyEarnings.isLoading && <EarningsCard title="Current Year" data={yearlyEarnings.data} />}
           </Loading>
-          <div className="mb-20px">
-            <h3>{"Public Code"}</h3>
-            <div>{user?.affiliate?.public_code.promo_code.toUpperCase()}</div>
-          </div>
 
-          <div className="mb-20px">
-            <h3>{"Private Code"}</h3>
-            <div>{user?.affiliate?.private_code.promo_code.toUpperCase()}</div>
-          </div>
+          <Box>
+            <Typography variant="h6" gutterBottom>
+              {"Public Code"}
+            </Typography>
+            <Typography>{user?.affiliate?.public_code.promo_code.toUpperCase()}</Typography>
+          </Box>
+
+          <Box>
+            <Typography variant="h6" gutterBottom>
+              {"Private Code"}
+            </Typography>
+            <Typography>{user?.affiliate?.private_code.promo_code.toUpperCase()}</Typography>
+          </Box>
+
           {user?.affiliate?.artist_name && (
-            <div className="mb-20px">
-              <h3>{"Projected Private Code Discount"}</h3>
-              <div>
+            <Box>
+              <Typography variant="h6" gutterBottom>
+                {"Projected Private Code Discount"}
+              </Typography>
+              <Typography>
                 {determineRevenueTier(user?.affiliate, currentMonthEarnings?.data?.revenue)}
                 {"% Off"}
-              </div>
-            </div>
+              </Typography>
+            </Box>
           )}
+
           {user?.affiliate?.sponsor && (
             <>
-              <div className="mb-20px">
-                <h3>{"Monthly Sponsor Code ($25 off)"}</h3>
-                <div>{sponsorCodes?.data?.twentyFiveOffCode?.toUpperCase()}</div>
-              </div>
-              <div className="mb-20px">
-                <h3>{"Refresh Pack Sponsor Code"}</h3>
-                <div>{sponsorCodes?.data?.refreshCode?.toUpperCase()}</div>
-              </div>
+              <Box>
+                <Typography variant="h6" gutterBottom>
+                  {"Monthly Sponsor Code ($25 off)"}
+                </Typography>
+                <Typography>{sponsorCodes?.data?.twentyFiveOffCode?.toUpperCase()}</Typography>
+              </Box>
+
+              <Box>
+                <Typography variant="h6" gutterBottom>
+                  {"Refresh Pack Sponsor Code"}
+                </Typography>
+                <Typography>{sponsorCodes?.data?.refreshCode?.toUpperCase()}</Typography>
+              </Box>
             </>
           )}
 
           {user?.affiliate?.sponsorTeamCaptain && (
-            <>
-              <div className="mb-20px">
-                <h3>
-                  {"You Earn "}
-                  {user?.affiliate?.sponsorTeamCaptain ? "20%" : user?.affiliate?.sponsor ? "15%" : "10%"} {"on"}
-                  {"Revenue Generated"}
-                </h3>
-
-                <div>
-                  {"Example: $20 * 10% = $18 *"}{" "}
-                  {user?.affiliate?.sponsorTeamCaptain
-                    ? "20% = $3.60"
-                    : user?.affiliate?.sponsor
-                      ? "15% = $2.70"
-                      : "10% = $2.00"}
-                </div>
-              </div>
-            </>
+            <Box>
+              <Typography variant="h6" gutterBottom>
+                {"You Earn "}
+                {user?.affiliate?.sponsorTeamCaptain ? "20%" : user?.affiliate?.sponsor ? "15%" : "10%"} {"on"}
+                {" Revenue Generated"}
+              </Typography>
+              <Typography>
+                {"Example: $20 * 10% = $18 * "}
+                {user?.affiliate?.sponsorTeamCaptain
+                  ? "20% = $3.60"
+                  : user?.affiliate?.sponsor
+                    ? "15% = $2.70"
+                    : "10% = $2.00"}
+              </Typography>
+            </Box>
           )}
-          <div className="mb-20px">
-            <h3>{"Promo Code URL"}</h3>
-            <div>
+
+          <Box>
+            <Typography variant="h6" gutterBottom>
+              {"Promo Code URL"}
+            </Typography>
+            <Typography>
               {"https://www.glow-leds.com?code="}
               {user?.affiliate?.public_code.promo_code.toUpperCase()}
-            </div>
-          </div>
-          <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-            <a
-              href={determine_terms_link(user.affiliate)}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="mr-10px"
-            >
-              <Button variant="contained" color="secondary" fullWidth>
-                {"Affiliate Terms"}
-              </Button>
-            </a>
-            <a
+            </Typography>
+          </Box>
+
+          <Stack spacing={2}>
+            <Button
+              variant="contained"
+              color="primary"
+              fullWidth
+              component="a"
               href="https://docs.google.com/document/d/1hiquje1Bw-SWlYEO2Lp8NMfVZhvMRNNrwNog4Ltr5Ac/edit"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Button variant="contained" color="secondary" fullWidth>
-                {"Affiliate Learnings"}
-              </Button>
-            </a>
-          </Box>
-        </div>
+              {"Affiliate Content"}
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              fullWidth
+              component="a"
+              href={determine_terms_link(user.affiliate)}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {"Affiliate Terms"}
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              fullWidth
+              component="a"
+              href="https://docs.google.com/document/d/1hiquje1Bw-SWlYEO2Lp8NMfVZhvMRNNrwNog4Ltr5Ac/edit"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {"Affiliate Learnings"}
+            </Button>
+          </Stack>
+        </Stack>
       )}
-    </div>
+    </Box>
   );
 };
+
 export default ProfileAffiliateMetrics;
